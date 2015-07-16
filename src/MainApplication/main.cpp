a4b545d5a4d0af0d3d05710ce48bc6626b304cb6
Valentin Roussellet <val.roussellet@gmail.com>
2015-07-16 17:05:02 +0200

    Moved up main.cpp + build config printing at start

--- src/MainApplication/main.cpp
+++ src/MainApplication/main.cpp
@@ -1,10 +1,24 @@
-#include <qapplication.h>
-#include <MainApplication/Gui/MainWindow.hpp>
-
+#include <iostream>
+#include <QApplication>
 #include <Core/CoreMacros.hpp>
+#include <MainApplication/Gui/MainWindow.hpp>
 
 int main(int argc, char** argv)
 {
+    std::cerr<<"*** Radium Engine Main App  ***"<<std::endl;
+#if defined (CORE_DEBUG)
+    std::cerr<<"(Debug Build)"<<std::endl;
+ #else
+    std::cerr<<"(Release Build)"<<std:endl;
+#endif
+    std::cerr<<"Floating point format : ";
+#if defined(CORE_USE_DOUBLE)
+    std::cerr<<"double precision"<<std::endl;
+#else
+    std::cerr<<"single precision"<<std::endl;
+#endif
+
+
     QApplication app(argc, argv);
 
     QSurfaceFormat format;
