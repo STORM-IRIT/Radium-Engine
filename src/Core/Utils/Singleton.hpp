1d6ff2f06d0e6b18b702d0d80290ab28ce03a498
Valentin Roussellet <val.roussellet@gmail.com>
2015-07-20 11:15:17 +0200

    Remove RTTI from singleton

--- src/Core/Utils/Singleton.hpp
+++ src/Core/Utils/Singleton.hpp
@@ -1,72 +1,60 @@
 #ifndef RADIUMENGINE_SINGLETON_HPP
 #define RADIUMENGINE_SINGLETON_HPP
 
-#include <cassert>
 #include <cstdio>
-#include <typeinfo>
+#include <Core/CoreMacros.hpp>
 
 namespace Ra { namespace Core
 {
 
-template <typename T>
-class Singleton
-{
-public:
-	template <typename... Args>
-	static T* createInstance(const Args&... args)
-	{
-		if (nullptr == s_instance)
-		{
-			s_instance = new T(args...);
-		}
-		else 
-		{
-			fprintf(stderr, "Singleton<%s> has already been instanciated.\n",
-					typeid(T).name());
-		}
+    template<typename T>
+    class Singleton
+    {
+    public:
+        template<typename... Args>
+        static T* createInstance(const Args& ... args)
+        {
+            CORE_ASSERT(nullptr == s_instance, "Singleton has already been created");
+            s_instance = new T(args...);
+            return s_instance;
+        }
 
-		return s_instance;
-	}
+        static T* getInstancePtr()
+        {
+            CORE_ASSERT(s_instance, "Singleton is uninitialized.");
+            return s_instance;
+        }
 
-	static T* getInstancePtr()
-	{
-		if (!s_instance)
-		{
-			fprintf(stderr, "Singleton<%s>::getInstancePtr() called on an initialized instance.\n",
-					typeid(T).name());
-		}
+        static T& getInstanceRef()
+        {
+            CORE_ASSERT(s_instance, "Singleton is uninitialized.");
+            return *s_instance;
+        }
 
-		return s_instance;
-	}
+        static void destroyInstance()
+        {
+            if (s_instance)
+            {
+                delete s_instance;
+                s_instance = nullptr;
+            }
+        }
 
-	static T& getInstanceRef()
-	{
-		assert(s_instance != nullptr);
-		return *s_instance;
-	}
+    protected:
+        Singleton() { }
 
-	static void destroyInstance()
-	{
-		if (s_instance)
-		{
-			delete s_instance;
-			s_instance = nullptr;
-		}
-	}
+        ~Singleton() { }
 
-protected:
-	Singleton() {}
-	~Singleton() {}
+    private:
+        Singleton(const Singleton<T>&) = delete;
 
-private:
-	Singleton(const Singleton<T>&) = delete;
-	void operator=(const Singleton<T>&) = delete;
+        void operator=(const Singleton<T>&) = delete;
 
-private:
-	static T* s_instance;
-};
+    private:
+        static T* s_instance;
+    };
 
-template <typename T> T* Singleton<T>::s_instance = nullptr;
+    template<typename T> T* Singleton<T>::s_instance = nullptr;
 
 }} // namespace Ra::Core
 
