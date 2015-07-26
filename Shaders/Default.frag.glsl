e308295e48c21d52c3e787e76c003f0850a14957
Zouch <charly.mourglia@gmail.com>
2015-07-26 16:15:44 +0200

    Fancier default shader

--- Shaders/Default.frag.glsl
+++ Shaders/Default.frag.glsl
@@ -1,6 +1,13 @@
 out vec4 fragColor;
 
+struct Material
+{
+    vec4 kd;
+};
+
+uniform Material material;
+
 void main()
 {
-    fragColor = vec4(1, 0, 0, 1);
+    fragColor = vec4(material.kd.xyz, 1);
 }
--- Shaders/Default.vert.glsl
+++ Shaders/Default.vert.glsl
@@ -1,6 +1,14 @@
 layout (location = 0) in vec3 pos;
 
+uniform mat4 mvp;
+
 void main()
 {
-    gl_Position = vec4(pos, 1.0);
+    mat4 theRealMvp = mvp;
+    if (mvp == mat4(0))
+    {
+        theRealMvp = mat4(1.0);
+    }
+
+    gl_Position = mvp * vec4(pos, 1.0);
 }
