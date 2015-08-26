964a9eab77100c00e01376eb4f6e06112cd73fdb
Valentin Roussellet <val.roussellet@gmail.com>
2015-08-26 15:13:57 +0200

    more color preset functions

--- src/Core/Math/ColorPresets.hpp
+++ src/Core/Math/ColorPresets.hpp
@@ -7,6 +7,8 @@ namespace Ra{
     namespace Core{
         namespace Colors
         {
+            // Primary and secondary colors.
+
             Color Black() { return Color(0,0,0,1); }
 
             Color Red()   { return Color(1,0,0,1); }
@@ -18,6 +20,27 @@ namespace Ra{
             Color Cyan()    { return Color(0,1,1,1); }
 
             Color White()   { return Color(1,1,1,1); }
+
+            // Convert to/from various int formats
+            Color FromChars(uchar r, uchar g, uchar b, uchar a = 0xff)
+            {
+                return Color(Scalar(r)/255.0f, Scalar(g)/255.0f, Scalar(b)/255.0f, Scalar(a)/255.0f );
+            }
+
+            Color FromRGBA32(uint32_t rgba)
+            {
+                uchar r = uchar((rgba >> 24) & 0xff);
+                uchar g = uchar((rgba >> 16) & 0xff);
+                uchar b = uchar((rgba >>  8) & 0xff);
+                uchar a = uchar((rgba >>  0) & 0xff);
+                return FromChars(r,g,b,a);
+            }
+
+            uint32_t ToRGBA32(const Color& color)
+            {
+                Vector4i scaled = (color * 255).cast<int>();
+                return (uint32_t(scaled[0])<<24) | (uint32_t(scaled[1])<<16) |(uint32_t(scaled[2])<<12) |(uint32_t(scaled[3])<<0);
+            }
         }
     }
 }
