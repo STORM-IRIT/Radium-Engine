098f919e8417c1aec592e11ab38a9b2431bfa0bf
Valentin Roussellet <val.roussellet@gmail.com>
2015-09-16 17:43:17 +0200

    Finished (?) cylinder raycast!

--- src/Core/Math/RayCast.hpp
+++ src/Core/Math/RayCast.hpp
@@ -2,9 +2,7 @@
 #define RADIUMENGINE_RAY_CAST_HPP_
 
 #include <Core/RaCore.hpp>
-
 #include <vector>
-
 #include <Core/Math/Ray.hpp>
 
 // useful : http://www.realtimerendering.com/intersections.html
@@ -16,12 +14,13 @@ namespace Ra
         /// Raycast function versus various abstract shapes.
         /// * All functions return true if there was at least a valid (t>=0) hit, false if the ray misses.
         /// * If a ray starts inside the shape, the resulting hit will be at the ray's origin (t=0).
-        /// * Some functions may return several hits.
+        /// * Some functions may return several hits. If the result is true,
+        /// the first hit will always be the first positive hit.
 
         namespace RayCast
         {
             /// Intersect a ray with an axis-aligned bounding box.
-            bool vsAabb( const Ray& r, const Core::Aabb& aabb, std::vector<Scalar>& hitsOut)
+            bool vsAabb(const Ray& r, const Core::Aabb& aabb, std::vector<Scalar>& hitsOut)
             {
                 // Based on optimized Woo version (ray vs 3 slabs)
                 // Ref : Graphics Gems p.395
@@ -39,16 +38,16 @@ namespace Ra
                 auto supMax = r.m_origin.array() > aabb.max().array();
 
                 // Get rid of the case where the origin of the ray is inside the box
-                if ( !(infMin.any()) && !(supMax.any()) )
+                if (!(infMin.any()) && !(supMax.any()))
                 {
                     hitsOut.push_back(0);
                     return true;
                 }
 
                 // Precompute the t values for each plane.
-                Core::Vector3 invDir = r.m_direction.cwiseInverse();
-                Core::Vector3 minOrig = (aabb.min() - r.m_origin).cwiseProduct(invDir);
-                Core::Vector3 maxOrig = (aabb.max() - r.m_origin).cwiseProduct(invDir);
+                const Core::Vector3 invDir = r.m_direction.cwiseInverse();
+                const Core::Vector3 minOrig = (aabb.min() - r.m_origin).cwiseProduct(invDir);
+                const Core::Vector3 maxOrig = (aabb.max() - r.m_origin).cwiseProduct(invDir);
 
                 // The following Eigen dark magic should be equivalent to this pseudo code
                 // For  i = 1..3
@@ -57,23 +56,22 @@ namespace Ra
                 //    else if (origin[i] > aabb.max) then maxT[i] = (max[i] - origin[i]) / direction[i]
 
                 auto maxT =
-                nEqualZero.select(
-                    infMin.select(
-                            minOrig.array(),
-                            supMax.select(
-                                    maxOrig.array(),
-                                    -1.f
-                            )
-                    ),
-                    -1.f
-                );
+                        nEqualZero.select(
+                                infMin.select(
+                                        minOrig.array(),
+                                        supMax.select(
+                                                maxOrig.array(),
+                                                -1.f
+                                        )
+                                ),
+                                -1.f
+                        );
 
-                uint maxTIdx;
-                Scalar t = maxT.maxCoeff(&maxTIdx);
+                const Scalar t = maxT.maxCoeff();
 
                 // Ignore negative t (box behind the origin), and points outside the aabb.
                 if (t >= 0 && aabb.contains(r.at(t)))
-                { 
+                {
                     hitsOut.push_back(t);
                     return true;
                 }
@@ -85,33 +83,34 @@ namespace Ra
             {
 
                 CORE_ASSERT(r.m_direction.squaredNorm() > 0.f, "Invalid Ray");
-                CORE_ASSERT(radius >0.f, "Invalid radius");
+                CORE_ASSERT(radius > 0.f, "Invalid radius");
 
                 // Solve a 2nd degree eqn. in t
                 // X = ray.origin + t* ray.direction
                 // ||X - center|| = radius.
 
-                const Core::Vector3 co = r.m_origin -  center;
+                const Core::Vector3 co = r.m_origin - center;
                 const Scalar co2 = co.squaredNorm();
                 const Scalar dirDotCO = r.m_direction.dot(co);
 
                 // t is one solution of at^2 + bt + c = 0;
                 const Scalar c = co2 - (radius * radius);
-                const Scalar b =  2.f * dirDotCO;
+                const Scalar b = 2.f * dirDotCO;
                 const Scalar a = r.m_direction.squaredNorm();
 
-                const Scalar delta = (b * b) - ( 4.f * a *c);
+                const Scalar delta = (b * b) - (4.f * a * c);
 
                 if (delta == 0.f)
                 {
-                    const Scalar t =  - b / (2.f * a);
+                    const Scalar t = -b / (2.f * a);
                     const bool tPositive = (t >= 0.f);
-                    if (tPositive) {hitsOut.push_back(t);}
+                    if (tPositive)
+                    { hitsOut.push_back(t); }
                     return tPositive;
                 }
                 else if (delta > 0.f)
                 {
-                    const Scalar div = 1.f / (2.f*a);
+                    const Scalar div = 1.f / (2.f * a);
                     const Scalar t1 = (-b - std::sqrt(delta)) * div;
                     const Scalar t2 = (-b + std::sqrt(delta)) * div;
 
@@ -128,8 +127,8 @@ namespace Ra
                 return false;
             }
 
-            /// Intersect a ray with an infinite plane.
-            bool vsPlane( const Ray& r, const Core::Vector3 a, const Core::Vector3& normal, std::vector<Scalar>& hitsOut)
+            /// Intersect a ray with an infinite plane defined by point A and normal.
+            bool vsPlane(const Ray& r, const Core::Vector3 a, const Core::Vector3& normal, std::vector<Scalar>& hitsOut)
             {
                 CORE_ASSERT(r.m_direction.squaredNorm() > 0.f, "Invalid Ray");
                 CORE_ASSERT(normal.squaredNorm() > 0.f, "Invalid plane normal");
@@ -151,7 +150,7 @@ namespace Ra
                 }
                 // If d.n is 0 the ray is parallel to the plane, so there is only an intersection
                 // if the ray is completely in the plane (i.e. if OA.n = 0).
-                else if ( ddotn == 0 && OAdotn == 0)
+                else if (ddotn == 0 && OAdotn == 0)
                 {
                     hitsOut.push_back(0);
                     return true;
@@ -162,17 +161,15 @@ namespace Ra
 
 
             /// Intersect  a ray with a cylinder with a and b as caps centers and given radius.
-i           /// Rer : Graphics Gem IV. http://www.realtimerendering.com/resources/GraphicsGems//gemsiv/ray_cyl.c
-            bool vsCylinder( const Ray& r, const Core::Vector3& a, const Core::Vector3& b, Scalar radius,
-            std::vector<Scalar>& hitsOut)
+            /// Ref : Graphics Gem IV. http://www.realtimerendering.com/resources/GraphicsGems//gemsiv/ray_cyl.c
+            // TODO : this needs serious optimizing if we want it fast :p
+            bool vsCylinder(const Ray& r, const Core::Vector3& a, const Core::Vector3& b, Scalar radius,
+                            std::vector<Scalar>& hitsOut)
             {
-                // Ref : Graphics Gem IV
+                const Scalar radiusSquared = radius * radius;
+
                 const Core::Vector3 cylAxis = b - a;
                 const Core::Vector3 ao = r.m_origin - a;
-
-                // Compute minmal distance from line to cylnder axis.
-                const Scalar distSquared = oa.cross(cylAxis).squaredNorm() / cylAxis.squaredNorm();
-
 
                 // Intersect the ray against plane A and B.
                 std::vector<Scalar> hitsA;
@@ -184,19 +181,22 @@ i           /// Rer : Graphics Gem IV. http://www.realtimerendering.com/resource
                 const Scalar hitB = vsB ? hitsB[0] : -1.f;
 
 
+                auto n = r.m_direction.cross(cylAxis);
                 // Degenerated case : cylinder axis parallel to ray.
-                auto cross = r.m_direction.cross(cylAxis);
-                if (cross.squaredNorm()  == 0)
+                if (UNLIKELY(n.squaredNorm() == 0))
                 {
+                    // Distance between two parallel lines.
+                    const Scalar distSquared = ao.cross(r.m_direction).squaredNorm() / r.m_direction.squaredNorm();
+
                     // Is the ray inside the cylinder ?
-                    if (distSquared <= (radius * radius))
+                    if (distSquared <= (radiusSquared))
                     {
                         // In this case we must hit at least one of the planes
-                        CORE_ASSERT(vsA || vsB, "Ray must hit at least one of the planes !")
+                        CORE_ASSERT(vsA || vsB, "Ray must hit at least one of the planes !");
 
                         // The most common case is that both plane are hits (i.e. the ray's origin is outside the
                         // cylinder). In that case we just return the smallest hit.
-                        if (LIKELY(hitsA && hitsB))
+                        if (LIKELY(vsA && vsB))
                         {
                             CORE_ASSERT(std::min(hitA, hitB) > 0, "Invalid hit result");
                             hitsOut.push_back(std::min(hitA, hitB));
@@ -205,7 +205,6 @@ i           /// Rer : Graphics Gem IV. http://www.realtimerendering.com/resource
 
                         // If only one plane is hit, then the ray is inside the cylinder, so we return the ray
                         // origin as the result point.
-
                         hitsOut.push_back(0);
                         return true;
 
@@ -215,21 +214,102 @@ i           /// Rer : Graphics Gem IV. http://www.realtimerendering.com/resource
                 }
                 else // Ray not parallel to the cylinder. We compute the ray/infinite cylinder intersection
                 {
-                    bool hitsCyl = (distSquared <= radius*radius)
-                    if (hitsCyl)
-                    {
-                        float cylT = -1.f;
+                    const Scalar ln = n.norm();
+                    n.normalize();
 
-                        // Select the minimal positive t between inf cylinder and the planes.
-                        float minT = std::min(std::min(hitA,hitB), cylT);
-                        
-                        if (minT >= 0) 
+                    // Distance between two skew lines ray and cylinder axis.
+                    const Scalar dist = std::abs(ao.dot(n));
+
+                    // If the distance is bigger than radius, no further processing is needed
+                    // and it's an early exit. If not then an intersection with the infinite
+                    // cylinder is guaranteed.
+                    if (dist <= radius)
+                    {
+                        // TODO : this could be optimized with squared norms.
+                        auto v1 = cylAxis.cross(ao);
+                        const Scalar t = -v1.dot(n) / ln;
+                        auto v2 = n.cross(cylAxis).normalized();
+                        const Scalar s = std::sqrt(radiusSquared - (dist * dist)) / std::abs(r.m_direction.dot(v2));
+
+                        Scalar tIn = t - s;
+                        Scalar tOut = t + s;
+                        CORE_ASSERT(tIn <= tOut," ");
+
+                        // Now clip the ray along planes. (TODO : refactor plane clipping with vsPlane ?)
+
+                        const Scalar ddotAxis = r.m_direction.dot(cylAxis);
+
+                        // Ray has an opposite direction cylinder axis, so it may enter through plane B and exit through
+                        // plane A.
+                        if ( ddotAxis < 0 )
                         {
-                            hitsOut.push_back(minT); 
+                            const Scalar tInPlaneB  = (b - r.m_origin).dot(cylAxis) / ddotAxis;
+                            const Scalar tOutPlaneA = (a - r.m_origin).dot(cylAxis) / ddotAxis;
+
+                            // Early exit condition if the ray misses the capped cylinder
+                            if (tInPlaneB > tOut || tOutPlaneA < tIn)
+                            {
+                                return false;
+                            }
+                            // Cap the in and out
+                            if (tInPlaneB  > tIn && tInPlaneB > tOut)
+                            {
+                                tIn = tInPlaneB;
+                            }
+                            if (tOutPlaneA > tIn && tOutPlaneA > tOut)
+                            {
+                                tOut = tOutPlaneA;
+                            }
+
+                        }
+                        // Ray has the same direction as the cylinder axis it may enter through plane A and exit through B.
+                        else if ( ddotAxis > 0 )
+                        {
+                            const Scalar tInPlaneA  = (a - r.m_origin).dot(cylAxis) / ddotAxis;
+                            const Scalar tOutPlaneB = (b - r.m_origin).dot(cylAxis) / ddotAxis;
+                            // Early exit condition if the ray misses the capped cylinder
+                            if (tInPlaneA > tOut || tOutPlaneB < tIn)
+                            {
+                                return false;
+                            }
+                            // Cap the in and out
+                            if (tInPlaneA  > tIn && tInPlaneA > tOut)
+                            {
+                                tIn = tInPlaneA;
+                            }
+
+                            if (tOutPlaneB > tIn && tOutPlaneB > tOut)
+                            {
+                                tOut = tOutPlaneB;
+                            }
+                        }
+                        // Degenerate case : ray parallel to end planes.
+                        // in that case we check that the ray does not miss the cylinder.
+                        else if (UNLIKELY(ddotAxis == 0))
+                        {
+                            const Scalar h = ao.dot(cylAxis);
+                            if (h < 0 || h*h > cylAxis.squaredNorm() )
+                            {
+                                return false;
+                            }
+                        }
+
+                        // At  this point our tIn and tOut are valid within the capped cylinder.
+                        // The only thing left is to find whether the ray hits (i.e. tIn is positive).
+                        if (tIn > 0)
+                        {
+                            hitsOut.push_back(tIn);
                             return true;
                         }
-                    }
-                }
+                        // tIn is negative but tOut is positive, which means the origin is inside the cylinder,
+                        // so we return 0
+                        else if (tIn * tOut < 0)
+                        {
+                            hitsOut.push_back(0);
+                            return true;
+                        }
+                    } // End if (distance between ray and cyl axis < radius)
+                } // End of else (ray not parallel to the cylinder.
 
                 return false;
             }
