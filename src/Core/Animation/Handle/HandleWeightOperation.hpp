e8cb77a5fba1e80b298a92bcc9d2ee46bc094a48
Maurizio Kovacic <mkovacic@botero.irit.fr>
2015-10-12 19:29:33 +0200

    Added the HandleWeights and operations on them

---
+++ src/Core/Animation/Handle/HandleWeightOperation.hpp
@@ -0,0 +1,31 @@
+#ifndef HANDLE_WEIGHT_OPERATION
+#define HANDLE_WEIGHT_OPERATION
+
+#include "HandleWeight.hpp"
+
+namespace Ra {
+namespace Core {
+namespace Animation {
+
+
+
+/*
+* Return the WeightMatrix extracted from the MeshWeight vector, for a handle with handle_size transforms
+*/
+WeightMatrix extractWeightMatrix( const MeshWeight& weight, const uint handle_size );
+
+
+
+/*
+* Return the MeshWeight from the given WeightMatrix.
+*/
+MeshWeight extractMeshWeight( const WeightMatrix& matrix );
+
+
+
+} // namespace Animation
+} // Namespace Core
+} // Namespace Ra
+
+#endif // HANDLE_WEIGHT_OPERATION
+
