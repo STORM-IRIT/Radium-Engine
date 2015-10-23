f98715b3e81f42824fa8e0ee2b81aec2f74ac676
Maurizio Kovacic <mkovacic@botero.irit.fr>
2015-10-23 16:44:00 +0200

    Added computation of distance between two meshes

---
+++ src/Core/Geometry/Distance/Distance.cpp
@@ -0,0 +1,49 @@
+#include <Core/Geometry/Distance/Distance.hpp>
+
+namespace Ra {
+namespace Core {
+namespace Geometry {
+
+
+
+void vertexDistance( const VectorArray< Vector3 >& v0,
+                     const VectorArray< Vector3 >& v1,
+                     std::vector< Scalar >&        sqrDist,
+                     Scalar&                       sqrMin,
+                     Scalar&                       sqrMax,
+                     Scalar&                       sqrAvg ) {
+    const uint n = v0.size();
+    sqrMax = 0.0;
+    sqrMin = std::numeric_limits< Scalar >::max();
+    sqrDist.resize( n );
+    for( uint i = 0; i < n; ++i ) {
+        sqrDist[i] = ( v0[i] - v1[i] ).squaredNorm();
+        sqrMax = ( sqrDist[i] > sqrMax ) ? sqrDist[i] : sqrMax;
+        sqrMin = ( sqrDist[i] < sqrMin ) ? sqrDist[i] : sqrMin;
+    }
+    sqrAvg = ( sqrMax + sqrMin ) * 0.5;
+}
+
+
+
+void vertexDistance(const VectorArray< Vector3 >& v0,
+                    const VectorArray< Vector3 >& v1,
+                    Scalar&                       sqrMin,
+                    Scalar&                       sqrMax,
+                    Scalar&                       sqrAvg ) {
+    const uint n = v0.size();
+    sqrMax = 0.0;
+    sqrMin = std::numeric_limits< Scalar >::max();
+    for( uint i = 0; i < n; ++i ) {
+        Scalar sqrDist = ( v0[i] - v1[i] ).squaredNorm();
+        sqrMax = ( sqrDist > sqrMax ) ? sqrDist : sqrMax;
+        sqrMin = ( sqrDist < sqrMin ) ? sqrDist : sqrMin;
+    }
+    sqrAvg = ( sqrMax + sqrMin ) * 0.5;
+}
+
+
+
+} // namespace Geometry
+} // namespace Core
+} // namespace Ra
