a9c9f388ef3275c83bcfb09dea98ad567bd5f7ae
Maurizio Kovacic <mkovacic@botero.irit.fr>
2015-10-28 19:05:06 +0100

    Minor optimization to the diffusion function

--- src/Core/Algorithm/Diffusion/Diffusion.cpp
+++ src/Core/Algorithm/Diffusion/Diffusion.cpp
@@ -32,16 +32,18 @@ ScalarValue diffuseDelta( const Geometry::AdjacencyMatrix& A, const Delta& delta
 void diffuseDelta( const Geometry::AdjacencyMatrix& A, const Delta& delta, const Scalar lambda, const uint iteration, ScalarValue& value ) {
     ScalarValue u( delta );
     ScalarValue tmp( delta );
-    unsigned int iter = iteration;
+    uint iter = iteration;
     iter = iter + ( iter % 2 );
     for( uint n = 0; n < iter; ++n ) {
         for( Delta::InnerIterator u_it( u, 0 ); u_it; ++u_it) {
+            uint j = u_it.row();
             Scalar oneRingMeanValue = 0.0;
-            for( Geometry::AdjacencyMatrix::InnerIterator A_it( A, u_it.row() ); A_it; ++A_it) {
-                oneRingMeanValue += u.coeff( A_it.row(), 0 );
+            for( Geometry::AdjacencyMatrix::InnerIterator A_it( A, j ); A_it; ++A_it) {
+                uint i = A_it.row();
+                oneRingMeanValue += u.coeff( i, 0 );
             }
-            oneRingMeanValue /= ( Scalar )A.col( u_it.row() ).nonZeros();
-            tmp.coeffRef( u_it.row(), 0 ) = ( lambda * oneRingMeanValue ) + ( delta.coeff( u_it.row(), 0 ) * ( 1.0 - lambda ) );
+            oneRingMeanValue /= ( Scalar )A.col( j ).nonZeros();
+            tmp.coeffRef( j, 0 ) = ( lambda * oneRingMeanValue ) + ( delta.coeff( j, 0 ) * ( 1.0 - lambda ) );
         }
         u.swap( tmp );
     }
