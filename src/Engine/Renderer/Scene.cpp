ca6b4bc07682522cbc8eecf8ad7af9e5456df62e
Zouch <charly.mourglia@gmail.com>
2015-12-28 03:08:00 +0100

    Removed scene since it does nothing right now

--- src/Engine/Renderer/Scene.cpp
+++
@@ -1,5 +0,0 @@
-#include <Engine/Renderer/Scene.hpp>
-
-#include <Engine/RadiumEngine.hpp>
-#include <Engine/Renderer/RenderObject/RenderObject.hpp>
-#include <Engine/Renderer/RenderObject/RenderObjectManager.hpp>
