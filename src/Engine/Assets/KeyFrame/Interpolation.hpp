81ddba45236abbe3dd9dab71b1027d15c346ee2f
Maurizio Kovacic <mkovacic@botero.irit.fr>
2016-01-14 18:27:53 +0100

    Interpolation functions maintenability improved

--- src/Engine/Assets/KeyFrame/Interpolation.hpp
+++ src/Engine/Assets/KeyFrame/Interpolation.hpp
@@ -2,13 +2,13 @@
 #define RADIUMENGINE_INTERPOLATION_HPP
 
 #include <Core/Math/LinearAlgebra.hpp>
-#include <Engine/Assets/FileData.hpp>
+//#include <Engine/Assets/FileData.hpp>
 
 namespace Ra {
 namespace Core {
 
 inline void interpolate( const Core::Vector3& v0, const Core::Vector3& v1, const Scalar t, Core::Vector3& result ) {
-    result = ( ( t * v0 ) + ( ( 1.0 - t ) * v1 ) );
+    result = ( ( ( 1.0 - t ) * v0 ) + ( t * v1 ) );
 }
 
 inline void interpolate( const Core::Quaternion& q0, const Core::Quaternion& q1, const Scalar t, Core::Quaternion& result ) {
@@ -16,10 +16,16 @@ inline void interpolate( const Core::Quaternion& q0, const Core::Quaternion& q1,
 }
 
 inline void interpolate( const Core::Transform& T0, const Core::Transform& T1, const Scalar t, Core::Transform& result ) {
-    Core::Quaternion q0 = Core::Quaternion( T0.rotation() );
-    Core::Quaternion q1 = Core::Quaternion( T1.rotation() );
-    Core::Quaternion q  = q0.slerp( t, q1 );
-    Core::Vector3    tr = ( ( t * T0.translation() ) + ( ( 1.0 - t ) * T1.translation() ) );
+    Core::Quaternion q;
+    Core::Vector3    tr;
+    interpolate( Core::Quaternion( T0.rotation() ),
+                 Core::Quaternion( T1.rotation() ),
+                 t,
+                 q );
+    interpolate( T0.translation(),
+                 T1.translation(),
+                 t,
+                 tr );
     result.linear()      = q.toRotationMatrix();
     result.translation() = tr;
 }
