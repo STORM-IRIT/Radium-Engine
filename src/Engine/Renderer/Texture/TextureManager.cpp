a4a376c2985d127b3ca46ca33adec8f1a949a0c5
Zouch <charly.mourglia@gmail.com>
2016-03-19 21:49:06 +0100

    Removed freeimage dependency. Not tested yet (since we have no uvs ATM). Fixes #67

--- src/Engine/Renderer/Texture/TextureManager.cpp
+++ src/Engine/Renderer/Texture/TextureManager.cpp
@@ -2,8 +2,8 @@
 
 #include <cstdio>
 
-#include <FreeImage.h>
-
+#define STB_IMAGE_IMPLEMENTATION
+#include <Engine/Renderer/Texture/stb_image.h>
 #include <Engine/Renderer/Texture/Texture.hpp>
 
 namespace Ra
@@ -39,78 +39,30 @@ namespace Ra
         {
             Texture* ret = nullptr;
 
-            FREE_IMAGE_FORMAT fif = FIF_UNKNOWN;
-            FIBITMAP* dib = nullptr; // TODO This name is nonsense, change it
+            int w, h, n;
+            stbi_set_flip_vertically_on_load(true);
 
-            // Find format from file signature
-            fif = FreeImage_GetFileType( filename.c_str(), 0 );
+            unsigned char* data = stbi_load(filename.c_str(), &w, &h, &n, 0);
 
-            if ( FIF_UNKNOWN == fif )
+            if (!data)
             {
-                // Find format from file extension
-                fif = FreeImage_GetFIFFromFilename( filename.c_str() );
-            }
-
-            if ( FIF_UNKNOWN == fif )
-            {
-                // Still unknown
-                std::string error = "Cannot determine " + filename + " image format.";
-                LOG( logERROR ) << error;
+                LOG(logERROR) << "Something went wrong when loading image \"" << filename << "\".";
                 return nullptr;
             }
 
-            if ( FreeImage_FIFSupportsReading( fif ) )
+            GLuint format;
+            switch(n)
             {
-                dib = FreeImage_Load( fif, filename.c_str() );
+                case 1: format = GL_RED;    break;
+                case 2: format = GL_RG;     break;
+                case 3: format = GL_RGB;    break;
+                case 4: format = GL_RGBA;   break;
             }
-
-            std::string error = "Something went wrong while trying to load " + filename + ".";
-
-            if ( nullptr == dib )
-            {
-                LOG( logERROR ) << error;
-                return nullptr;
-            }
-
-            ret = new Texture( filename, GL_TEXTURE_2D );
-            unsigned char* data = FreeImage_GetBits( dib );
-
-            int bpp = FreeImage_GetBPP( dib );
-            int format = 0, internal = 0;
-            switch ( bpp )
-            {
-                case 8:
-                {
-                    // FIXME(Charly): Debug this
-                    format = GL_RED;
-                    internal = GL_RED;
-                } break;
-
-                case 24:
-                {
-                    format = GL_BGR;
-                    internal = GL_RGB;
-                } break;
-
-                case 32:
-                {
-                    format = GL_BGRA;
-                    internal = GL_RGBA;
-                } break;
-
-                default:
-                {
-                    LOG( logERROR ) << "Unknow bytes per pixel format : " << bpp;
-                }
-            }
-
-            int w = FreeImage_GetWidth( dib );
-            int h = FreeImage_GetHeight( dib );
 
             if ( m_verbose )
             {
                 LOG( logINFO ) << "Image stats (" << filename << ") :\n"
-                               << "\tBPP    : 0x" << std::hex << bpp << std::dec << std::endl
+                               << "\tPixels : " << n << std::endl
                                << "\tFormat : 0x" << std::hex << format << std::dec << std::endl
                                << "\tSize   : " << w << ", " << h;
             }
@@ -118,12 +70,12 @@ namespace Ra
 
             CORE_ASSERT( data, "Data is null" );
 
-            ret->initGL( internal, w, h, format, GL_UNSIGNED_BYTE, data );
+            ret->initGL( format, w, h, format, GL_UNSIGNED_BYTE, data );
             ret->genMipmap( GL_LINEAR, GL_LINEAR );
 
             m_textures.insert( TexturePair( filename, ret ) );
 
-            FreeImage_Unload( dib );
+            stbi_image_free(data);
 
             return ret;
         }
