90949c01031d8d738aca87f9f3082c03227570dc
Maurizio Kovacic <mkovacic@botero.irit.fr>
2016-05-18 14:21:23 +0200

    New checks on weightmatrix and partition of unity computation

--- src/Core/Animation/Handle/HandleWeightOperation.cpp
+++ src/Core/Animation/Handle/HandleWeightOperation.cpp
@@ -3,142 +3,179 @@
 #include <utility>
 #include <Core/Log/Log.hpp>
 
-namespace Ra
+namespace Ra {
+namespace Core {
+namespace Animation {
+
+
+WeightMatrix extractWeightMatrix(const MeshWeight& weight, const uint weight_size)
 {
-    namespace Core
+    WeightMatrix W(weight.size(), weight_size);
+    W.setZero();
+    for (uint i = 0; i < weight.size(); ++i)
     {
-        namespace Animation
+        for (const auto& w : weight[i])
         {
+            W.coeffRef(i, w.first) = w.second;
+        }
+    }
+    return W;
+}
 
 
-            WeightMatrix extractWeightMatrix(const MeshWeight& weight, const uint weight_size)
+MeshWeight extractMeshWeight(const WeightMatrix& matrix)
+{
+    MeshWeight W(matrix.rows());
+    for (int i = 0; i < matrix.rows(); ++i)
+    {
+        for (int j = 0; j < matrix.cols(); ++j)
+        {
+            if (matrix.coeff(i, j) != 0.0)
             {
-                WeightMatrix W(weight.size(), weight_size);
-                W.setZero();
-                for (uint i = 0; i < weight.size(); ++i)
-                {
-                    for (const auto& w : weight[i])
-                    {
-                        W.coeffRef(i, w.first) = w.second;
+                std::pair<uint, Scalar> w(uint(j), matrix.coeff(i, j));
+                W[i].push_back(w);
+            }
+        }
+    }
+    return W;
+}
+
+
+
+WeightMatrix partitionOfUnity( const WeightMatrix& weights ) {
+    WeightMatrix W( weights.rows(), weights.cols() );
+    W.reserve( weights.nonZeros() );
+    Ra::Core::Vector1Array norm( weights.rows() );
+    #pragma omp parallel for
+    for( uint i = 0; i < weights.rows(); ++i ) {
+        Ra::Core::VectorN row = weights.row( i );
+        norm[i] = row.lpNorm<1>();
+        norm[i] = ( norm[i] == 0.0 ) ? 1.0 : norm[i];
+    }
+    #pragma omp parallel for
+    for( uint k = 0; k < weights.outerSize(); ++k ) {
+        for( WeightMatrix::InnerIterator it( weights, k ); it; ++it ) {
+            const uint   i = it.row();
+            const uint   j = it.col();
+            const Scalar w = it.value();
+            W.coeffRef( i, j ) = w / norm[i];
+        }
+    }
+    return W;
+}
+
+
+uint getMaxWeightIndex( const WeightMatrix& weights, const uint vertexID ) {
+    uint maxId = uint( -1 );
+    VectorN row = weights.row( vertexID );
+    row.maxCoeff(&maxId);
+    return maxId;
+}
+
+
+
+void getMaxWeightIndex( const WeightMatrix& weights, std::vector< uint >& handleID ) {
+    handleID.resize( weights.rows() );
+    for( int i = 0; i < weights.rows(); ++i ) {
+        handleID[i] = getMaxWeightIndex( weights, i );
+    }
+}
+
+
+
+void checkWeightMatrix( const WeightMatrix& matrix, const bool FAIL_ON_ASSERT, const bool MT ) {
+    if( check_NAN( matrix, FAIL_ON_ASSERT, MT ) ) {
+        LOG( logDEBUG ) << "Matrix is good.";
+    } else {
+        LOG( logDEBUG ) << "Matrix is not good.";
+    }
+
+    if( check_NoWeightVertex( matrix, FAIL_ON_ASSERT, MT ) ) {
+        LOG( logDEBUG ) << "Matrix is good.";
+    } else {
+        LOG( logDEBUG ) << "Matrix is not good.";
+    }
+}
+
+
+
+bool RA_CORE_API check_NAN( const WeightMatrix& matrix, const bool FAIL_ON_ASSERT, const bool MT ) {
+    bool status = true;
+    LOG( logDEBUG ) << "Searching for nans in the matrix...";
+    if( MT ) {
+        #pragma omp parallel for
+        for( int k = 0; k < matrix.outerSize(); ++k ) {
+            for( WeightMatrix::InnerIterator it( matrix, k ); it; ++it ) {
+                const Scalar      value = it.value();
+                const bool        check = isnan( value );
+                #pragma omp atomic
+                status &= check;
+            }
+        }
+        if( !status ) {
+            if( FAIL_ON_ASSERT ) {
+                CORE_ASSERT( status, "At least an element is nan" );
+            } else {
+                LOG( logDEBUG ) << "At least an element is nan";
+            }
+        }
+    } else {
+        for( int k = 0; k < matrix.outerSize(); ++k ) {
+            for( WeightMatrix::InnerIterator it( matrix, k ); it; ++it ) {
+                const uint        i     = it.row();
+                const uint        j     = it.col();
+                const Scalar      value = it.value();
+                const std::string text  = "Element (" + std::to_string( i ) + "," + std::to_string(j) + ") is nan.";
+                if( FAIL_ON_ASSERT ) {
+                    CORE_ASSERT( !isnan( value ), text.c_str() );
+                } else {
+                    if( isnan( value ) ) {
+                        LOG( logDEBUG ) << text;
+                        status = false;
                     }
                 }
-                return W;
             }
+        }
+    }
+    return status;
+}
 
-
-            MeshWeight extractMeshWeight(const WeightMatrix& matrix)
-            {
-                MeshWeight W(matrix.rows());
-                for (int i = 0; i < matrix.rows(); ++i)
-                {
-                    for (int j = 0; j < matrix.cols(); ++j)
-                    {
-                        if (matrix.coeff(i, j) != 0.0)
-                        {
-                            std::pair<uint, Scalar> w(uint(j), matrix.coeff(i, j));
-                            W[i].push_back(w);
-                        }
-                    }
-                }
-                return W;
+bool RA_CORE_API check_NoWeightVertex( const WeightMatrix& matrix, const bool FAIL_ON_ASSERT, const bool MT ) {
+    bool status = true;
+    LOG( logDEBUG ) << "Searching for empty rows in the matrix...";
+    if( MT ) {
+        #pragma omp parallel for
+        for( int i = 0; i < matrix.rows(); ++i ) {
+            Sparse row = matrix.row( i );
+            const bool check = ( row.nonZeros() == 0 );
+            #pragma omp atomic
+            status &= check;
+        }
+        if( !status ) {
+            if( FAIL_ON_ASSERT ) {
+                CORE_ASSERT( status, "At least a vertex as no weights" );
+            } else {
+                LOG( logDEBUG ) << "At least a vertex as no weights";
             }
-
-
-            uint getMaxWeightIndex(const WeightMatrix& weights, const uint vertexID)
-            {
-                uint maxId  = uint(-1);
-                VectorN row = weights.row(vertexID);
-                row.maxCoeff(&maxId);
-                return maxId;
-            }
-
-
-            void getMaxWeightIndex(const WeightMatrix& weights, std::vector<uint>& handleID)
-            {
-                handleID.resize(weights.rows());
-                for (int i = 0; i < weights.rows(); ++i)
-                {
-                    handleID[i] = getMaxWeightIndex(weights, i);
+        }
+    } else {
+        for( int i = 0; i < matrix.rows(); ++i ) {
+            Sparse row = matrix.row( i );
+            if( row.nonZeros() == 0 ) {
+                const std::string text = "Vertex " + std::to_string( i ) + " has no weights.";
+                if( FAIL_ON_ASSERT ) {
+                    CORE_ASSERT( false, text.c_str() );
+                } else {
+                    status = false;
+                    LOG( logDEBUG ) << text;
                 }
             }
+        }
+    }
+    return status;
+}
 
 
-            void checkWeightMatrix(const WeightMatrix& matrix, const bool FAIL_ON_ASSERT)
-            {
-                if (check_NAN(matrix, FAIL_ON_ASSERT))
-                {
-                    LOG(logDEBUG) << "Matrix is good.";
-                }
-                else
-                {
-                    LOG(logDEBUG) << "Matrix is not good.";
-                }
-
-                if (check_NoWeightVertex(matrix, FAIL_ON_ASSERT))
-                {
-                    LOG(logDEBUG) << "Matrix is good.";
-                }
-                else
-                {
-                    LOG(logDEBUG) << "Matrix is not good.";
-                }
-            }
-
-
-            bool RA_CORE_API check_NAN(const WeightMatrix& matrix, const bool FAIL_ON_ASSERT)
-            {
-                bool status = true;
-                LOG(logDEBUG) << "Searching for nans in the matrix...";
-                for (int k = 0; k < matrix.outerSize(); ++k)
-                {
-                    for (WeightMatrix::InnerIterator it(matrix, k); it; ++it)
-                    {
-                        const uint i           = it.row();
-                        const uint j           = it.col();
-                        const Scalar value     = it.value();
-                        const std::string text = "Element (" + std::to_string(i) + "," + std::to_string(j) + ") is nan.";
-                        if (FAIL_ON_ASSERT)
-                        {
-                            CORE_ASSERT(!std::isnan(value), text.c_str());
-                        }
-                        else
-                        {
-                            if (std::isnan(value))
-                            {
-                                LOG(logDEBUG) << text;
-                                status = false;
-                            }
-                        }
-                    }
-                }
-                return status;
-            }
-
-            bool RA_CORE_API check_NoWeightVertex(const WeightMatrix& matrix, const bool FAIL_ON_ASSERT)
-            {
-                bool status = true;
-                LOG(logDEBUG) << "Searching for empty rows in the matrix...";
-                for (int i = 0; i < matrix.rows(); ++i)
-                {
-                    Sparse row = matrix.row(i);
-                    if (row.nonZeros() == 0)
-                    {
-                        const std::string text = "Vertex " + std::to_string(i) + " has no weights.";
-                        if (FAIL_ON_ASSERT)
-                        {
-                            CORE_ASSERT(false, text.c_str());
-                        }
-                        else
-                        {
-                            status = false;
-                            LOG(logDEBUG) << text;
-                        }
-                    }
-                }
-                return status;
-            }
-
-
-        } // namespace Animation
-    }     // Namespace Core
+} // namespace Animation
+} // Namespace Core
 } // Namespace Ra
--- src/Core/Animation/Handle/HandleWeightOperation.hpp
+++ src/Core/Animation/Handle/HandleWeightOperation.hpp
@@ -20,6 +20,17 @@ WeightMatrix RA_CORE_API extractWeightMatrix( const MeshWeight& weight, const ui
 MeshWeight RA_CORE_API extractMeshWeight( const WeightMatrix& matrix );
 
 
+
+/*
+* Return the WeightMatrix holding the partition of unity property.
+* This is obtained by normalizing each row by its l1-norm, assuming:
+*       weights( i, j ) >= 0
+*       lpNorm1( weights.row( i ) ) > 0
+*/
+WeightMatrix RA_CORE_API partitionOfUnity( const WeightMatrix& weights );
+
+
+
 /*
 * Return the index of the weight that influence the most the position of vertex at vertexId.
 */
@@ -34,11 +45,11 @@ void RA_CORE_API getMaxWeightIndex( const WeightMatrix& weights, std::vector< ui
 
 
 
-void RA_CORE_API checkWeightMatrix( const WeightMatrix& matrix, const bool FAIL_ON_ASSERT = false );
+void RA_CORE_API checkWeightMatrix( const WeightMatrix& matrix, const bool FAIL_ON_ASSERT = false, const bool MT = false );
 
-bool RA_CORE_API check_NAN( const WeightMatrix& matrix, const bool FAIL_ON_ASSERT = false );
+bool RA_CORE_API check_NAN( const WeightMatrix& matrix, const bool FAIL_ON_ASSERT = false, const bool MT = false );
 
-bool RA_CORE_API check_NoWeightVertex( const WeightMatrix& matrix, const bool FAIL_ON_ASSERT = false );
+bool RA_CORE_API check_NoWeightVertex( const WeightMatrix& matrix, const bool FAIL_ON_ASSERT = false, const bool MT = false );
 
 
 
