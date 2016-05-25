0ee9891ecad6f3853670683d04e64c03a444d9d8
Maurizio Kovacic <mkovacic@botero.irit.fr>
2016-05-25 16:47:31 +0200

    Improved the MT version of DQS

--- src/Core/Animation/Skinning/DualQuaternionSkinning.cpp
+++ src/Core/Animation/Skinning/DualQuaternionSkinning.cpp
@@ -7,31 +7,40 @@ namespace Animation {
 void computeDQ( const Pose& pose, const WeightMatrix& weight, DQList& DQ ) {
     DQ.clear();
     DQ.resize( weight.rows(), DualQuaternion( Quaternion( 0, 0, 0, 0 ), Quaternion( 0, 0, 0, 0 ) ) );
-    #pragma omp parallel for
-        for( uint k = 0; k < weight.outerSize(); ++k ) {
-            DualQuaternion q( pose[k] );
-            for( WeightMatrix::InnerIterator it( weight, k ); it; ++it ) {
-                const uint   i = it.row();
-                const Scalar w = it.value();
-                const auto wq = q * w;
-    #pragma omp critical
-                {
-                    DQ[i] += wq;
-                }
+    //#pragma omp parallel for
+    for( uint k = 0; k < weight.outerSize(); ++k ) {
+        const DualQuaternion q( pose[k] );
+        const uint nonZero = weight.col( k ).nonZeros();
+        omp_set_dynamic(0);
+        #pragma omp parallel for schedule( static ) num_threads(4)
+        for( uint nz = 0; nz < nonZero; ++nz ) {
+            WeightMatrix::InnerIterator it( weight, k );
+            for( uint j = 0; j < nz; ++j ) {
+                ++it;
+            }
+            const uint   i  = it.row();
+            const Scalar w  = it.value();
+            const auto   wq = q * w;
+            //#pragma omp critical
+            {
+                DQ[i] += wq;
             }
         }
+    }
 
-        // Normalize all dual quats.
-    #pragma omp parallel for
-        for( uint i = 0; i < DQ.size() ; ++i) {
-            DQ[i].normalize();
-        }
+    // Normalize all dual quats.
+    omp_set_dynamic(0);
+    #pragma omp parallel for schedule( static ) num_threads(4)
+    for( uint i = 0; i < DQ.size() ; ++i) {
+        DQ[i].normalize();
+    }
 }
 
 void DualQuaternionSkinning( const Vector3Array& input, const DQList& DQ, Vector3Array& output ) {
     const uint size = input.size();
     output.resize( size );
-#pragma omp parallel for
+    omp_set_dynamic(0);
+    #pragma omp parallel for schedule( static ) num_threads(4)
     for( uint i = 0; i < size; ++i ) {
         output[i] = DQ[i].transform( input[i] );
     }
