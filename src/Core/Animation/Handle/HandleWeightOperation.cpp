STORM-IRIT/Radium-Engine

commit 2ebb858bcd839d5263dce7c5676208e98293e15c

OpenMP build option and windows build fixes
Note : openmp is really bad on windows but it is now supported.
it requires using only signed int in for loops and unary operators
on ints for atomics.

src/Core/Animation/Handle/HandleWeightOperation.cpp
@@ -47,13 +47,13 @@ WeightMatrix partitionOfUnity( const WeightMatrix& weights ) {
     W.reserve( weights.nonZeros() );
     Ra::Core::Vector1Array norm( weights.rows() );
     #pragma omp parallel for
-    for( uint i = 0; i < weights.rows(); ++i ) {
+    for( int i = 0; i < weights.rows(); ++i ) {
         Ra::Core::VectorN row = weights.row( i );
         norm[i] = row.lpNorm<1>();
         norm[i] = ( norm[i] == 0.0 ) ? 1.0 : norm[i];
     }
     #pragma omp parallel for
-    for( uint k = 0; k < weights.outerSize(); ++k ) {
+    for( int k = 0; k < weights.outerSize(); ++k ) {
         for( WeightMatrix::InnerIterator it( weights, k ); it; ++it ) {
             const uint   i = it.row();
             const uint   j = it.col();
@@ -100,19 +100,19 @@ void checkWeightMatrix( const WeightMatrix& matrix, const bool FAIL_ON_ASSERT, c
 
 
 bool RA_CORE_API check_NAN( const WeightMatrix& matrix, const bool FAIL_ON_ASSERT, const bool MT ) {
-    bool status = true;
+    int status = 0;
     LOG( logDEBUG ) << "Searching for nans in the matrix...";
     if( MT ) {
         #pragma omp parallel for
         for( int k = 0; k < matrix.outerSize(); ++k ) {
             for( WeightMatrix::InnerIterator it( matrix, k ); it; ++it ) {
                 const Scalar      value = it.value();
-                const bool        check = isnan( value );
+                const bool check = isnan( value );
                 #pragma omp atomic
-                status &= check;
+                status += check ;
             }
         }
-        if( !status ) {
+        if( status > 0 ) {
             if( FAIL_ON_ASSERT ) {
                 CORE_ASSERT( status, "At least an element is nan" );
             } else {
@@ -137,21 +137,21 @@ bool RA_CORE_API check_NAN( const WeightMatrix& matrix, const bool FAIL_ON_ASSER
             }
         }
     }
-    return status;
+    return status != 0;
 }
 
 bool RA_CORE_API check_NoWeightVertex( const WeightMatrix& matrix, const bool FAIL_ON_ASSERT, const bool MT ) {
-    bool status = true;
+    int status = 0;
     LOG( logDEBUG ) << "Searching for empty rows in the matrix...";
     if( MT ) {
         #pragma omp parallel for
         for( int i = 0; i < matrix.rows(); ++i ) {
             Sparse row = matrix.row( i );
-            const bool check = ( row.nonZeros() == 0 );
+            const int check = row.size() - row.nonZeros();
             #pragma omp atomic
-            status &= check;
+            status += check;
         }
-        if( !status ) {
+        if( status > 0 ) {
             if( FAIL_ON_ASSERT ) {
                 CORE_ASSERT( status, "At least a vertex as no weights" );
             } else {
@@ -172,7 +172,7 @@ bool RA_CORE_API check_NoWeightVertex( const WeightMatrix& matrix, const bool FA
             }
         }
     }
-    return status;
+    return status != 0;
 }
 
 

CMakeLists.txt
@@ -2,72 +2,21 @@ project(RadiumEngine)
 
 cmake_minimum_required(VERSION 2.8.11)
 
-#if ((${CMAKE_CXX_COMPILER_ID} STREQUAL "Clang"))
-if (APPLE)
-    # No openmp on MacosX Clang (TODO, find better compiler identification)
-    set(CMAKE_CXX_FLAGS                "-Wall -Wextra  -std=c++1y -msse3 -Wno-sign-compare -Wno-unused-parameter -fno-exceptions ${CMAKE_CXX_FLAGS}")
-    set(CMAKE_CXX_FLAGS_DEBUG          "-D_DEBUG -DCORE_DEBUG -g3 -ggdb ${CMAKE_CXX_FLAGS_DEBUG}")
-    set(CMAKE_CXX_FLAGS_RELEASE        "-DNDEBUG -O3 -ffast-math -mfpmath=sse")
-    set(CMAKE_CXX_FLAGS_RELWITHDEBINFO "-g3 ${CMAKE_CXX_FLAGS_RELEASE}")
+# CMake setups
+set(CMAKE_MODULE_PATH ${CMAKE_SOURCE_DIR}/CMakeModules)
+set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} ${CMAKE_SOURCE_DIR}/cmake)
 
-    add_definitions( -Wno-deprecated-declarations ) # Do not warn for eigen bind being deprecated
-elseif (UNIX)
-    if ((${CMAKE_CXX_COMPILER_ID} STREQUAL "Clang"))
-        set(OMP_FLAG "-fopenmp=libiomp5")
-        set(MATH_FLAG "-mfpmath=sse")
-    else()
-        set(OMP_FLAG "-fopenmp -ftree-vectorize")
-        set(MATH_FLAG "-mfpmath=sse -ffast-math")
-    endif()
+# Build options options.
+set( USE_DOUBLE False CACHE BOOL "Use double precision" )
 
-    set(CMAKE_CXX_FLAGS                "-Wall -Wextra  -pthread -std=c++1y -msse3 -Wno-sign-compare -Wno-unused-parameter -fno-exceptions ${OMP_FLAG} ${CMAKE_CXX_FLAGS}")
-    set(CMAKE_CXX_FLAGS_DEBUG          "-D_DEBUG -DCORE_DEBUG -g3 -ggdb ${CMAKE_CXX_FLAGS_DEBUG}")
-    set(CMAKE_CXX_FLAGS_RELEASE        "-DNDEBUG -O2 ${MATH_FLAG}")
-    set(CMAKE_CXX_FLAGS_RELWITHDEBINFO "-g3 ${CMAKE_CXX_FLAGS_RELEASE}")
-
-  add_definitions( -Wno-deprecated-declarations ) # Do not warn for eigen bind being deprecated
-elseif (MSVC)
-    # Visual studio flags breakdown
-    # /GR- : no rtti ; /Ehs-c- : no exceptions
-    # /Od  : disable optimization
-    # /Ox :  maximum optimization
-    # /GL : enable link time optimization
-    # /Zi  : generate debug info
-    #remove exceptions from default args
-    add_definitions(-D_HAS_EXCEPTIONS=0)
-    string (REGEX REPLACE "/EHsc *" "" CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}")
-    string (REGEX REPLACE "/GR" ""     CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}")
-
-    set(CMAKE_CXX_FLAGS	               "/arch:AVX2 /GR- /EHs-c- /MP ${CMAKE_CXX_FLAGS}")
-    set(CMAKE_CXX_FLAGS_DEBUG		   "/D_DEBUG /DCORE_DEBUG /Od /Zi ${CMAKE_CXX_FLAGS_DEBUG} /MDd")
-    set(CMAKE_CXX_FLAGS_RELEASE		   "/DNDEBUG /Ox /fp:fast /GL /MT")
-    set(CMAKE_CXX_FLAGS_RELWITHDEBINFO "/Zi ${CMAKE_CXX_FLAGS_RELEASE}")
-endif()
+# Set the compiler flags.
+include(CompileFlags)
 
 # Problem with Qt linking
 # FIXME(Charly): Not sure if this should be done on Linux
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -DQT_COMPILING_QSTRING_COMPAT_CPP")
 
-set( USE_DOUBLE False CACHE BOOL "Use double precision" )
-if ("${USE_DOUBLE}" STREQUAL "True")
-  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -DCORE_USE_DOUBLE")
-  message("Compile using double precision.")
-else()
-  message("Compile using single precision.")
-endif()
 
-# CMake setups
-set(CMAKE_MODULE_PATH ${CMAKE_SOURCE_DIR}/CMakeModules)
-
-# Debug by default !
-if ( NOT CMAKE_BUILD_TYPE )
-  set( CMAKE_BUILD_TYPE Debug )
-endif()
-
-set( VALID_CMAKE_BUILD_TYPES "Debug Release RelWithDebInfo" )
-if ( NOT "${VALID_CMAKE_BUILD_TYPES}" MATCHES ${CMAKE_BUILD_TYPE} )
-    set( CMAKE_BUILD_TYPE Debug )
-endif()
 
 set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_SOURCE_DIR}/bin)
 set(EXECUTABLE_OUTPUT_PATH         ${CMAKE_SOURCE_DIR}/bin)
@@ -105,12 +54,9 @@ if (MSVC OR MSVC_IDE)
     endif()
 endif(MSVC OR MSVC_IDE)
 
-set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} ${CMAKE_SOURCE_DIR}/cmake)
-
 # EXTERNALS
 # TODO Move this in another cmake config file
 set(EXTERNAL_DIR ${CMAKE_SOURCE_DIR}/3rdPartyLibraries)
-
 add_subdirectory(3rdPartyLibraries)
 
 # Eigen
@@ -159,8 +105,6 @@ if (${LOAD_TEXTURES})
     message(STATUS "Textures will be loaded")
     add_definitions(-DLOAD_TEXTURES)
 endif()
-
-add_definitions(-Wall)
 
 add_subdirectory(src)
 

CMakeModules/CompileFlags.cmake
@@ -1,0 +1,92 @@
+# This file define common compile flags for all Radium projects.
+
+# Compilation flag for each platforms =========================================
+
+if (APPLE)
+    # No openmp on MacosX Clang (TODO, find better compiler identification)
+    set(CMAKE_CXX_FLAGS                "-Wall -Wextra  -std=c++1y -msse3 -Wno-sign-compare -Wno-unused-parameter -fno-exceptions ${CMAKE_CXX_FLAGS}")
+    set(CMAKE_CXX_FLAGS_DEBUG          "-D_DEBUG -DCORE_DEBUG -g3 -ggdb ${CMAKE_CXX_FLAGS_DEBUG}")
+    set(CMAKE_CXX_FLAGS_RELEASE        "-DNDEBUG -O3 -ffast-math -mfpmath=sse")
+    set(CMAKE_CXX_FLAGS_RELWITHDEBINFO "-g3 ${CMAKE_CXX_FLAGS_RELEASE}")
+
+    add_definitions( -Wno-deprecated-declarations ) # Do not warn for eigen bind being deprecated
+elseif (UNIX)
+    if ((${CMAKE_CXX_COMPILER_ID} STREQUAL "Clang"))
+        set(OMP_FLAG "-fopenmp=libiomp5")
+        set(MATH_FLAG "-mfpmath=sse")
+    else()
+        set(OMP_FLAG "-fopenmp -ftree-vectorize")
+        set(MATH_FLAG "-mfpmath=sse -ffast-math")
+    endif()
+
+    if ("${USE_OMP}" STREQUAL "False")
+        set (OMP_FLAG "")
+    endif()
+
+    set(CMAKE_CXX_FLAGS                "-Wall -Wextra  -pthread -std=c++1y -msse3 -Wno-sign-compare -Wno-unused-parameter -fno-exceptions ${OMP_FLAG} ${CMAKE_CXX_FLAGS}")
+    set(CMAKE_CXX_FLAGS_DEBUG          "-D_DEBUG -DCORE_DEBUG -g3 -ggdb ${CMAKE_CXX_FLAGS_DEBUG}")
+    set(CMAKE_CXX_FLAGS_RELEASE        "-DNDEBUG -O2 ${MATH_FLAG}")
+    set(CMAKE_CXX_FLAGS_RELWITHDEBINFO "-g3 -ggdb ${CMAKE_CXX_FLAGS_RELEASE}")
+
+  add_definitions( -Wno-deprecated-declarations ) # Do not warn for eigen bind being deprecated
+elseif (MSVC)
+    # Visual studio flags breakdown
+    # /GR- : no rtti ; /Ehs-c- : no exceptions
+    # /Od  : disable optimization
+    # /Ox :  maximum optimization
+    # /GL : enable link time optimization
+    # /Zi  : generate debug info
+
+    #remove exceptions from default args
+    add_definitions(-D_HAS_EXCEPTIONS=0)
+    string (REGEX REPLACE "/EHsc *" "" CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}")
+    string (REGEX REPLACE "/GR" ""     CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}")
+
+
+    if ("${USE_OMP}" STREQUAL "True")
+        set (OMP_FLAG "/openmp")
+    else()
+        set (OMP_FLAG "")
+    endif()
+
+    set(CMAKE_CXX_FLAGS	               "/arch:AVX2 /GR- /EHs-c- /MP ${OMP_FLAG} ${CMAKE_CXX_FLAGS}")
+    set(CMAKE_CXX_FLAGS_DEBUG		   "/D_DEBUG /DCORE_DEBUG /Od /Zi ${CMAKE_CXX_FLAGS_DEBUG} /MDd")
+    set(CMAKE_CXX_FLAGS_RELEASE		   "/DNDEBUG /Ox /fp:fast /GL /MT")
+    set(CMAKE_CXX_FLAGS_RELWITHDEBINFO "/Zi ${CMAKE_CXX_FLAGS_RELEASE}")
+endif()
+
+# Additional flags depending on build options =================================
+
+if ("${USE_DOUBLE}" STREQUAL "True")
+  add_definitions(-DCORE_USE_DOUBLE)
+  message("Using double precision.")
+else()
+  message("Using single precision.")
+endif()
+
+if ("${USE_OMP}" STREQUAL "True")
+    add_definitions(-DCORE_USE_OMP)
+    message("Using OpenMP")
+else()
+    message("OpenMP disabled")
+endif()
+
+if (CMAKE_SIZEOF_VOID_P EQUAL 8)
+    message("64 bits build")
+else()
+    message("32 bits build")
+endif()
+
+
+# Set build configurations ====================================================
+
+# Debug by default
+if ( NOT CMAKE_BUILD_TYPE )
+  set( CMAKE_BUILD_TYPE Debug )
+endif()
+
+set( VALID_CMAKE_BUILD_TYPES "Debug Release RelWithDebInfo" )
+if ( NOT "${VALID_CMAKE_BUILD_TYPES}" MATCHES ${CMAKE_BUILD_TYPE} )
+    set( CMAKE_BUILD_TYPE Debug )
+endif()
+

CMakeModules/FindGLEW.cmake
@@ -8,6 +8,12 @@
 # GLEW_LIBRARY
 #
 
+if (CMAKE_SIZEOF_VOID_P EQUAL 8)
+    set( WIN_FOLDER_NAME "x64")
+else()
+    set( WIN_FOLDER_NAME "Win32")
+endif()
+
 FIND_PATH( GLEW_INCLUDE_DIR GL/glew.h
 		${CMAKE_SOURCE_DIR}/3rdPartyLibraries/Glew/include
 		/usr/include
@@ -19,7 +25,7 @@ FIND_PATH( GLEW_INCLUDE_DIR GL/glew.h
 FIND_LIBRARY( GLEW_LIBRARY
 		NAMES GLEW glew glew32.lib
 		PATHS
-		${CMAKE_SOURCE_DIR}/3rdPartyLibraries/Glew/lib/Release/Win32
+        ${CMAKE_SOURCE_DIR}/3rdPartyLibraries/Glew/lib/Release/${WIN_FOLDER_NAME}
 		/usr/lib64
 		/usr/lib
 		/usr/local/lib64

Plugins/Skinning/src/Display/SkinningDisplayComponent.hpp
@@ -62,7 +62,7 @@ public:
 
             std::vector< uint > partition( size );
             #pragma omp parallel for
-            for( uint i = 0; i < size; ++i ) {
+            for( int i = 0; i < int(size); ++i ) {
                 uint ID;
                 Ra::Core::VectorN row = weights.row( i );
                 row.maxCoeff( &ID );
@@ -106,7 +106,7 @@ public:
 
             Ra::Core::Vector4Array color( size, Ra::Core::Vector4::Zero() );
             #pragma omp parallel for
-            for( uint i = 0; i < size; ++i ) {
+            for( int i = 0; i < int(size); ++i ) {
                 color[i] = palette[ assignedColor[ partition[i] ] ];
             }
 

Plugins/Skinning/src/DualQuaternionSkinning.hpp
@@ -38,7 +38,7 @@ inline void computeDQ( const Pose& pose, const WeightMatrix& weight, DQList& DQ 
 
         // Normalize all dual quats.
     #pragma omp parallel for
-        for(uint i = 0; i < DQ.size() ; ++i) {
+        for(int i = 0; i < int(DQ.size()) ; ++i) {
             DQ[i].normalize();
         }
 }
@@ -47,7 +47,7 @@ inline void DualQuaternionSkinning( const VertexList& input, const DQList& DQ, V
     const uint size = input.size();
     output.resize( size );
 #pragma omp parallel for
-    for( uint i = 0; i < size; ++i ) {
+    for( int i = 0; i < int(size); ++i ) {
         output[i] = DQ[i].transform( input[i] );
     }
 }

src/Core/Algorithm/Delta/Delta.cpp
@@ -24,7 +24,7 @@ void delta( const BitSet& bit,
             const Scalar& default_value ) {
     Delta d( bit.size(), 1 );
 #pragma omp parallel for
-    for( uint i = 0; i < bit.size(); ++i ) {
+    for( int i = 0; i < int(bit.size()); ++i ) {
         if( bit[i]) {
 #pragma omp critical
 {

src/Core/Animation/Pose/PoseOperation.cpp
@@ -17,7 +17,7 @@ Pose relativePose( const Pose& modelPose, const RestPose& restPose )  {
     CORE_ASSERT( compatible( modelPose, restPose ), " Poses with different size " );
     Pose T( restPose.size() );
     #pragma omp parallel for
-    for( uint i = 0; i < T.size(); ++i ) {
+    for( int i = 0; i < int(T.size()); ++i ) {
         T[i] = modelPose[i] * restPose[i].inverse( Eigen::Affine );
     }
     return T;
@@ -28,7 +28,7 @@ Pose relativePose( const Pose& modelPose, const RestPose& restPose )  {
 Pose applyTransformation(const Pose& pose, const AlignedStdVector<Transform> &transform ) {
     Pose T( std::min( pose.size(), transform.size() ) );
     #pragma omp parallel for
-    for( uint i = 0; i < T.size(); ++i ) {
+    for( int i = 0; i < int(T.size()); ++i ) {
         T[i] = transform[i] * pose[i];
     }
     return T;
@@ -39,7 +39,7 @@ Pose applyTransformation(const Pose& pose, const AlignedStdVector<Transform> &tr
 Pose applyTransformation( const Pose& pose, const Transform& transform ) {
     Pose T( pose.size() );
     #pragma omp parallel for
-    for( uint i = 0; i < T.size(); ++i ) {
+    for( int i = 0; i < int(T.size()); ++i ) {
         T[i] = transform * pose[i];
     }
     return T;
@@ -66,7 +66,7 @@ Pose interpolatePoses(const Pose& a, const Pose& b, const Scalar t ) {
     Pose interpolatedPose( size );
 
 #pragma omp parallel for
-    for ( uint i = 0; i < size; ++i ) {
+    for ( int i = 0; i < int(size); ++i ) {
         // interpolate between the transforms
         Ra::Core::Transform aTransform = a[i];
         Ra::Core::Transform bTransform = b[i];

commit 6bb804e79cff9fd12ee3e5310adce59a72b9446f

FIXED some bugs introduced by 2ebb858bc

src/Core/Animation/Handle/HandleWeightOperation.cpp
@@ -100,21 +100,21 @@ void checkWeightMatrix( const WeightMatrix& matrix, const bool FAIL_ON_ASSERT, c
 
 
 bool RA_CORE_API check_NAN( const WeightMatrix& matrix, const bool FAIL_ON_ASSERT, const bool MT ) {
-    int status = 0;
+    int status = 1;
     LOG( logDEBUG ) << "Searching for nans in the matrix...";
     if( MT ) {
         #pragma omp parallel for
         for( int k = 0; k < matrix.outerSize(); ++k ) {
             for( WeightMatrix::InnerIterator it( matrix, k ); it; ++it ) {
                 const Scalar      value = it.value();
-                const bool check = std::isnan( value );
+                const int check = std::isnan( value ) ? 0 : 1;
                 #pragma omp atomic
-                status += check ;
+                status &= check ;
             }
         }
-        if( status > 0 ) {
+        if( status == 0 ) {
             if( FAIL_ON_ASSERT ) {
-                CORE_ASSERT( status, "At least an element is nan" );
+                CORE_ASSERT( false, "At least an element is nan" );
             } else {
                 LOG( logDEBUG ) << "At least an element is nan";
             }
@@ -131,7 +131,7 @@ bool RA_CORE_API check_NAN( const WeightMatrix& matrix, const bool FAIL_ON_ASSER
                 } else {
                     if( std::isnan( value ) ) {
                         LOG( logDEBUG ) << text;
-                        status = false;
+                        status = 0;
                     }
                 }
             }
@@ -141,19 +141,19 @@ bool RA_CORE_API check_NAN( const WeightMatrix& matrix, const bool FAIL_ON_ASSER
 }
 
 bool RA_CORE_API check_NoWeightVertex( const WeightMatrix& matrix, const bool FAIL_ON_ASSERT, const bool MT ) {
-    int status = 0;
+    int status = 1;
     LOG( logDEBUG ) << "Searching for empty rows in the matrix...";
     if( MT ) {
         #pragma omp parallel for
         for( int i = 0; i < matrix.rows(); ++i ) {
             Sparse row = matrix.row( i );
-            const int check = row.size() - row.nonZeros();
+            const int check = ( row.nonZeros() > 0 ) ? 1 : 0;
             #pragma omp atomic
-            status += check;
+            status &= check;
         }
-        if( status > 0 ) {
+        if( status == 0 ) {
             if( FAIL_ON_ASSERT ) {
-                CORE_ASSERT( status, "At least a vertex as no weights" );
+                CORE_ASSERT( false, "At least a vertex as no weights" );
             } else {
                 LOG( logDEBUG ) << "At least a vertex as no weights";
             }
@@ -166,7 +166,7 @@ bool RA_CORE_API check_NoWeightVertex( const WeightMatrix& matrix, const bool FA
                 if( FAIL_ON_ASSERT ) {
                     CORE_ASSERT( false, text.c_str() );
                 } else {
-                    status = false;
+                    status = 0;
                     LOG( logDEBUG ) << text;
                 }
             }