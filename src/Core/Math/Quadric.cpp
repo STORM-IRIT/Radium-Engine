8532b247b2fc4f90a9edfe0865c52a2f705d73f9
Céline MICHAUD <michaud@pollock.irit.fr>
2016-09-15 18:01:54 +0200

    new Quadric class

---
+++ src/Core/Math/Quadric.cpp
@@ -0,0 +1,40 @@
+#include <Core/Math/Quadric.hpp>
+
+namespace Ra
+{
+    namespace Core
+    {
+
+        Quadric::Quadric()
+        {
+            m_a = Matrix3d::Zero();
+            m_b = Vector3d::Zero();
+            m_c = 0.0;
+        }
+
+
+        Quadric::Quadric(const Quadric &q)
+        {
+            m_a = q.getA();
+            m_b = q.getB();
+            m_c = q.getC();
+        }
+
+        Quadric::~Quadric() {}
+
+        void Quadric::compute(const Vector3d& n, double ndotp)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int k = 0; k < 3; k++)
+                {
+                    m_a(i, k) = n[i] * n[k];
+                }
+            }
+            m_b = ndotp * n;
+            m_c = ndotp * ndotp;
+        }
+
+
+    }
+}
