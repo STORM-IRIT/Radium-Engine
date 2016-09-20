c29e5967e01856e92cf3287c2d5cf8e504b3facd
Zouch <charly.mourglia@gmail.com>
2016-09-20 09:58:14 +0100

    Shadow mapping WIP

--- Shaders/BlinnPhong.frag.glsl
+++ Shaders/BlinnPhong.frag.glsl
@@ -2,6 +2,7 @@
 
 uniform Material material;
 uniform Light light;
+uniform sampler2D uShadowMap;
 
 out vec4 fragColor;
 
@@ -11,16 +12,30 @@ in VS_OUT
     vec3 normal;
     vec3 texcoord;
     vec3 eye;
+    vec4 position_light_space;
     mat3 TBN;
 } fs_in;
 
 #include "LightingFunctions.glsl"
 
+float Shadow()
+{
+    vec3 proj_coords = fs_in.position_light_space.xyz / fs_in.position_light_space.w;
+    proj_coords = proj_coords * 0.5 + 0.5;
+
+    float closest = texture(uShadowMap, proj_coords.xy).r;
+    float current = proj_coords.z;
+
+    float result = current > closest ? 1.0 : 0.0;
+    return result;
+}
+
 void main()
 {
     if (toDiscard()) discard;
 
-    vec3 color = computeLighting();
+    vec3 color = getKd() * 0.1 + (1 - Shadow()) * computeLighting();
+    //vec3 color = vec3(1 - Shadow());
 
     fragColor = vec4(color, 1.0);
 }
