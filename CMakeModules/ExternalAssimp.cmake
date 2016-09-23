a5b38c7717b3cb7ce2b50ccd6ba61b6288b9b93f
Zouch <charly.mourglia@gmail.com>
2016-09-23 17:56:39 +0100

    Fixed compilation error on windows

--- CMakeModules/ExternalAssimp.cmake
+++ CMakeModules/ExternalAssimp.cmake
@@ -1,6 +1,10 @@
 # have ExternalProject available
 include(ExternalProject)
 
+#set( ASSIMP_BUILD_ASSIMP_TOOLS OFF CACHE BOOL "" FORCE )
+#set( ASSIMP_BUILD_TESTS OFF CACHE BOOL "" FORCE )
+#set( ASSIMP_BUILD_SAMPLES OFF CACHE BOOL "" FORCE )
+
 # here is defined the way we want to import assimp
 ExternalProject_Add(
         assimp
@@ -13,5 +17,5 @@ ExternalProject_Add(
 
         # set the installatin to root
         INSTALL_DIR ${CMAKE_CURRENT_SOURCE_DIR}
-        CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>
+        CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR> -DASSIMP_BUILD_ASSIMP_TOOLS=False -DASSIMP_BUILD_SAMPLES=False -DASSIMP_BUILD_TESTS=False
 )
