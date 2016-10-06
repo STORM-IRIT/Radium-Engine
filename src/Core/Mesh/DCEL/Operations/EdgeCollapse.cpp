c605308619772bf6e4787b9fba5d73e51ba3e4b7
Anahid Ghazanfarpour <aghazanf@kahlo.irit.fr>
2016-10-06 15:39:34 +0200

    Problem in half-edge : twins with same starting vertex

--- src/Core/Mesh/DCEL/Operations/EdgeCollapse.cpp
+++ src/Core/Mesh/DCEL/Operations/EdgeCollapse.cpp
@@ -12,31 +12,65 @@ namespace DcelOperations {
 void edgeCollapse( Dcel& dcel, Index edgeIndex /*,Vector3d v*/ ) //v=v1+v2/2
 {
 
+    //Exception
+    /*
+
+                T
+               /|\
+              / | \
+             /  |  \
+            /   S   \
+           /   / \   \
+          /   /   \   \
+         /   /     \   \
+        /   /       \   \
+       V1 — — — — — — — V2
+
+       Il faut supprimer les 3 faces.
+
+    */
+
+
     //On récupère le edge à collapser
+
     HalfEdge_ptr edge = dcel.m_halfedge[edgeIndex];
 
     Vertex_ptr v1 = edge->V();
     Vertex_ptr v2 = edge->Next()->V();
 
-    Vector3 v1_c= v1->P();
-    Vector3 v2_c= v2->P();
+    Vector3 v1_c = v1->P();
+    Vector3 v2_c = v2->P();
     Vector3 v = (v1_c+v2_c)/2;
-
-    v1->setP(v);
-    v2->setHE(NULL);
-
 
     //On récupère les 2 half-edge
     HalfEdge_ptr h1 = edge;
     HalfEdge_ptr h2 = h1->Twin();
+
+
+
+
+
     Face_ptr f1 = h1->F();
-    Face_ptr f2 = h2->F();
+    Face_ptr f2 = nullptr;
     f1->setHE(NULL);
-    f2->setHE(NULL);
+    if (h2 != NULL) {
+        f2 = h2->F();
+        f2->setHE(NULL);
+    }
+
+    v1->setP(v);
+    v2->setHE(NULL); //on supprime v2
+
+//Dans quel cas se trouve-t-on? Cas classique ou exception?
+//    FullEdge_ptr ST = ((h1->Next())->Next())->FE();
+//    Vertex_ptr S = ST->V(0);
+//    Vertex_ptr T = ST->V(1);
+//    if ((h1->Next())->Next())
+
 
     //----------------------------------------------
     //TODO
-    if (v1->HE()->F() == f2)
+    if (f2 != nullptr && v1->HE()->F() == f2)
         v1->setHE(v1->HE()->Twin()->Next());
     else if (v1->HE()->F() == f1)
         v1->setHE(v1->HE()->Prev()->Twin());
@@ -44,15 +78,29 @@ void edgeCollapse( Dcel& dcel, Index edgeIndex /*,Vector3d v*/ ) //v=v1+v2/2
     //-----------------------------------------------
 
 //Cas classique on où n'est pas sur des bords
+
+
+//Cas classique
 //On ne modifie que les half-edge ayant pour premier vertex v2
 
     //h pour parcourir les half-edges et remplacer v2 par v1
-    HalfEdge_ptr h = h1->Next();
+    //on parcourt à partir de h1
+    HalfEdge_ptr h = h1;
+    do {
+       h=h->Next();
+       h->setV(v1);
+       h=h->Twin();
+    } while (h!=h1 && h!=NULL);
 
-    while (h!=h2) {
-        h->setV(v1);
+    //Si on a une ouverture dans le maillage, on parcourt à partir de h2 également
+    if (h==NULL && h2!=NULL) {
+        h=h2->Prev();
         h=h->Twin();
-        h=h->Next();
+        while (h!=NULL) {
+            h->setV(v1);
+            h=h->Prev();
+            h=h->Twin();
+        }
     }
 
     //On met à jour les twins half-edge
@@ -60,170 +108,18 @@ void edgeCollapse( Dcel& dcel, Index edgeIndex /*,Vector3d v*/ ) //v=v1+v2/2
     HalfEdge_ptr e2 = (h1->Next())->Twin();
     e1->setTwin(e2);
     e2->setTwin(e1);
-    HalfEdge_ptr e3 = (h2->Prev())->Twin();
-    HalfEdge_ptr e4 = (h2->Next())->Twin();
-    e3->setTwin(e4);
-    e4->setTwin(e3);
 
-    //gérer les half-edges, full-edges et faces
-
-
-
-//    HalfEdge_ptr h = h1->Next();
-//    // Attention 2 cas : cas cyclique et non-cyclique (x2)
-//    // Cas cyclique
-//    while( h != h2 ) {
-//        //on remplace le premier vertex par v
-//        h->setV(v);
-//        h=h->Twin();
-//        //on se sert du twin pour changer de face
-//        h=h->Next();
-//    }
-//    while( h != h1 ) {
-//        //on remplace le premier vertex par v
-//        h->setV(v);
-//        h=h->Twin();
-//        //on se sert du twin pour changer de face
-//        h=h->Next();
-//    }
-
-//    //mettre à jour les 2 nouveaux couples de twins aux extrémités
-//    if (h==h1) {
-//        //couple 1
-//        HalfEdge_ptr e1 = (h1->Prev())->Twin();
-//        HalfEdge_ptr e2 = (h1->Next())->Twin(); //à faire en double
-//        e1.setTwin(e2);
-//        //couple 2
-//        HalfEdge_ptr e3 = (h2->Next())->Twin();
-//        HalfEdge_ptr e4 = (h2->Prev())->Twin();
-//        e3.setTwin(e4);
-//    }
-    // Cas non cyclique
-//    if (h==NULL) {
-//        h = h1->Prev();
-//        h = h->Twin();
-//        while (h!=NULL) {
-//            h->setV(v);
-//            h=h->Twin();
-//            h=h->Next();
-//        }
-//        h = h2->Next();
-//        while (h!=NULL) {
-//            h->setV(v);
-//            h=h->Twin();
-//            h=h->Next();
-//        }
-//        //màj extrémités
-//        HalfEdge_ptr e5 = (h1->Prev())->Twin();
-//        HalfEdge_ptr e6 = (h1->Next())->Twin();
-//        e5.setTwin(e6);
-//    }
-}
-
-/*
-v is the optimal vertex to replace the collapsing edge
-void edgeCollapse( Dcel& dcel, Index edgeIndex) //v=v1+v2/2
-{
-
-    //On récupère le edge à collapser
-    FullEdge_ptr edge = dcel.m_fulledge[edgeIndex];
-
-    Vertex_ptr v1 = edge->V( 0 );
-    Vertex_ptr v2 = edge->V( 1 );
-
-    Vector3 v1_c= v1->P();
-    Vector3 v2_c= v2->P();
-    Vector3 v = (v1_c+v2_c)/2;
-
-    v1->setP(v);
-    v2->setHE(NULL);
-
-
-    //On récupère les 2 half-edge
-    HalfEdge_ptr h1 = edge->HE( 0 );
-    HalfEdge_ptr h2 = h1->Twin();
-    Face_ptr f1 = h1->F();
-    Face_ptr f2 = h2->F();
-    f1->setHE(NULL);
-    f2->setHE(NULL);
-
-//Cas classique on où n'est pas sur des bords
-//On ne modifie que les half-edge ayant pour premier vertex v2
-
-    //h pour parcourir les half-edges et remplacer v2 par v1
-    HalfEdge_ptr h = h1->Next();
-
-    while (h!=h2) {
-        h->setV(v1);
-        h=h->Twin();
-        h=h->Next();
+    if (h2!=NULL) {
+        HalfEdge_ptr e3 = (h2->Prev())->Twin();
+        HalfEdge_ptr e4 = (h2->Next())->Twin();
+        e3->setTwin(e4);
+        e4->setTwin(e3);
     }
 
-    //On met à jour les twins half-edge
-    HalfEdge_ptr e1 = (h1->Prev())->Twin();
-    HalfEdge_ptr e2 = (h1->Next())->Twin();
-    e1->setTwin(e2);
-    e2->setTwin(e1);
-    HalfEdge_ptr e3 = (h2->Prev())->Twin();
-    HalfEdge_ptr e4 = (h2->Next())->Twin();
-    e3->setTwin(e4);
-    e4->setTwin(e3);
-
     //gérer les half-edges, full-edges et faces
 
-
-
-//    HalfEdge_ptr h = h1->Next();
-//    // Attention 2 cas : cas cyclique et non-cyclique (x2)
-//    // Cas cyclique
-//    while( h != h2 ) {
-//        //on remplace le premier vertex par v
-//        h->setV(v);
-//        h=h->Twin();
-//        //on se sert du twin pour changer de face
-//        h=h->Next();
-//    }
-//    while( h != h1 ) {
-//        //on remplace le premier vertex par v
-//        h->setV(v);
-//        h=h->Twin();
-//        //on se sert du twin pour changer de face
-//        h=h->Next();
-//    }
-
-//    //mettre à jour les 2 nouveaux couples de twins aux extrémités
-//    if (h==h1) {
-//        //couple 1
-//        HalfEdge_ptr e1 = (h1->Prev())->Twin();
-//        HalfEdge_ptr e2 = (h1->Next())->Twin(); //à faire en double
-//        e1.setTwin(e2);
-//        //couple 2
-//        HalfEdge_ptr e3 = (h2->Next())->Twin();
-//        HalfEdge_ptr e4 = (h2->Prev())->Twin();
-//        e3.setTwin(e4);
-//    }
-    // Cas non cyclique
-//    if (h==NULL) {
-//        h = h1->Prev();
-//        h = h->Twin();
-//        while (h!=NULL) {
-//            h->setV(v);
-//            h=h->Twin();
-//            h=h->Next();
-//        }
-//        h = h2->Next();
-//        while (h!=NULL) {
-//            h->setV(v);
-//            h=h->Twin();
-//            h=h->Next();
-//        }
-//        //màj extrémités
-//        HalfEdge_ptr e5 = (h1->Prev())->Twin();
-//        HalfEdge_ptr e6 = (h1->Next())->Twin();
-//        e5.setTwin(e6);
-//    }
 }
-*/
+
 
 }
 }
