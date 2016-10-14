8ee7981e532849a5627908d10de9f1669aa66974
Celine Michaud <celine.michaud@irit.fr>
2016-10-14 18:37:26 +0200

    vertex split not ok yet because there is a problem on the edge collapse : an edge and his twin has the same starting vertex. See and uncomment l. 157 of EdgeCollapse.cpp

---
+++ src/Core/Mesh/ProgressiveMesh/ProgressiveMeshLOD.hpp
@@ -0,0 +1,62 @@
+#ifndef PROGRESSIVEMESHLOD_H
+#define PROGRESSIVEMESHLOD_H
+
+#include <Core/Mesh/ProgressiveMesh/ProgressiveMesh.hpp>
+
+namespace Ra
+{
+    namespace Core
+    {
+        class ProgressiveMeshLOD
+        {
+        public:
+
+            /// Constructor
+            ProgressiveMeshLOD();
+            ProgressiveMeshLOD(ProgressiveMesh* pm);
+            ProgressiveMeshLOD(const ProgressiveMeshLOD &pmlod)
+            {
+                std::cout << "test" << std::endl;
+            }
+            ~ProgressiveMeshLOD()
+            {
+                std::cout << "dest_test" << std::endl;
+            }
+
+            /// This method will build the Progressive Mesh
+            TriangleMesh build(int target_nb_faces);
+
+            /// Applies one vertex split :
+            /// enables 1 vertex, and 2 faces
+            int more();
+
+            /// Applies one edge collapse :
+            /// disables 1 vertex, and 2 faces
+            int less();
+
+            /// Applies edge collapses or vertex splits
+            /// to get the desired level of detail
+            TriangleMesh gotoM(int target);
+            //TriangleMesh gotoM(Type t, int target_nb_faces);
+            //TriangleMesh gotoM(int target_vsplit);
+
+            /// Getters and Setters
+            void setNbNoFrVSplit(const int v);
+            int getNbNoFrVSplit();
+            int getCurrVSplit();
+            ProgressiveMesh* getProgressiveMesh();
+
+        private:
+            std::vector<ProgressiveMeshData> m_pmdata;
+            ProgressiveMesh* m_pm;
+            int m_nb_no_fr_vsplit;
+            int m_curr_vsplit;
+
+
+
+        };
+    } // namespace Core
+} // namespace Ra
+
+
+#endif // PROGRESSIVEMESHLOD_H
