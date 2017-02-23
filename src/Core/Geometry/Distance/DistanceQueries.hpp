6cfb13e1669e9989a2a0f5936c27d12cc430e032
Valentin Roussellet <valentin.roussellet@irit.fr>
2017-02-23 18:04:14 +0100

    cleaned documentation for pt-triangle query

--- src/Core/Geometry/Distance/DistanceQueries.hpp
+++ src/Core/Geometry/Distance/DistanceQueries.hpp
@@ -7,8 +7,6 @@
 
 /// Functions in this file are utilities to compute the distance between various geometric sets.
 /// They always return the squared distance.
-
-
 namespace Ra
 {
     namespace Core
@@ -35,39 +33,29 @@ namespace Ra
             // Point-to-triangle distance
             //
 
+            /// Structure holding the result of a point-to triangle distance query.
             struct PointToTriangleOutput
             {
-                enum Flags
-                {
-                    HIT_FACE = 0,
-                    HIT_VERTEX = 1,
-                    HIT_EDGE = 2,
-                };
+                /// Flags denoting the primitive where the closest point is.
+                enum Flags {HIT_FACE = 0,HIT_VERTEX = 1,HIT_EDGE = 2};
 
-                PointToTriangleOutput() : meshPoint({0,0,0}), distanceSquared(std::numeric_limits<Scalar>::max()), flags(0){}
+                PointToTriangleOutput()
+                        : meshPoint({0,0,0}), distanceSquared(std::numeric_limits<Scalar>::max()), flags(0){}
 
-                Vector3 meshPoint; // the point hit on the mesh
-                Scalar distanceSquared; // distance squared to the point
-                uchar flags;     // Bits 0-1 : if the hit is a face (00) a vertex (01) or an edge (10)
-                                 // bits 2-3 : if vertex, index of the hit vertex
-                                 //            if edge, index of the edge's first vertex
+                Vector3 meshPoint; //! the point hit on the mesh
+                Scalar distanceSquared; //! distance squared to the point
+                uchar flags;     //! Bits 0-1 : if the hit is a face (00) a vertex (01) or an edge (10)
+                                 //! bits 2-3 : if vertex, index of the hit vertex
+                                 //!            if edge, index of the edge's first vertex
 
-                // Return if the hit is a face, a vertex or an edge
+                /// Return if the hit is a face, a vertex or an edge
                 inline Flags getHitPrimitive() const { return Flags(flags & 0x3u); }
-                // Return the index of the hit vertex or the first vertex of the hit edge.
-                // If hit is a face hit, returns 0
-                uint getHitIndex() const
-                {
-                    return (flags & 0xcu)>>2;
-                }
+                /// Return the index of the hit vertex or the first vertex of the hit edge.
+                /// If hit is a face hit, returns 0
+                uint getHitIndex() const { return (flags & 0xcu)>>2;}
             };
 
-
-
             /// Returns the squared distance d from a query point Q to the triangle ABC.
-            /// triPoint T is computed as the closest point on the triangle.
-            /// Thus ||QT|| = d.
-            /// Triangle ABC must not be degenerate.
             inline RA_CORE_API PointToTriangleOutput
             pointToTriSq(const Vector3& q, const Vector3& a, const Vector3& b, const Vector3& c);
 
