8807f503437ee2c441f42f8b34e9951c979cd3dc
Valentin Roussellet <valentin.roussellet@irit.fr>
2017-03-10 09:14:05 +0100

    Minor fixes for previous commits
    Subsequent to PR #157 comments

--- src/Engine/RadiumEngine.hpp
+++ src/Engine/RadiumEngine.hpp
@@ -10,7 +10,6 @@
 #include <vector>
 
 #include <Core/Log/Log.hpp>
-#include <Core/Utils/Singleton.hpp>
 #include <Engine/Renderer/RenderObject/RenderObjectManager.hpp>
 #include <Engine/Managers/EntityManager/EntityManager.hpp>
 #include <Engine/Managers/SignalManager/SignalManager.hpp>
