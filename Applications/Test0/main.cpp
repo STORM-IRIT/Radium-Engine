08d9ecb75b07cb0b6ab280c9c1f09d7476f66b62
Niluap'M <mathiaspaulin@users.noreply.github.com>
2017-05-24 15:09:41 +0200

    Fix WIN32 unknown OpenGL function (include glbinding without importing namespace) Remove unwanted includes in OpenGL.h Fix Test0 cmake

--- Applications/Test0/main.cpp
+++ Applications/Test0/main.cpp
@@ -12,89 +12,7 @@
 #include <minimalradium.hpp>
 /* This file contains a minimal radium/qt application which shows the
 classic "Spinning Cube" demo. */
-<<<<<<< HEAD
 #include <minimalapp.hpp>
-=======
-
-    /// Our minimal application uses QTimer to be called at a regular frame rate.
-    class MinimalApp : public QApplication
-    {
-    public:
-        MinimalApp(int argc, char** argv)
-                : QApplication(argc, argv), _engine(nullptr),
-                  _task_queue(nullptr),
-                  _viewer(nullptr),
-                  _frame_timer(nullptr),
-                  _target_fps(60)
-        {
-            LOG(logDEBUG) << "Initialize engine.";
-            _engine.reset(Ra::Engine::RadiumEngine::createInstance());
-            _engine->initialize();
-            LOG(logDEBUG) << "Initialize taskqueue.";
-            _task_queue.reset(new Ra::Core::TaskQueue(std::thread::hardware_concurrency() - 1));
-            LOG(logDEBUG) << "Initialize viewer.";
-            _viewer.reset(new Ra::Gui::Viewer);
-
-            CORE_ASSERT( _viewer != nullptr, "GUI was not initialized" );
-
-            LOG(logDEBUG) << "Initialize timer.";
-            _frame_timer = new QTimer(this);
-            _frame_timer->setInterval(1000 / _target_fps);
-            connect(_frame_timer, &QTimer::timeout, this, &MinimalApp::frame);
-        }
-
-        ~MinimalApp()
-        {
-            _engine->cleanup();
-        }
-
-
-    public slots:
-
-        /// This function is the basic "game loop" iteration of the engine.
-        /// It starts the rendering then advance all systems by one frame.
-        void frame()
-        {
-            // We use a fixed time step, but it is also possible
-            // to check the time from last frame.
-            const Scalar dt = 1.f / Scalar(_target_fps);
-
-            // Starts the renderer
-            _viewer->startRendering(dt);
-
-            // Collect and run tasks
-            _engine->getTasks(_task_queue.get(), dt);
-            _task_queue->startTasks();
-            _task_queue->waitForTasks();
-            _task_queue->flushTaskQueue();
-
-            // Finish the frame
-            _viewer->waitForRendering();
-            //_viewer.update();
-            _viewer->repaint();
-            _engine->endFrameSync();
-        }
-
-    public:
-        // Our instance of the engine
-        std::unique_ptr<Ra::Engine::RadiumEngine> _engine;
-
-        // Task queue
-        std::unique_ptr<Ra::Core::TaskQueue> _task_queue;
-
-        // Pointer to Qt/OpenGL Viewer widget.
-        std::unique_ptr<Ra::Gui::Viewer>  _viewer;
-
-        // Timer to wake us up at every frame start.
-        QTimer* _frame_timer;
-
-        // Our framerate
-        uint _target_fps;
-
-    }; // end class
-
-
->>>>>>> Fix make files for out of disturb compilation
 
 int main(int argc, char* argv[])
 {
@@ -111,7 +29,6 @@ int main(int argc, char* argv[])
     QSurfaceFormat::setDefaultFormat( format );
 
     // Create app
-<<<<<<< HEAD
     LOG(logDEBUG) << "Creating application.";
     MinimalApp app(argc, argv);
     LOG(logDEBUG) << "Show viewer.";
@@ -120,48 +37,20 @@ int main(int argc, char* argv[])
 
     // Create one system
     LOG(logDEBUG) << "Creating minimal system.";
-=======
-    LOG(logINFO) << "Creating application.";
-    MinimalApp app(argc, argv);
-    LOG(logINFO) << "Show viewer.";
-    app._viewer->show();
-    CORE_ASSERT( app._viewer->context()->isValid(), "OpenGL was not initialized" );
-
-    LOG(logINFO) << "Creating BlinnPhong Shader.";
-    // Load Blinn-Phong shader
-    Ra::Engine::ShaderConfiguration bpConfig("BlinnPhong");
-    bpConfig.addShader(Ra::Engine::ShaderType_VERTEX, "Shaders/BlinnPhong.vert.glsl");
-    bpConfig.addShader(Ra::Engine::ShaderType_FRAGMENT, "Shaders/BlinnPhong.frag.glsl");
-    Ra::Engine::ShaderConfigurationFactory::addConfiguration(bpConfig);
-
-    // Create one system
-    LOG(logINFO) << "Creating minimal system.";
->>>>>>> Fix make files for out of disturb compilation
     Ra::Engine::System* sys = new MinimalSystem;
     app.m_engine->registerSystem("Minimal system", sys);
 
     // Create and initialize entity and component
-<<<<<<< HEAD
     LOG(logDEBUG) << "Creating an entity.";
     Ra::Engine::Entity* e = app.m_engine->getEntityManager()->createEntity("Cube");
-=======
-    LOG(logINFO) << "Creating an entity.";
-    Ra::Engine::Entity* e = app._engine->getEntityManager()->createEntity("Cube");
->>>>>>> Fix make files for out of disturb compilation
     Ra::Engine::Component* c = new MinimalComponent;
     e->addComponent(c);
     sys->registerComponent(e, c);
     c->initialize();
 
-
     // Start the app.
 
-<<<<<<< HEAD
     LOG(logDEBUG) << "Main loop.";
     app.m_frame_timer->start();
-=======
-    LOG(logINFO) << "Main loop.";
-    app._frame_timer->start();
->>>>>>> Fix make files for out of disturb compilation
     return app.exec();
 }
