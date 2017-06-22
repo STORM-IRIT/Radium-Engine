1220c5a0b97c9f4a161948af29f833b4f987936d
Matthieu LOCUSSOL <dev.matthieu.locussol@gmail.com>
2017-06-22 11:43:34 +0200

    Updated AssimpFileLoader (getFileExtensions() still needs to be done)

--- src/Engine/Assets/AssimpFileLoader.cpp
+++ src/Engine/Assets/AssimpFileLoader.cpp
@@ -1,10 +1,7 @@
 #include <Engine/Assets/AssimpFileLoader.hpp>
 
-#include <assimp/Importer.hpp>
 #include <assimp/scene.h>
 #include <assimp/postprocess.h>
-
-#include <Engine/Assets/FileData.hpp>
 
 #include <Engine/Assets/AssimpHandleDataLoader.hpp>
 #include <Engine/Assets/AssimpGeometryDataLoader.hpp>
@@ -13,7 +10,7 @@
 
 namespace Ra
 {
-    namespace Engine
+    namespace Asset
     {
         AssimpFileLoader::AssimpFileLoader()
         {
@@ -25,17 +22,21 @@ namespace Ra
 
         }
 
-        virtual const std::vector<std::string>& AssimpFileLoader::getFileExtensions() const
+        const std::vector<std::string>& AssimpFileLoader::getFileExtensions() const
         {
+            aiString extensionsList;
 
+            m_importer.GetExtensionList( extensionsList );
+
+            return std::vector<std::string>();
         }
 
-        virtual bool AssimpFileLoader::handleFileExtension( const std::string& extension ) const
+        bool AssimpFileLoader::handleFileExtension( const std::string& extension ) const
         {
-
+            return m_importer.IsExtensionSupported( extension );
         }
 
-        virtual FileData * AssimpFileLoader::loadFile( const std::string& filename ) const
+        FileData * AssimpFileLoader::loadFile( const std::string& filename )
         {
             FileData * fileData = new FileData( filename );
 
@@ -44,32 +45,50 @@ namespace Ra
                 return nullptr;
             }
 
-            Assimp::Importer importer;
-
-            const aiScene * scene = importer.ReadFile( getFileName(),
-                                                       aiProcess_Triangulate           | // This could/should be taken away if we want to deal with mesh types other than trimehses
-                                                       aiProcess_JoinIdenticalVertices |
-                                                       aiProcess_GenSmoothNormals      |
-                                                       aiProcess_SortByPType           |
-                                                       aiProcess_FixInfacingNormals    |
-                                                       aiProcess_CalcTangentSpace      |
-                                                       aiProcess_GenUVCoords );
+            const aiScene * scene = m_importer.ReadFile( fileData->getFileName(),
+                                                         aiProcess_Triangulate           | // This could/should be taken away if we want to deal with mesh types other than trimehsesaiProcess_JoinIdenticalVertices |
+                                                         aiProcess_GenSmoothNormals      |
+                                                         aiProcess_SortByPType           |
+                                                         aiProcess_FixInfacingNormals    |
+                                                         aiProcess_CalcTangentSpace      |
+                                                         aiProcess_GenUVCoords );
 
             if ( scene == nullptr )
             {
-                LOG( logINFO ) << "File \"" << getFileName() << "\" assimp error : " << importer.GetErrorString() << ".";
+                LOG( logINFO ) << "File \"" << fileData->getFileName() << "\" assimp error : " << m_importer.GetErrorString() << ".";
                 return nullptr;
             }
 
             if( fileData->isVerbose() )
             {
-                LOG(logINFO) << "File Loading begin...";
+                LOG( logINFO ) << "File Loading begin...";
             }
+
+            std::clock_t startTime;
+            startTime = std::clock();
+
+            AssimpGeometryDataLoader geometryLoader( Core::StringUtils::getDirName( filename ), fileData->isVerbose() );
+            geometryLoader.loadData( scene, fileData->m_geometryData );
+
+            AssimpHandleDataLoader handleLoader( fileData->isVerbose() );
+            handleLoader.loadData( scene, fileData->m_handleData );
+
+            AssimpAnimationDataLoader animationLoader( fileData->isVerbose() );
+            animationLoader.loadData( scene, fileData->m_animationData );
+
+            AssimpLightDataLoader lightLoader( Core::StringUtils::getDirName( filename ), fileData->isVerbose() );
+            lightLoader.loadData( scene, fileData->m_lightData );
+
+            fileData->m_loadingTime = ( std::clock() - startTime ) / Scalar( CLOCKS_PER_SEC );
 
             if( fileData->isVerbose() )
             {
+                LOG( logINFO ) << "File Loading end.";
 
+                fileData->displayInfo();
             }
+
+            fileData->m_processed = true;
 
             return fileData;
         }
--- src/Engine/Assets/AssimpFileLoader.hpp
+++ src/Engine/Assets/AssimpFileLoader.hpp
@@ -1,18 +1,30 @@
 #ifndef RADIUMENGINE_ASSIMPFILELOADER_HPP
 #define RADIUMENGINE_ASSIMPFILELOADER_HPP
 
+#include <vector>
+
+#include <assimp/Importer.hpp>
+
+#include <Engine/Assets/FileData.hpp>
 #include <Engine/Assets/FileLoaderInterface.hpp>
 
 namespace Ra
 {
-    namespace Engine
+    namespace Asset
     {
         class AssimpFileLoader : public FileLoaderInterface
         {
         public:
+            AssimpFileLoader();
+
+            virtual ~AssimpFileLoader();
+
             virtual const std::vector<std::string>& getFileExtensions() const override;
             virtual bool handleFileExtension( const std::string& extension ) const override;
-            virtual FileData * loadFile( const std::string& filename ) const override;
+            virtual FileData * loadFile( const std::string& filename ) override;
+
+        private:
+            Assimp::Importer m_importer;
         };
     }
 }
