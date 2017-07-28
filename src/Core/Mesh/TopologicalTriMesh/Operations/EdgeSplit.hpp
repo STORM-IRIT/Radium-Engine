b68ea78df9973de3fd9d6bf399a7f2a15c0ae9fb
Syn4ck <xavier.chalut@gmail.com>
2017-07-28 11:33:20 +0200

    Update topological mesh files

--- src/Core/Mesh/TopologicalTriMesh/Operations/EdgeSplit.hpp
+++ src/Core/Mesh/TopologicalTriMesh/Operations/EdgeSplit.hpp
@@ -1,12 +1,17 @@
+#ifndef EDGESPLIT_H
+#define EDGESPLIT_H
+
 #include <Core/RaCore.hpp>
-#include <Core/Mesh/TopologicalTriMesh/TopologicalMesh.h>
+#include <Core/Mesh/TopologicalTriMesh/TopologicalMesh.hpp>
 #include <Core/Index/IndexMap.hpp>
 #include <Core/Index/IndexedObject.hpp>
 
 namespace Ra {
 namespace Core {
-namespace TTMOperations {
-    void splitEdge( TopologicalMesh& topoMesh, Index edgeIndex, Scalar fraction );
+namespace TMOperations {
+    void splitEdge( TopologicalMesh& topologicalMesh, TopologicalMesh::EdgeHandle edgeHandle, Scalar fraction );
 }
 }
 }
+
+#endif // EDGESPLIT_H
