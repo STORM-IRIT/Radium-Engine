2d1485903ed160e5ace83b5b6cad37eea64fd271
nmellado <nmellado0@gmail.com>
2017-10-27 18:15:53 +0200

    Factorize compiler flags between macos and linux

--- cmake/CompileFlags.cmake
+++ cmake/CompileFlags.cmake
@@ -15,6 +15,10 @@ endif()
 
 # Compilation flag for each platforms =========================================
 
+set(UNIX_DEFAULT_CXX_FLAGS                "-Wall -Wextra  -pthread -msse3 -Wno-sign-compare -Wno-unused-parameter -fno-exceptions -fPIC")
+set(UNIX_DEFAULT_CXX_FLATS_DEBUG          "-D_DEBUG -DCORE_DEBUG -g3 -ggdb")
+set(UNIX_DEFAULT_CXX_FLATS_RELEASE        "-DNDEBUG -O3")
+set(UNIX_DEFAULT_CXX_FLATS_RELWITHDEBINFO "-g3")
 if (APPLE)
 #    message(STATUS "${PROJECT_NAME} : Compiling on Apple with compiler " ${CMAKE_CXX_COMPILER_ID})
 
@@ -25,10 +29,10 @@ if (APPLE)
     endif()
 
     set(CMAKE_CXX_STANDARD 14)
-    set(CMAKE_CXX_FLAGS                "-Wall -Wextra  -pthread -msse3 -Wno-sign-compare -Wno-unused-parameter -fno-exceptions -fPIC ${CMAKE_CXX_FLAGS}")
-    set(CMAKE_CXX_FLAGS_DEBUG          "-D_DEBUG -DCORE_DEBUG -g3 -ggdb ${CMAKE_CXX_FLAGS_DEBUG}")
-    set(CMAKE_CXX_FLAGS_RELEASE        "-DNDEBUG -O3 ${MATH_FLAG}")
-    set(CMAKE_CXX_FLAGS_RELWITHDEBINFO "-g3 ${CMAKE_CXX_FLAGS_RELEASE}")
+    set(CMAKE_CXX_FLAGS                "${UNIX_DEFAULT_CXX_FLAGS}                ${CMAKE_CXX_FLAGS}")
+    set(CMAKE_CXX_FLAGS_DEBUG          "${UNIX_DEFAULT_CXX_FLATS_DEBUG}          ${CMAKE_CXX_FLAGS_DEBUG}")
+    set(CMAKE_CXX_FLAGS_RELEASE        "${UNIX_DEFAULT_CXX_FLATS_RELEASE}        ${MATH_FLAG}")
+    set(CMAKE_CXX_FLAGS_RELWITHDEBINFO "${UNIX_DEFAULT_CXX_FLATS_RELWITHDEBINFO} ${CMAKE_CXX_FLAGS_RELEASE}")
 
     add_definitions( -Wno-deprecated-declarations ) # Do not warn for eigen bind being deprecated
 elseif (UNIX OR MINGW)
@@ -46,10 +50,10 @@ elseif (UNIX OR MINGW)
     endif()
 
     set(CMAKE_CXX_STANDARD 14)
-    set(CMAKE_CXX_FLAGS                "-Wall -Wextra  -pthread -msse3 -Wno-sign-compare -Wno-unused-parameter -fno-exceptions -fPIC ${EIGEN_ALIGNMENT_FLAG} ${CMAKE_CXX_FLAGS}")
-    set(CMAKE_CXX_FLAGS_DEBUG          "-D_DEBUG -DCORE_DEBUG -g3 -ggdb ${CMAKE_CXX_FLAGS_DEBUG}")
-    set(CMAKE_CXX_FLAGS_RELEASE        "-DNDEBUG -O3 ${MATH_FLAG}")
-    set(CMAKE_CXX_FLAGS_RELWITHDEBINFO "-g3 -ggdb ${CMAKE_CXX_FLAGS_RELEASE}")
+    set(CMAKE_CXX_FLAGS                "${UNIX_DEFAULT_CXX_FLAGS}                ${EIGEN_ALIGNMENT_FLAG} ${CMAKE_CXX_FLAGS}")
+    set(CMAKE_CXX_FLAGS_DEBUG          "${UNIX_DEFAULT_CXX_FLATS_DEBUG}          ${CMAKE_CXX_FLAGS_DEBUG}")
+    set(CMAKE_CXX_FLAGS_RELEASE        "${UNIX_DEFAULT_CXX_FLATS_RELEASE}        ${MATH_FLAG}")
+    set(CMAKE_CXX_FLAGS_RELWITHDEBINFO "${UNIX_DEFAULT_CXX_FLATS_RELWITHDEBINFO} -ggdb ${CMAKE_CXX_FLAGS_RELEASE}")
 
     # Prevent Eigen from spitting thousands of warnings with gcc 6+
     add_definitions(-Wno-deprecated-declarations)
