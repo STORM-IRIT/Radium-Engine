cfd01a210de7da8845f2484f184b82dbd5cd0bb1
Nicolas Mellado <nmellado0@gmail.com>
2017-12-05 11:29:51 +0100

    Construct list of supported file extensions from registered loaders
    Also remove dependency to assimp, which was preventing to compile with RADIUM_ASSIMP_SUPPORT=FALSE

--- Applications/MainApplication/Gui/MainWindow.cpp
+++ Applications/MainApplication/Gui/MainWindow.cpp
@@ -7,8 +7,6 @@
 #include <QToolButton>
 #include <QComboBox>
 
-#include <assimp/Importer.hpp>
-
 #include <Core/File/deprecated/OBJFileManager.hpp>
 
 #include <Engine/Managers/SignalManager/SignalManager.hpp>
@@ -171,13 +169,19 @@ namespace Ra
 
     void Gui::MainWindow::loadFile()
     {
-        // Filter the files
-        aiString extList;
-        Assimp::Importer importer;
-        importer.GetExtensionList(extList);
-        std::string extListStd(extList.C_Str());
-        std::replace(extListStd.begin(), extListStd.end(), ';', ' ');
-        QString filter =""/* QString::fromStdString(extListStd)*/;
+
+        QString filter;
+
+        for ( const Asset::FileLoaderInterface * loader : mainApp->m_engine->getFileLoaders() )
+        {
+            QString exts;
+            for (const auto& e : loader->getFileExtensions())
+                exts.append(QString::fromStdString(e) + tr(" "));
+            filter.append( QString::fromStdString(loader->name()) +
+                           tr(" (") +
+                           exts +
+                           tr(");; "));
+        }
 
         QSettings settings;
         QString path = settings.value("files/load", QDir::homePath()).toString();
