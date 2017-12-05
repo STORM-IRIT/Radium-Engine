2689e680797f14199aba0cce4576fc050c6aca88
dlyr <github@dlyr.fr>
2017-12-06 00:21:04 +0100

    typos and blank lines

--- src/Core/Mesh/Wrapper/TopologicalMeshConvert.hpp
+++ src/Core/Mesh/Wrapper/TopologicalMeshConvert.hpp
@@ -7,7 +7,7 @@
 namespace Ra {
 namespace Core {
 
-    //! Adapter class to convert between vortex::Mesh and vortex::TopologicalMesh
+    //! Adapter class to convert between Core::Mesh and vortex::TopologicalMesh
     //! \todo take into account texture coordinates and normals more robustly.
     class RA_CORE_API MeshConverter{
     public:
