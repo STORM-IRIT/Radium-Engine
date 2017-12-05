b576f26c56116baae324deb69058f2a9f5560f0c
Nicolas Mellado <nmellado0@gmail.com>
2017-12-05 11:27:41 +0100

    Remove virtual keyword for overriding functions

--- src/IO/AssimpLoader/AssimpFileLoader.hpp
+++ src/IO/AssimpLoader/AssimpFileLoader.hpp
@@ -18,9 +18,9 @@ namespace Ra {
 
             virtual ~AssimpFileLoader();
 
-            virtual std::vector<std::string> getFileExtensions() const override;
-            virtual bool handleFileExtension( const std::string& extension ) const override;
-            virtual Asset::FileData * loadFile( const std::string& filename ) override;
+            std::vector<std::string> getFileExtensions() const override;
+            bool handleFileExtension( const std::string& extension ) const override;
+            Asset::FileData * loadFile( const std::string& filename ) override;
 
         private:
             Assimp::Importer m_importer;
