181a26d7916aceeb7011bbe928db13c0776204c1
Niluap'M <Mathias.Paulin@irit.fr>
2017-12-09 16:41:55 +0100

    Begin material system extension.

--- src/Engine/Renderer/RenderTechnique/Material.hpp
+++ src/Engine/Renderer/RenderTechnique/Material.hpp
@@ -12,74 +12,83 @@
 
 namespace Ra
 {
-    namespace Engine
-    {
-        class Texture;
-        class ShaderProgram;
-    }
+  namespace Engine
+  {
+    class Texture;
+
+    class ShaderProgram;
+  }
 }
 
 namespace Ra
 {
-    namespace Engine
+  namespace Engine
+  {
+
+    class RA_ENGINE_API Material
     {
-
-        class RA_ENGINE_API Material
+    public:
+        enum class TextureType
         {
-        public:
-            enum class TextureType
-            {
-                TEX_DIFFUSE,
-                TEX_SPECULAR,
-                TEX_NORMAL,
-                TEX_SHININESS,
-                TEX_ALPHA
-            };
-
-            enum class MaterialType
-            {
-                MAT_OPAQUE,
-                MAT_TRANSPARENT
-            };
-
-        public:
-            RA_CORE_ALIGNED_NEW
-
-            explicit Material( const std::string& name );
-            ~Material();
-
-            // FIXME(Charly): This should be a "gl material" class work
-            void updateGL();
-            void bind( const ShaderProgram* shader );
-
-            const std::string& getName() const;
-
-            void addTexture( const TextureType& type, Texture* texture );
-            TextureData& addTexture( const TextureType& type, const std::string& texture );
-            TextureData& addTexture( const TextureType& type, const TextureData& texture );
-            Texture* getTexture( const TextureType& type ) const;
-
-            void setMaterialType( const MaterialType& type );
-            const MaterialType& getMaterialType() const;
-
-        public:
-            Core::Color m_kd;
-            Core::Color m_ks;
-            Scalar m_ns;
-            Scalar m_alpha;
-
-    private:
-            std::string m_name;
-
-            bool m_isDirty;
-
-            std::map<TextureType, Texture*> m_textures;
-            std::map<TextureType, TextureData> m_pendingTextures;
-
-            MaterialType m_type;
+            TEX_DIFFUSE,
+            TEX_SPECULAR,
+            TEX_NORMAL,
+            TEX_SHININESS,
+            TEX_ALPHA
         };
 
-    } // namespace Engine
+        // TODO : make Radium Material follow the diversity of MaterialData
+        // This material could be the default : Blinn-Phong material
+        enum class MaterialType
+        {
+            MAT_OPAQUE,
+            MAT_TRANSPARENT
+        };
+
+    public:
+        RA_CORE_ALIGNED_NEW
+
+        explicit Material( const std::string& name );
+
+        ~Material();
+
+        // FIXME(Charly): This should be a "gl material" class work
+        void updateGL();
+
+        void bind( const ShaderProgram* shader );
+
+        const std::string& getName() const;
+
+        void addTexture( const TextureType& type, Texture* texture );
+
+        TextureData& addTexture( const TextureType& type, const std::string& texture );
+
+        TextureData& addTexture( const TextureType& type, const TextureData& texture );
+
+        Texture* getTexture( const TextureType& type ) const;
+
+        void setMaterialType( const MaterialType& type );
+
+        const MaterialType& getMaterialType() const;
+
+    public:
+        Core::Color m_kd;
+        Core::Color m_ks;
+        Scalar m_ns;
+        Scalar m_alpha;
+
+    private:
+        std::string m_name;
+
+        bool m_isDirty;
+
+        std::map<TextureType, Texture*> m_textures;
+        std::map<TextureType, TextureData> m_pendingTextures;
+
+        MaterialType m_type;
+    };
+
+  } // namespace Engine
 } // namespace Ra
 
 #endif
