3296fe079d0fc9d88c0d8a57c4175b4191acab87
Niluap'M <Mathias.Paulin@irit.fr>
2017-12-12 01:05:54 +0100

    GLSL #include search path (via OpenGL Named String). Adding glsl #include search path from an application or plugin is now allowed.

--- src/Engine/Renderer/RenderTechnique/ShaderProgramManager.cpp
+++ src/Engine/Renderer/RenderTechnique/ShaderProgramManager.cpp
@@ -9,13 +9,11 @@
 #include <Core/Containers/MakeShared.hpp>
 #include <Engine/Renderer/RenderTechnique/ShaderProgram.hpp>
 
-namespace Ra
-{
-    namespace Engine
-    {
+namespace Ra {
+    namespace Engine {
         using ShaderProgramPtr = std::shared_ptr<ShaderProgram>;
         
-        ShaderProgramManager::ShaderProgramManager(const std::string& vs, const std::string& fs):
+        ShaderProgramManager::ShaderProgramManager(const std::string &vs, const std::string &fs) :
         m_defaultVsName(vs),
         m_defaultFsName(fs)
         {
@@ -33,23 +31,30 @@ namespace Ra
             // NOTE: if you want to add a named string to handle a new shader include file, be SURE that the name (first
             // parameter) begin with a "/", otherwise it won't work !
             
-            m_files.push_back( globjects::File::create( "Shaders/Helpers.glsl" ) );
-            m_files.push_back( globjects::File::create( "Shaders/Structs.glsl" ) );
-            m_files.push_back( globjects::File::create( "Shaders/Tonemap.glsl" ) );
-            m_files.push_back( globjects::File::create( "Shaders/LightingFunctions.glsl" ) );
+            m_files.push_back(globjects::File::create("Shaders/Helpers.glsl"));
+            m_files.push_back(globjects::File::create("Shaders/Structs.glsl"));
+            m_files.push_back(globjects::File::create("Shaders/Tonemap.glsl"));
+            m_files.push_back(globjects::File::create("Shaders/LightingFunctions.glsl"));
             
-            m_namedStrings.push_back( globjects::NamedString::create( "/Helpers.glsl", m_files[0].get() ) );
-            m_namedStrings.push_back( globjects::NamedString::create( "/Structs.glsl", m_files[1].get() ) );
-            m_namedStrings.push_back( globjects::NamedString::create( "/Tonemap.glsl", m_files[2].get() ) );
-            m_namedStrings.push_back( globjects::NamedString::create( "/LightingFunctions.glsl", m_files[3].get() ) );
+            m_namedStrings.push_back(globjects::NamedString::create("/Helpers.glsl", m_files[0].get()));
+            m_namedStrings.push_back(globjects::NamedString::create("/Structs.glsl", m_files[1].get()));
+            m_namedStrings.push_back(globjects::NamedString::create("/Tonemap.glsl", m_files[2].get()));
+            m_namedStrings.push_back(globjects::NamedString::create("/LightingFunctions.glsl", m_files[3].get()));
             
             m_defaultShaderProgram = addShaderProgram("Default Program", m_defaultVsName, m_defaultFsName);
             
         }
         
-        const ShaderProgram* ShaderProgramManager::addShaderProgram(const std::string& name,
-                                                                    const std::string& vert,
-                                                                    const std::string& frag)
+        void ShaderProgramManager::addNamedString(const std::string& includepath, const std::string& realfile)
+        {
+            LOG(logINFO) << "Inserting named string : " << includepath << " --> " << realfile;
+            m_files.push_back(globjects::File::create(realfile));
+            m_namedStrings.push_back(globjects::NamedString::create(includepath, m_files[m_files.size()-1].get()));
+        }
+        
+        const ShaderProgram *ShaderProgramManager::addShaderProgram(const std::string &name,
+                                                                    const std::string &vert,
+                                                                    const std::string &frag)
         {
             ShaderConfiguration config(name);
             
@@ -59,20 +64,20 @@ namespace Ra
             return addShaderProgram(config);
         }
         
-        const ShaderProgram* ShaderProgramManager::addShaderProgram(const ShaderConfiguration& config)
+        const ShaderProgram *ShaderProgramManager::addShaderProgram(const ShaderConfiguration &config)
         {
-            auto found = m_shaderPrograms.find( config );
+            auto found = m_shaderPrograms.find(config);
             
-            if ( found != m_shaderPrograms.end() )
+            if (found != m_shaderPrograms.end())
             {
                 return found->second.get();
             }
             
             // Try to load the shader
-            auto prog = Core::make_shared<ShaderProgram>( config );
+            auto prog = Core::make_shared<ShaderProgram>(config);
             
             // FIXED : use isLinked not isValid
-            if ( prog->getProgramObject()->isLinked() )
+            if (prog->getProgramObject()->isLinked())
             {
                 insertShader(config, prog);
                 return prog.get();
@@ -80,10 +85,10 @@ namespace Ra
             else
             {
                 std::string error;
-                Core::StringUtils::stringPrintf( error,
+                Core::StringUtils::stringPrintf(error,
                                                 "Error occurred while loading shader program %s :\nDefault shader program used instead.\n",
-                                                config.m_name.c_str() );
-                LOG( logERROR ) << error;
+                                                config.m_name.c_str());
+                LOG(logERROR) << error;
                 
                 // insert in the failed shaders list
                 m_shaderFailedConfs.push_back(config);
@@ -93,7 +98,7 @@ namespace Ra
             
         }
         
-        const ShaderProgram* ShaderProgramManager::getShaderProgram(const std::string &id)
+        const ShaderProgram *ShaderProgramManager::getShaderProgram(const std::string &id)
         {
             auto found = m_shaderProgramIds.find(id);
             
@@ -105,7 +110,7 @@ namespace Ra
             return m_defaultShaderProgram;
         }
         
-        const ShaderProgram* ShaderProgramManager::getShaderProgram(const ShaderConfiguration& config)
+        const ShaderProgram *ShaderProgramManager::getShaderProgram(const ShaderConfiguration &config)
         {
             return addShaderProgram(config);
         }
@@ -113,7 +118,7 @@ namespace Ra
         void ShaderProgramManager::reloadAllShaderPrograms()
         {
             // For each shader in the map
-            for ( auto& shader : m_shaderPrograms )
+            for (auto &shader : m_shaderPrograms)
             {
                 shader.second->reload();
             }
@@ -127,7 +132,7 @@ namespace Ra
             // for each shader in the failed map, try to reload
             for (std::vector<ShaderConfiguration>::iterator conf = m_shaderFailedConfs.begin();
                  conf != m_shaderFailedConfs.end();
-                 ++ conf)
+                 ++conf)
             {
                 auto prog = Core::make_shared<ShaderProgram>(*conf);
                 
@@ -139,17 +144,17 @@ namespace Ra
             }
         }
         
-        const ShaderProgram* ShaderProgramManager::getDefaultShaderProgram() const
+        const ShaderProgram *ShaderProgramManager::getDefaultShaderProgram() const
         {
             return m_defaultShaderProgram;
         }
         
-        void ShaderProgramManager::insertShader(const ShaderConfiguration& config, const ShaderProgramPtr& shader)
+        void ShaderProgramManager::insertShader(const ShaderConfiguration &config, const ShaderProgramPtr &shader)
         {
             m_shaderProgramIds.insert(std::pair<std::string, ShaderConfiguration>(config.m_name, config));
             m_shaderPrograms.insert(std::pair<ShaderConfiguration, ShaderProgramPtr>(config, shader));
         }
         
-        RA_SINGLETON_IMPLEMENTATION( ShaderProgramManager );
+        RA_SINGLETON_IMPLEMENTATION(ShaderProgramManager);
     }// namespace Engine
 } // namespace Ra
