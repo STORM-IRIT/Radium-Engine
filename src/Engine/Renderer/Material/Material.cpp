d0667fba3af97bc440c293eddf6403b4d64fcf30
Mathias Paulin <mathias.paulin@irit.fr>
2017-12-12 19:33:38 +0100

    Material subsystem integration

---
+++ src/Engine/Renderer/Material/Material.cpp
@@ -0,0 +1,22 @@
+#include <Engine/Renderer/Material/Material.hpp>
+
+
+namespace Ra {
+  namespace Engine {
+
+    Material::Material(const std::string &name, MaterialType type)
+        : m_name(name), m_isDirty(true), m_type(MaterialType::MAT_OPAQUE)
+    {
+    }
+
+    Material::~Material()
+    {
+    }
+
+    bool Material::isTransparent() const
+    {
+        return m_type == MaterialType::MAT_TRANSPARENT;
+    }
+  }
+}
+
