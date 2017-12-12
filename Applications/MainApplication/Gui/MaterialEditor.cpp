c3b50e15c79b419aa31fbfa8c416d9c6827be019
Niluap'M <MathiasPaulin@users.noreply.github.com>
2017-12-12 19:31:50 +0100

    Last commit on the wrong formatting repair ... wrong file merge :(

--- Applications/MainApplication/Gui/MaterialEditor.cpp
+++ Applications/MainApplication/Gui/MaterialEditor.cpp
@@ -1,82 +1,176 @@
-#include <Core/Algorithm/Diffusion/Diffusion.hpp>
+#include <Gui/MaterialEditor.hpp>
 
-#include <utility>
+#include <QCloseEvent>
 
-namespace Ra {
-    namespace Core {
-        namespace Algorithm {
-            
-            
-            
-            ScalarValue diffuseDelta( const Geometry::AdjacencyMatrix& A, const Delta& delta, const Scalar lambda, const uint iteration ) {
-                ScalarValue u( delta );
-                ScalarValue tmp( delta );
-                unsigned int iter = iteration;
-                iter = iter + ( iter % 2 );
-                for( uint n = 0; n < iter; ++n ) {
-                    for( Delta::InnerIterator u_it( u, 0 ); u_it; ++u_it) {
-                        Scalar oneRingMeanValue = 0.0;
-                        for( Geometry::AdjacencyMatrix::InnerIterator A_it( A, u_it.row() ); A_it; ++A_it) {
-                            oneRingMeanValue += u.coeff( A_it.row(), 0 );
-                        }
-                        oneRingMeanValue /= ( Scalar )A.col( u_it.row() ).nonZeros();
-                        tmp.coeffRef( u_it.row(), 0 ) = ( lambda * oneRingMeanValue ) + ( delta.coeff( u_it.row(), 0 ) * ( 1.0 - lambda ) );
-                    }
-                    u.swap( tmp );
-                }
-                return u;
+#include <Engine/RadiumEngine.hpp>
+#include <Engine/Renderer/RenderObject/RenderObject.hpp>
+#include <Engine/Renderer/RenderObject/RenderObjectManager.hpp>
+#include <Engine/Renderer/Material/BlinnPhongMaterial.hpp>
+#include <Engine/Renderer/RenderTechnique/RenderTechnique.hpp>
+
+namespace Ra
+{
+    namespace Gui
+    {
+        MaterialEditor::MaterialEditor( QWidget* parent )
+            : QWidget( parent )
+            , m_visible( false )
+            , m_roIdx( -1 )
+            , m_usable( false )
+        {
+            setupUi( this );
+            typedef void ( QSpinBox::*sigPtr )( int );
+            connect( kdR, static_cast<sigPtr>( &QSpinBox::valueChanged ), this, &MaterialEditor::onKdColorChanged );
+            connect( kdG, static_cast<sigPtr>( &QSpinBox::valueChanged ), this, &MaterialEditor::onKdColorChanged );
+            connect( kdB, static_cast<sigPtr>( &QSpinBox::valueChanged ), this, &MaterialEditor::onKdColorChanged );
+
+            connect( ksR, static_cast<sigPtr>( &QSpinBox::valueChanged ), this, &MaterialEditor::onKsColorChanged );
+            connect( ksG, static_cast<sigPtr>( &QSpinBox::valueChanged ), this, &MaterialEditor::onKsColorChanged );
+            connect( ksB, static_cast<sigPtr>( &QSpinBox::valueChanged ), this, &MaterialEditor::onKsColorChanged );
+
+            connect( exp, static_cast<void ( QDoubleSpinBox::* )( double )>( &QDoubleSpinBox::valueChanged ), this, &MaterialEditor::onExpChanged );
+
+            connect( kdColorWidget, &ColorWidget::newColorPicked, this, &MaterialEditor::newKdColor );
+            connect( ksColorWidget, &ColorWidget::newColorPicked, this, &MaterialEditor::newKsColor );
+        }
+
+        void MaterialEditor::onExpChanged( double v )
+        {
+            if ( m_renderObject && m_usable )
+            {
+                m_material->m_ns = v;
             }
-            
-            
-            
-            void diffuseDelta( const Geometry::AdjacencyMatrix& A, const Delta& delta, const Scalar lambda, const uint iteration, ScalarValue& value ) {
-                ScalarValue u = delta;
-                ScalarValue tmp = delta;
-                uint iter = iteration;
-                iter = iter + ( iter % 2 ); // Why always even?
-                for( uint n = 0; n < iter; ++n ) {
-                    
-                    //        for( Delta::InnerIterator u_it( u, 0 ); u_it; ++u_it) {
-                    //            uint j = u_it.row();
-                    //            const Scalar uv = u_it.value();
-                    //            Scalar oneRingMeanValue = 0.0;
-                    //            uint nonZero = 0;
-                    //            for( Geometry::AdjacencyMatrix::InnerIterator A_it( A, j ); A_it; ++A_it) {
-                    //                uint i = A_it.row();
-                    //                oneRingMeanValue += u.coeff( i, 0 );
-                    //                ++nonZero;
-                    //            }
-                    
-                    //            oneRingMeanValue /= ( Scalar )nonZero; //oneRingMeanValue /= ( Scalar )A.col( j ).nonZeros();
-                    //            tmp.coeffRef( j, 0 ) = ( lambda * oneRingMeanValue ) + ( uv * ( 1.0 - lambda ) );
-                    //        }
-                    //        u.swap( tmp );
-                    
-#pragma omp parallel for
-                    for( int k = 0; k < A.outerSize(); ++k ) {
-                        uint nonZero = 0;
-                        Scalar oneRingMeanValue = 0.0;
-                        for( Geometry::AdjacencyMatrix::InnerIterator it( A, k ); it; ++it) {
-                            const uint i = it.row();
-                            ++nonZero;
-                            oneRingMeanValue += u.coeff( i, 0 );
-                        }
-                        if( nonZero > 0 ) {
-                            oneRingMeanValue /= ( Scalar )nonZero;
-                            Scalar s = ( lambda * oneRingMeanValue ) + ( delta.coeff( k, 0 ) * ( 1.0 - lambda ) );
-#pragma omp critical
-                            {
-                                tmp.coeffRef( k, 0 ) = s;
-                            }
-                        }
-                    }
-#pragma omp barrier
-                    std::swap( u, tmp ); //u.swap( tmp );
-                }
-                std::swap( u, value ); //u.swap( value );
+        }
+
+        void MaterialEditor::onKdColorChanged( int )
+        {
+            kdColorWidget->colorChanged( kdR->value(), kdG->value(), kdB->value() );
+
+            if ( m_renderObject && m_usable )
+            {
+                Core::Color c( kdR->value() / 255.f, kdG->value() / 255.f, kdB->value() / 255.f, 1.0 );
+                m_material->m_kd = c;
             }
-            
-            
-        } // namespace Algorithm
-    } // namespace Core
-} // namespace Ra
+        }
+
+        void MaterialEditor::onKsColorChanged( int )
+        {
+            ksColorWidget->colorChanged( ksR->value(), ksG->value(), ksB->value() );
+
+            if ( m_renderObject && m_usable )
+            {
+                Core::Color c( ksR->value() / 255.f, ksG->value() / 255.f, ksB->value() / 255.f, 1.0 );
+                m_material->m_ks = c;
+            }
+        }
+
+        void MaterialEditor::newKdColor( const QColor& color )
+        {
+            const QSignalBlocker br( kdR );
+            const QSignalBlocker bg( kdG );
+            const QSignalBlocker bb( kdB );
+
+            kdR->setValue( color.red() );
+            kdG->setValue( color.green() );
+            kdB->setValue( color.blue() );
+
+            if ( m_renderObject && m_usable)
+            {
+                Core::Color c( color.redF(), color.greenF(), color.blueF(), 1.0 );
+                m_material->m_kd = c;
+            }
+        }
+
+        void MaterialEditor::newKsColor( const QColor& color )
+        {
+            const QSignalBlocker br( ksR );
+            const QSignalBlocker bg( ksG );
+            const QSignalBlocker bb( ksB );
+
+            ksR->setValue( color.red() );
+            ksG->setValue( color.green() );
+            ksB->setValue( color.blue() );
+
+            if ( m_renderObject && m_usable )
+            {
+                Core::Color c( color.redF(), color.greenF(), color.blueF(), 1.0 );
+                m_material->m_ks = c;
+            }
+        }
+
+        void MaterialEditor::showEvent( QShowEvent* e )
+        {
+            QWidget::showEvent( e );
+
+            m_visible = true;
+
+            m_engine = Engine::RadiumEngine::getInstance();
+            m_roMgr = m_engine->getRenderObjectManager();
+        }
+
+        void MaterialEditor::closeEvent( QCloseEvent* e )
+        {
+            m_visible = false;
+
+            hide();
+        }
+
+        void MaterialEditor::changeRenderObject( Core::Index roIdx )
+        {
+             if ( !m_visible )
+            {
+                return;
+            }
+
+            m_roIdx = roIdx;
+            m_renderObject = m_roMgr->getRenderObject( m_roIdx );
+
+            /// TODO : replace this ugly dynamic_cast by something more static ...
+            m_material = dynamic_cast<Ra::Engine::BlinnPhongMaterial *>( m_renderObject->getRenderTechnique()->getMaterial().get() );
+            if ( m_material == nullptr )
+            {
+                m_usable = false;
+                return;
+            }
+
+            if ( m_renderObject != nullptr )
+            {
+                m_renderObjectName->setText( m_renderObject->getName().c_str() );
+                updateMaterialViz( );
+            }
+        }
+
+        void MaterialEditor::updateMaterialViz( )
+        {
+            const Core::Color kd = m_material->m_kd;
+            const Core::Color ks = m_material->m_ks;
+
+            int kdr = kd.x() * 255, kdg = kd.y() * 255, kdb = kd.y() * 255;
+            int ksr = ks.x() * 255, ksg = ks.y() * 255, ksb = ks.z() * 255;
+
+            const QSignalBlocker bdr( kdR );
+            const QSignalBlocker bdg( kdG );
+            const QSignalBlocker bdb( kdB );
+
+            const QSignalBlocker bsr( ksR );
+            const QSignalBlocker bsg( ksG );
+            const QSignalBlocker bsb( ksB );
+
+            const QSignalBlocker bns( exp );
+
+            kdR->setValue( kdr );
+            kdG->setValue( kdg );
+            kdB->setValue( kdb );
+
+            ksR->setValue( ksr );
+            ksG->setValue( ksg );
+            ksB->setValue( ksb );
+
+            exp->setValue( m_material->m_ns );
+
+            kdColorWidget->colorChanged( kdr, kdg, kdb );
+            ksColorWidget->colorChanged( ksr, ksg, ksb );
+        }
+
+    }
+}
