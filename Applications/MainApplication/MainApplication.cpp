25e80db653425c2f9d5722836e64beb2d0219ba4
nmellado <nmellado0@gmail.com>
2017-12-13 20:30:30 +0100

    Remove PBRT deadcode

--- Applications/MainApplication/MainApplication.cpp
+++ Applications/MainApplication/MainApplication.cpp
@@ -42,9 +42,6 @@
 #ifdef IO_USE_ASSIMP
     #include <IO/AssimpLoader/AssimpFileLoader.hpp>
 #endif
-#ifdef IO_USE_PBRT
-    #include <IO/PbrtLoader/PbrtFileLoader.hpp>
-#endif
 
 
 // Const parameters : TODO : make config / command line options
@@ -148,7 +145,7 @@ namespace Ra
 #else
         config << "disabled" ;
 #endif
-        
+
         LOG( logINFO ) << config.str();
 
         config.str( std::string() );
@@ -186,10 +183,6 @@ namespace Ra
 #ifdef IO_USE_ASSIMP
         m_engine->registerFileLoader( std::shared_ptr<Asset::FileLoaderInterface>(new IO::AssimpFileLoader()) );
 #endif
-#ifdef IO_USE_PBRT
-        m_engine->registerFileLoader( std::shared_ptr<Asset::FileLoaderInterface>(new IO::PbrtFileLoader()) );
-#endif
-
         // Create main window.
         m_mainWindow.reset( new Gui::MainWindow );
         m_mainWindow->show();
