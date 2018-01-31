a816186421df636e16d45926b7cb158a5c9b598b
Hugo Rens <zhugoz.rens@gmail.com>
2018-01-31 15:03:19 +0100

    Make Light a Component (Part. 1)

--- src/Engine/Renderer/Light/Light.hpp
+++ src/Engine/Renderer/Light/Light.hpp
@@ -3,6 +3,7 @@
 
 #include <Engine/RaEngine.hpp>
 #include <Core/Math/LinearAlgebra.hpp>
+#include <Engine/Component/Component.hpp>
 
 namespace Ra
 {
@@ -17,12 +18,15 @@ namespace Ra
     namespace Engine
     {
 
-        class RA_ENGINE_API Light
+        // FIXME (Hugo) To me this class could totally be renamed LightComponent and get a Light struct embedded.
+        // Thoughts are welcome !
+        class RA_ENGINE_API Light : Component
         {
         public:
             enum LightType
             {
                 DIRECTIONAL,
+                POLYGONAL, // Adding this here is a bit bad, since it will rely on a plugin, isn't it ?
                 POINT,
                 SPOT
             };
@@ -30,18 +34,22 @@ namespace Ra
         public:
             RA_CORE_ALIGNED_NEW
 
-            Light( const LightType& type );
+            Light( const LightType& type, const std::string& name = "light" );
             virtual ~Light();
 
             inline const Core::Color& getColor() const;
             inline void setColor( const Core::Color& color );
 
+            // These function will be replaced by their use of a component -> entity
             virtual void setDirection( const Core::Vector3& dir ) {}
             virtual void setPosition( const Core::Vector3& pos ) {}
+            // ...
             
             inline const LightType& getType() const;
 
             virtual void getRenderParameters( RenderParameters& params );
+
+            void initialize() override;
 
         private:
             Core::Color m_color;
