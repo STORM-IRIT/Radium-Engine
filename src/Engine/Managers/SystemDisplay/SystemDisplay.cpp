191eaa3851684c0970fb7ab73501d631f6841b58
dlyr <github@dlyr.fr>
2018-02-14 12:55:04 +0100

    At least one thread for taskQueue, change assert in SystemDisplay. Minor typo in comment.

--- src/Engine/Managers/SystemDisplay/SystemDisplay.cpp
+++ src/Engine/Managers/SystemDisplay/SystemDisplay.cpp
@@ -15,8 +15,6 @@ namespace Ra
         UiComponent* SystemEntity::uiCmp()
         {
             SystemEntity *instance=getInstance();
-            CORE_ASSERT(instance->getNumComponents() == 2 || instance->getNumComponents() == 3,
-                        "This entity should have only two or three components");
             CORE_ASSERT(instance->getTransformAsMatrix() ==
                         Core::Matrix4::Identity(), "Transform has been changed !");
             return static_cast<UiComponent*>(instance->getComponent("UI"));
@@ -25,11 +23,10 @@ namespace Ra
 #ifndef RA_DISABLE_DEBUG_DISPLAY
         DebugComponent* SystemEntity::dbgCmp()
         {
-            CORE_ASSERT(getInstance()->getNumComponents() == 2,  
-                        "This entity should have two components only");
-            CORE_ASSERT(getInstance()->getTransformAsMatrix() == 
+            SystemEntity *instance=getInstance();
+            CORE_ASSERT(instance->getTransformAsMatrix() ==
                         Core::Matrix4::Identity(), "Transform has been changed !");
-            return static_cast<DebugComponent*>(getInstance()->getComponent("Debug"));
+            return static_cast<DebugComponent*>(instance->getComponent("Debug"));
         }
 
         RenderObject* DebugComponent::getRenderObject( Core::Index idx )
