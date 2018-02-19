475214dac97e43c84f3ea8d43abebd550ccc10b7
dlyr <github@dlyr.fr>
2018-02-19 23:59:08 +0100

    Use TopoVector (a.k.a Core::Vector3) as base type for TopologicalMesh.

--- src/Core/Mesh/TopologicalTriMesh/TopologicalMesh.hpp
+++ src/Core/Mesh/TopologicalTriMesh/TopologicalMesh.hpp
@@ -3,32 +3,86 @@
 
 #include <Core/Math/LinearAlgebra.hpp>
 #include <Core/RaCore.hpp>
+#include <OpenMesh/Core/Mesh/PolyMesh_ArrayKernelT.hh>
 #include <OpenMesh/Core/Mesh/TriMesh_ArrayKernelT.hh>
 #include <OpenMesh/Core/Utils/Property.hh>
 
 #include <OpenMesh/Core/Mesh/Traits.hh>
 
-namespace Ra {
-namespace Core {
+namespace Ra
+{
+namespace Core
+{
+
+///\class TopoVector3 : small extension to Vector3 for OpenMesh compatibility
+class TopoVector3 : public Ra::Core::Vector3
+{
+  public:
+    using Ra::Core::Vector3::Vector3;
+    Scalar length() { return norm(); }
+    TopoVector3 vectorize(Scalar v)
+    {
+        (*this)[0] = v;
+        (*this)[1] = v;
+        (*this)[2] = v;
+        return *this;
+    }
+    TopoVector3 normalize()
+    {
+        Ra::Core::Vector3::normalize();
+        return *this;
+    }
+};
+
+Scalar dot(TopoVector3 a, TopoVector3 b);
+TopoVector3 cross(TopoVector3 a, TopoVector3 b);
+}
+}
+
+template <>
+struct OpenMesh::vector_traits<Ra::Core::TopoVector3>
+{
+    /// Type of the vector class
+    typedef typename Ra::Core::Vector3 vector_type;
+
+    /// Type of the scalar value
+    typedef Scalar value_type;
+
+    /// size/dimension of the vector
+    static const size_t size_ = 3;
+
+    /// size/dimension of the vector
+    static size_t size() { return size_; }
+};
+
+namespace Ra
+{
+namespace Core
+{
 
 // Define an OpenMesh TriMesh structure according to
 // http://openmesh.org/Documentation/OpenMesh-2.1-Documentation/mesh_type.html
-// Attributes define data store on structure.
-struct TopologicalMeshTraits : public OpenMesh::DefaultTraits {
-    using Point = OpenMesh::VectorT<Scalar, 3>;
-    using Normal = OpenMesh::VectorT<Scalar, 3>;
+    // Attributes define data store on structure.
 
-    VertexAttributes( OpenMesh::Attributes::Status | OpenMesh::Attributes::Normal );
-    FaceAttributes( OpenMesh::Attributes::Status | OpenMesh::Attributes::Normal );
-    EdgeAttributes( OpenMesh::Attributes::Status );
-    HalfedgeAttributes( OpenMesh::Attributes::Status | OpenMesh::Attributes::Normal );
+struct TopologicalMeshTraits : public OpenMesh::DefaultTraits
+{
+    typedef TopoVector3 Point;
+    typedef TopoVector3 Normal;
+
+        VertexAttributes(OpenMesh::Attributes::Status | OpenMesh::Attributes::Normal);
+        FaceAttributes(OpenMesh::Attributes::Status | OpenMesh::Attributes::Normal);
+        EdgeAttributes(OpenMesh::Attributes::Status);
+        HalfedgeAttributes(OpenMesh::Attributes::Status| OpenMesh::Attributes::Normal);
+
+  public:
+    };
+
+class RA_CORE_API TopologicalMesh : public OpenMesh::PolyMesh_ArrayKernelT<TopologicalMeshTraits>
+{
+    using base = OpenMesh::PolyMesh_ArrayKernelT<TopologicalMeshTraits>;
+    using base::PolyMesh_ArrayKernelT;
 };
-using TopologicalMesh = OpenMesh::TriMesh_ArrayKernelT<TopologicalMeshTraits> RA_CORE_API;
-
-inline Eigen::Map<const Vector3> convertVec3OpenMeshToEigen( const TopologicalMesh::Point& vec ) {
-    return Eigen::Map<const Vector3>( vec.data() );
 }
-} // namespace Core
-} // namespace Ra
+}
 
 #endif // TOPOLOGICALMESH_H
--- src/Core/Mesh/Wrapper/TopologicalMeshConvert.cpp
+++ src/Core/Mesh/Wrapper/TopologicalMeshConvert.cpp
@@ -1,124 +1,141 @@
-#include <Core/Log/Log.hpp>
 #include <Core/Mesh/Wrapper/TopologicalMeshConvert.hpp>
+#include <Core/Log/Log.hpp>
 
 #include <unordered_map>
 
-namespace Ra {
-namespace Core {
+namespace Ra
+{
+    namespace Core
+    {
 
-void MeshConverter::convert( TopologicalMesh& in, TriangleMesh& out ) {
+        void MeshConverter::convert( TopologicalMesh& in, TriangleMesh& out )
+        {
 
-    struct vertexData {
-        Vector3 _vertex;
-        Vector3 _normal;
-    };
-
-    struct comp_vec {
-        bool operator()( const vertexData& lhv, const vertexData& rhv ) const {
-            if ( lhv._vertex[0] < rhv._vertex[0] ||
-                 ( lhv._vertex[0] == rhv._vertex[0] && lhv._vertex[1] < rhv._vertex[1] ) ||
-                 ( lhv._vertex[0] == rhv._vertex[0] && lhv._vertex[1] == rhv._vertex[1] &&
-                   lhv._vertex[2] < rhv._vertex[2] ) )
+            struct vertexData
             {
-                return true;
+                Vector3 _vertex;
+                Vector3 _normal;
+            };
+
+            struct comp_vec
+            {
+                bool operator()( const vertexData& lhv, const vertexData& rhv ) const
+                {
+                    if ( lhv._vertex[0] < rhv._vertex[0] || ( lhv._vertex[0] == rhv._vertex[0] &&
+                                                              lhv._vertex[1] < rhv._vertex[1] ) || ( lhv._vertex[0] == rhv._vertex[0] &&
+                                                                                                     lhv._vertex[1] == rhv._vertex[1] && lhv._vertex[2] < rhv._vertex[2] ) )
+                    {
+                        return true;
+                    }
+                    return false;
+                }
+            };
+
+            out.clear();
+            typedef std::map<vertexData, int, comp_vec> vMap;
+
+            vMap vertexHandles;
+
+            in.request_face_normals();
+            in.request_vertex_normals();
+            in.update_vertex_normals();
+
+            // iterator over all faces
+            unsigned int vertexIndex = 0;
+
+            // out will have at least least n_vertices and n_normals.
+            out.m_vertices.reserve( in.n_vertices() );
+            out.m_normals.reserve( in.n_vertices() );
+            out.m_triangles.reserve( in.n_faces() );
+            
+            for ( TopologicalMesh::FaceIter f_it = in.faces_sbegin(); f_it != in.faces_end(); ++f_it )
+            {
+                vertexData v;
+                int indices[3];
+                int i = 0;
+                // iterator over vertex (thru halfedge to get access to halfedge normals)
+                for ( TopologicalMesh::FaceHalfedgeIter fv_it = in.fh_iter( *f_it ); fv_it.is_valid(); ++fv_it )
+                {
+                    assert( i < 3 );
+                    TopologicalMesh::Point p = in.point( in.to_vertex_handle( *fv_it ) );
+                    TopologicalMesh::Normal n = in.normal( in.to_vertex_handle( *fv_it ) );
+                    v._vertex = p;
+                    v._normal = n;
+
+                    int vi;
+                    vMap::iterator vtr = vertexHandles.find( v );
+                    if ( vtr == vertexHandles.end() )
+                    {
+                        vi = vertexIndex++;
+                        vertexHandles.insert( vtr, vMap::value_type( v, vi ) );
+                        out.m_vertices.push_back( v._vertex );
+                        out.m_normals.push_back( v._normal );
+                    }
+                    else
+                    {
+                        vi = vtr->second;
+                    }
+                    indices[i] = vi;
+                    i ++;
+                }
+                out.m_triangles.emplace_back( indices[0], indices[1], indices[2] );
             }
-            return false;
+            assert( vertexIndex == out.m_vertices.size() );
+
         }
-    };
 
-    out.clear();
-    using vMap = std::map<vertexData, int, comp_vec>;
-
-    vMap vertexHandles;
-
-    in.request_face_normals();
-    in.request_vertex_normals();
-    in.update_vertex_normals();
-
-    // iterator over all faces
-    unsigned int vertexIndex = 0;
-
-    // out will have at least least n_vertices and n_normals.
-    out.m_vertices.reserve( in.n_vertices() );
-    out.m_normals.reserve( in.n_vertices() );
-    out.m_triangles.reserve( in.n_faces() );
-
-    for ( TopologicalMesh::FaceIter f_it = in.faces_sbegin(); f_it != in.faces_end(); ++f_it )
-    {
-        vertexData v;
-        int indices[3];
-        int i = 0;
-        // iterator over vertex (thru halfedge to get access to halfedge normals)
-        for ( TopologicalMesh::FaceHalfedgeIter fv_it = in.fh_iter( *f_it ); fv_it.is_valid();
-              ++fv_it )
+        void MeshConverter::convert( const TriangleMesh& in, TopologicalMesh& out )
         {
-            assert( i < 3 );
-            TopologicalMesh::Point p = in.point( in.to_vertex_handle( *fv_it ) );
-            TopologicalMesh::Normal n = in.normal( in.to_vertex_handle( *fv_it ) );
-            v._vertex = Core::Vector3( p[0], p[1], p[2] );
-            v._normal = Core::Vector3( n[0], n[1], n[2] );
 
-            int vi;
-            vMap::iterator vtr = vertexHandles.find( v );
-            if ( vtr == vertexHandles.end() )
+            struct hash_vec
             {
-                vi = vertexIndex++;
-                vertexHandles.insert( vtr, vMap::value_type( v, vi ) );
-                out.m_vertices.push_back( v._vertex );
-                out.m_normals.push_back( v._normal );
-            } else
-            { vi = vtr->second; }
-            indices[i] = vi;
-            i++;
+                size_t operator()( const Vector3& lvalue ) const
+                {
+                    return lvalue[0] + lvalue[1] + lvalue[2] +
+                           floor( lvalue[0] ) * 1000.f +
+                           floor( lvalue[1] ) * 1000.f +
+                           floor( lvalue[2] ) * 1000.f;
+                }
+            };
+
+            //Delete old data in out mesh
+            out = TopologicalMesh();
+            out.garbage_collection();
+            out.request_vertex_normals();
+            typedef std::unordered_map<Vector3, TopologicalMesh::VertexHandle, hash_vec> vMap;
+            vMap vertexHandles;
+
+            std::vector<TopologicalMesh::VertexHandle> face_vhandles;
+
+            uint num_halfedge = in.m_triangles.size() * 3;
+            for ( unsigned int i = 0; i < num_halfedge; i++ )
+            {
+                Vector3 p = in.m_vertices[in.m_triangles[i / 3][i % 3]];
+                Vector3 n = in.m_normals[in.m_triangles[i / 3][i % 3]];
+
+                vMap::iterator vtr = vertexHandles.find( p );
+                TopologicalMesh::VertexHandle vh;
+                if ( vtr == vertexHandles.end() )
+                {
+                    vh = out.add_vertex( p );
+                    vertexHandles.insert( vtr, vMap::value_type( p, vh ) );
+                    out.set_normal( vh, n );
+                }
+                else
+                {
+                    vh = vtr->second;
+                }
+                face_vhandles.push_back( vh );
+
+                if ( ( ( i + 1 ) % 3 ) == 0 )
+                {
+                    out.add_face( face_vhandles );
+                    face_vhandles.clear();
+                }
+
+            }
+            assert( out.n_faces() == num_halfedge / 3 );
         }
-        out.m_triangles.emplace_back( indices[0], indices[1], indices[2] );
+
     }
-    assert( vertexIndex == out.m_vertices.size() );
 }
-
-void MeshConverter::convert( const TriangleMesh& in, TopologicalMesh& out ) {
-
-    struct hash_vec {
-        size_t operator()( const Vector3& lvalue ) const {
-            return lvalue[0] + lvalue[1] + lvalue[2] + floor( lvalue[0] ) * 1000.f +
-                   floor( lvalue[1] ) * 1000.f + floor( lvalue[2] ) * 1000.f;
-        }
-    };
-
-    // Delete old data in out mesh
-    out = TopologicalMesh();
-    out.garbage_collection();
-    out.request_vertex_normals();
-    using vMap = std::unordered_map<Vector3, TopologicalMesh::VertexHandle, hash_vec>;
-    vMap vertexHandles;
-
-    std::vector<TopologicalMesh::VertexHandle> face_vhandles;
-
-    uint num_halfedge = in.m_triangles.size() * 3;
-    for ( unsigned int i = 0; i < num_halfedge; i++ )
-    {
-        Vector3 p = in.m_vertices[in.m_triangles[i / 3][i % 3]];
-        Vector3 n = in.m_normals[in.m_triangles[i / 3][i % 3]];
-
-        vMap::iterator vtr = vertexHandles.find( p );
-        TopologicalMesh::VertexHandle vh;
-        if ( vtr == vertexHandles.end() )
-        {
-            vh = out.add_vertex( TopologicalMesh::Point( p[0], p[1], p[2] ) );
-            vertexHandles.insert( vtr, vMap::value_type( p, vh ) );
-            out.set_normal( vh, TopologicalMesh::Normal( n[0], n[1], n[2] ) );
-        } else
-        { vh = vtr->second; }
-        face_vhandles.push_back( vh );
-
-        if ( ( ( i + 1 ) % 3 ) == 0 )
-        {
-            out.add_face( face_vhandles );
-            face_vhandles.clear();
-        }
-    }
-    assert( out.n_faces() == num_halfedge / 3 );
-}
-
-} // namespace Core
-} // namespace Ra
