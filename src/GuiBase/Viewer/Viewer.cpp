STORM-IRIT/Radium-Engine

commit f681b330e90a337f423725d866a76e7dc6c3aa8f

Lock mutex before unlocking
This was causing a crash on windows/debug mode

src/GuiBase/Viewer/Viewer.cpp
@@ -495,6 +495,7 @@ namespace Ra
             }
 
             m_currentRenderer = m_renderers[index].get();
+            m_currentRenderer->lockRendering();
             m_currentRenderer->resize( width(), height() );
             m_currentRenderer->unlockRendering();
 

commit adf4ebf9f385d0e5e540c7b10212226c17591d13

Fix potential deadlock introduced by f681b33

src/GuiBase/Viewer/Viewer.cpp
@@ -240,6 +240,7 @@ namespace Ra
         {
             renderer->addLight( m_camera->getLight() );
         }
+        renderer->lockRendering();
     }
 
     void Gui::Viewer::resizeGL( int width_, int height_ )
@@ -495,7 +496,7 @@ namespace Ra
             }
 
             m_currentRenderer = m_renderers[index].get();
-            m_currentRenderer->lockRendering();
+            // renderers in m_renderers are supposed to be locked
             m_currentRenderer->resize( width(), height() );
             m_currentRenderer->unlockRendering();
 