353eac3a6a644ff22aaa04849a7d0f625f5880a5
dlyr <github@dlyr.fr>
2018-03-06 21:42:03 +0100

    Script to format all the code, (de-)install hooks and format files.

---
+++ scripts/install-scripts-linux.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+ROOT=`git rev-parse --show-toplevel`
+
+file_or_link () {
+    if  [ ! -e "$1" ] && [ ! -L "$1" ]; then
+	return 0
+    else
+	return 1
+    fi
+}
+
+if file_or_link "${ROOT}/.git/hooks/pre-commit" ; then
+    echo "create symlink for pre-commit hook";
+    ln -rs "${ROOT}/scripts/pre-commit-clang-format.sh" "${ROOT}/.git/hooks/pre-commit"
+    chmod +x "${ROOT}/scripts/pre-commit-clang-format.sh"
+else
+    echo "pre-commit hook already present, please remove .git/hooks/pre-commit";
+fi
+if file_or_link "${ROOT}/.clang-format" ; then
+    echo "create symlink for clang-format style file";
+    ln -rs "${ROOT}/scripts/clang-format" "${ROOT}/.clang-format"
+else
+    echo "clang-format link already present, please remove .clang-format";
+fi
