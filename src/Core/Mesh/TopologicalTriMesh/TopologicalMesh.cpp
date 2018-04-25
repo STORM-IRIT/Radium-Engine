df29685fd498350f7073b09a857d4075b003de75
dlyr <github@dlyr.fr>
2018-04-25 09:05:26 +0200

    Comply with openmesh syntax and make short func inline.

--- src/Core/Mesh/TopologicalTriMesh/TopologicalMesh.cpp
+++ src/Core/Mesh/TopologicalTriMesh/TopologicalMesh.cpp
@@ -1,13 +1,5 @@
 #include <Core/Mesh/TopologicalTriMesh/TopologicalMesh.hpp>
 
 namespace Ra {
-namespace Core {
-
-Scalar dot( const TopoVector3& a, const TopoVector3& b ) {
-    return a.dot( b );
-}
-TopoVector3 cross( const TopoVector3& a, const TopoVector3& b ) {
-    return a.cross( b );
-}
-} // namespace Core
+namespace Core {} // namespace Core
 } // namespace Ra
