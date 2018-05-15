bd73d558efb3dc72b1c9a64b99cd45d87d94551c
Niluap'M <Mathias.Paulin@irit.fr>
2018-05-16 01:05:03 +0200

    Fix issue #316 "Creating textures"    see documentation of updateParameters in Texture.cpp    see documentation of loadTexture (private) in TextureManager.cpp

--- src/Engine/Renderer/Texture/TextureManager.hpp
+++ src/Engine/Renderer/Texture/TextureManager.hpp
@@ -17,10 +17,13 @@ class Texture;
 
 namespace Ra {
 namespace Engine {
+/**
+ * Describes the content and parameters of a texture.
+ */
 struct TextureData {
     std::string name;
-    int width;
-    int height;
+    int width = -1;
+    int height = -1;
 
     GLenum format = GL_RGB;
     GLenum internalFormat = GL_RGB;
@@ -35,6 +38,9 @@ struct TextureData {
     void* data = nullptr;
 };
 
+/**
+ * Manage Texture loading and registration.
+ */
 class RA_ENGINE_API TextureManager final {
     RA_SINGLETON_INTERFACE( TextureManager );
 
@@ -42,25 +48,57 @@ class RA_ENGINE_API TextureManager final {
     using TexturePair = std::pair<std::string, Texture*>;
 
   public:
+    /** Add a texture giving its name, dimension and content.
+     * Usefull for defining procedural textures
+     */
     TextureData& addTexture( const std::string& name, int width, int height, void* data );
-    Texture* addTexture( const std::string& filename );
+
+    /**
+     * Get or load texture from a file.
+     * The name of the texture is the name of its file
+     * @param filename
+     * @return
+     */
     Texture* getOrLoadTexture( const std::string& filename );
+
+    /**
+     * Get or load a named texture.
+     * The name of the texture might be different of the associated file but the data must be
+     * loaded in the TextureData before calling this method.
+     * @param filename
+     * @return
+     */
     Texture* getOrLoadTexture( const TextureData& data );
 
+    /**
+     * Delete a named texture from the manager
+     * @param filename
+     */
     void deleteTexture( const std::string& filename );
+    /**
+     * Delete a texture from the manager
+     * @param texture
+     */
     void deleteTexture( Texture* texture );
 
-    // Call this method to update given texture
+    /// Call this method to update given texture
     void updateTexture( const std::string& texture, void* data );
 
-    // Called by materials
+    /// Called by materials
     void updateTextures();
 
   private:
     TextureManager();
     ~TextureManager();
 
-  private:
+    /** Load a given filename and return the associated TextureData.
+    * @note : only loads 2D image file for now.
+    * @param filename
+    * @return
+    */
+    TextureData loadTexture( const std::string& filename );
+
+private:
     std::map<std::string, Texture*> m_textures;
     std::map<std::string, TextureData> m_pendingTextures;
     std::map<std::string, void*> m_pendingData;
