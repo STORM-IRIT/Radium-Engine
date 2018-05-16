9a69a772147e4c72013c8010d0243baf7bfb4dfc
Florian Canezin <460844+hoshiryu@users.noreply.github.com>
2018-05-16 15:45:25 +0200

    Improve Skeleton API

--- src/Core/Animation/Handle/Handle.hpp
+++ src/Core/Animation/Handle/Handle.hpp
@@ -9,51 +9,95 @@ namespace Ra {
 namespace Core {
 namespace Animation {
 
+/**
+ * Type for transform names
+ */
 using Label = std::string;
 
 /**
- * The Handle class describe the base handle structure used for deformation.
+ * The Handle class describes the base handle structure used for deformation.
  */
 class RA_CORE_API Handle {
   public:
-    /// ENUM
+    /**
+     * The SpaceType specifies the space the transform lives in.
+     */
     enum class SpaceType { LOCAL, MODEL };
 
-    /// CONSTRUCTOR
-    Handle();                                            // Default constructor
-    Handle( const uint n );                              // Reserve the memory for n transforms
-    Handle( const Handle& handle ) = default;            // Copy constructor
-    Handle& operator=( const Handle& handle ) = default; // Assignment operator
+    Handle();
+    Handle( const uint n );
+    Handle( const Handle& handle ) = default;
+    Handle& operator=( const Handle& handle ) = default;
 
-    /// DESTRUCTOR
-    ~Handle();
+    virtual ~Handle();
 
-    /// SIZE
-    inline virtual uint size() const; // Return the number of transforms
-    virtual void clear();             // Empty the pose
+    /**
+     * @return the number of transformations
+     */
+    inline virtual uint size() const;
 
-    /// SPACE INTERFACE
-    virtual const Pose& getPose( const SpaceType MODE ) const;      // Return the pose in MODE space
-    virtual void setPose( const Pose& pose, const SpaceType MODE ); // Set the MODE space pose
-    virtual const Transform& getTransform( const uint i, const SpaceType MODE )
-        const; // Return the i-th transform matrix of the pose in MODE space
-    virtual void
-    setTransform( const uint i, const Transform& T,
-                  const SpaceType MODE ); // Set the i-th transform of the MODE space pose
+    /**
+     * Clear the transformtions data
+     */
+    virtual void clear();
 
-    /// NAME
-    inline Label getName() const;                // Return the label of the handle
-    inline void setName( const Label& name );    // Set the label of the handle
-    inline Label getLabel( const uint i ) const; // Return the label of the i-th transform
-    inline void setLabel( const uint i, const Label& text ); // Set the label of the i-th transform
+    /**
+     * @return the current pose in the \p MODE space.
+     */
+    virtual const Pose& getPose( const SpaceType MODE ) const;
 
-    /// VARIABLE
-    LocalPose m_pose; // The pose of the handle
+    /**
+     * Set the current pose.
+     * @param pose the new pose
+     * @param MODE the space the psoe lives in
+     */
+    virtual void setPose( const Pose& pose, const SpaceType MODE );
+
+    /**
+     * @return the \p i-th transformation in \p MODE space.
+     */
+    virtual const Transform& getTransform( const uint i, const SpaceType MODE ) const;
+
+    /**
+     * Set the i-th transform to \p T, which is given in the MODE space.
+     */
+    virtual void setTransform( const uint i, const Transform& T, const SpaceType MODE );
+
+    /**
+     * @return the Handle's name
+     */
+    inline Label getName() const;
+
+    /**
+     * Set the Handle's name
+     */
+    inline void setName( const Label& name );
+
+    /**
+     * @return the name of the \p i-th transform.
+     */
+    inline Label getLabel( const uint i ) const;
+
+    /**
+     * Set the name of the \p i-th transform.
+     */
+    inline void setLabel( const uint i, const Label& text );
+
+    /**
+     * The current pose of the Handle
+     */
+    LocalPose m_pose;
 
   protected:
-    /// VARIABLE
-    Label m_name;               // Label of the handle
-    std::vector<Label> m_label; // Label of each transform
+    /**
+     * The Handle's name
+     */
+    Label m_name;
+
+    /**
+     * The names of the transforms.
+     */
+    std::vector<Label> m_label;
 };
 
 } // namespace Animation
