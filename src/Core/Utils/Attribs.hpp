0aab044e6a7d55d658db06a745b5f6fc9f8487db
dlyr <github@dlyr.fr>
2018-06-11 15:58:09 +0200

    Copy Mesh Attrib <> OpenMesh properties.
    attrib itr
    vec3 attr set back and forth
    Use aligned alloc, since we need to ;)
    Change remove attrib behavior

--- src/Core/Utils/Attribs.hpp
+++ src/Core/Utils/Attribs.hpp
@@ -2,10 +2,13 @@
 #define RADIUMENGINE_ATTRIBS_HPP
 
 #include <Core/Containers/VectorArray.hpp>
+#include <Core/Log/Log.hpp>
 #include <Core/RaCore.hpp>
 
-namespace Ra {
-namespace Core {
+namespace Ra
+{
+namespace Core
+{
 
 template <typename T>
 class Attrib;
@@ -14,7 +17,8 @@ class Attrib;
 /// An Attrib is data linked to Vertices of a mesh.
 /// In the future, it is expected to allow automatic binding between the CPU
 /// and the rendered data on the GPU.
-class AttribBase {
+class AttribBase
+{
   public:
     /// attrib name is used to automatic location binding when using shaders.
     virtual ~AttribBase() {}
@@ -28,21 +32,29 @@ class AttribBase {
     bool inline operator==( const AttribBase& rhs ) { return m_name == rhs.getName(); }
 
     template <typename T>
-    inline Attrib<T>& cast() {
+    inline Attrib<T>& cast()
+    {
         return static_cast<Attrib<T>&>( *this );
     }
 
     template <typename T>
-    inline const Attrib<T>& cast() const {
+    inline const Attrib<T>& cast() const
+    {
         return static_cast<const Attrib<T>&>( *this );
     }
+
+    virtual bool isFloat() const = 0;
+    virtual bool isVec2() const = 0;
+    virtual bool isVec3() const = 0;
+    virtual bool isVec4() const = 0;
 
   private:
     std::string m_name;
 };
 
 template <typename T>
-class Attrib : public AttribBase {
+class Attrib : public AttribBase
+{
   public:
     using value_type = T;
     using Container = VectorArray<T>;
@@ -60,12 +72,18 @@ class Attrib : public AttribBase {
     uint getSize() override { return Container::Matrix::RowsAtCompileTime; }
     int getStride() override { return sizeof( typename Container::value_type ); }
 
+    bool isFloat() const override { return std::is_same<float, T>::value; }
+    bool isVec2() const override { return std::is_same<Core::Vector2, T>::value; }
+    bool isVec3() const override { return std::is_same<Core::Vector3, T>::value; }
+    bool isVec4() const override { return std::is_same<Core::Vector4, T>::value; }
+
   private:
     Container m_data;
 };
 
 template <typename T>
-class AttribHandle {
+class AttribHandle
+{
   public:
     typedef T value_type;
     using Container = typename Attrib<T>::Container;
@@ -73,6 +91,16 @@ class AttribHandle {
     /// There is no validity check against the corresponding mesh, but just a
     /// simple test to allow the manipuation of unitialized handles.
     constexpr bool isValid() const { return m_idx != -1; }
+
+    /// compare two handle, there are the same if they both represent the same
+    /// attrib (type and value).
+    template <typename U>
+    bool operator==( const AttribHandle<U>& lhs ) const
+    {
+        return std::is_same<T, U>::value && m_idx == lhs.m_idx;
+    }
+
+    int idx() { return m_idx; }
 
   private:
     int m_idx = -1;
@@ -106,14 +134,18 @@ class AttribHandle {
  * \warning There is no error check on the handles attribute type
  *
  */
-class AttribManager {
+class AttribManager
+{
   public:
     using value_type = AttribBase*;
     using Container = std::vector<value_type>;
 
+    /// const acces to attrib vector
     const Container& attribs() const { return m_attribs; }
+
     /// clear all attribs, invalidate handles !
-    void clear() {
+    void clear()
+    {
         for ( auto a : m_attribs )
         {
             delete a;
@@ -141,7 +173,8 @@ class AttribManager {
      * \warning There is no error check on the attribute type
      */
     template <typename T>
-    inline AttribHandle<T> getAttribHandler( const std::string& name ) const {
+    inline AttribHandle<T> getAttribHandle( const std::string& name ) const
+    {
         auto c = m_attribsIndex.find( name );
         AttribHandle<T> handle;
         if ( c != m_attribsIndex.end() )
@@ -153,52 +186,71 @@ class AttribManager {
 
     /// Get attribute by handle
     template <typename T>
-    inline Attrib<T>& getAttrib( AttribHandle<T> h ) {
+    inline Attrib<T>& getAttrib( AttribHandle<T> h )
+    {
         return *static_cast<Attrib<T>*>( m_attribs[h.m_idx] );
     }
 
     /// Get attribute by handle (const)
     template <typename T>
-    inline const Attrib<T>& getAttrib( AttribHandle<T> h ) const {
+    inline const Attrib<T>& getAttrib( AttribHandle<T> h ) const
+    {
         return *static_cast<Attrib<T>*>( m_attribs[h.m_idx] );
     }
 
     /// Add attribute by name
     template <typename T>
-    AttribHandle<T> addAttrib( const std::string& name ) {
+    AttribHandle<T> addAttrib( const std::string& name )
+    {
         AttribHandle<T> h;
         Attrib<T>* attrib = new Attrib<T>;
         attrib->setName( name );
-        m_attribs.push_back( attrib );
-        h.m_idx = m_attribs.size() - 1;
+
+        auto itr = std::find_if( m_attribs.begin(), m_attribs.end(),
+                                 []( AttribBase* b ) { return b == nullptr; } );
+        if ( itr != m_attribs.end() )
+        {
+
+            *itr = attrib;
+            h.m_idx = std::distance( m_attribs.begin(), itr );
+        }
+        else
+        {
+
+            m_attribs.push_back( attrib );
+            h.m_idx = m_attribs.size() - 1;
+        }
         m_attribsIndex[name] = h.m_idx;
         return h;
     }
 
-    /// Remove attribute by name, invalidate all the handles
-    void removeAttrib( const std::string& name ) {
+    /// Remove attribute by name
+    void removeAttrib( const std::string& name )
+    {
         auto c = m_attribsIndex.find( name );
         if ( c != m_attribsIndex.end() )
         {
             int idx = c->second;
             delete m_attribs[idx];
-            m_attribs.erase( m_attribs.begin() + idx );
+            m_attribs[idx] = nullptr;
+            //            m_attribs.erase( m_attribs.begin() + idx );
             m_attribsIndex.erase( c );
 
             // reindex attribs with index superior to removed index
-            for ( auto& d : m_attribsIndex )
-            {
-                if ( d.second > idx )
-                {
-                    --d.second;
-                }
-            }
+            //            for ( auto& d : m_attribsIndex )
+            //            {
+            //                if ( d.second > idx )
+            //                {
+            //                    --d.second;
+            //                }
+            //            }
         }
     }
 
     /// Remove attribute by handle, invalidate all the handles
     template <typename T>
-    void removeAttrib( AttribHandle<T> h ) {
+    void removeAttrib( AttribHandle<T> h )
+    {
         const auto& att = getAttrib( h ); // check the attribute exists
         removeAttrib( att.getName() );
     }
