24dd573573d01422247de4d5219a4c2e700b3350
hoshiryu <florian.canezin@irit.fr>
2018-07-04 11:07:43 +0200

    Add attribute-aware subdivision (still noraml and attributes computation issues) ;

--- src/Core/Mesh/TopologicalTriMesh/TopologicalMesh.hpp
+++ src/Core/Mesh/TopologicalTriMesh/TopologicalMesh.hpp
@@ -16,15 +16,12 @@
 #include <Eigen/Core>
 #include <Eigen/Geometry>
 
-namespace Ra
-{
-namespace Core
-{
+namespace Ra {
+namespace Core {
 
 class TriangleMesh;
 
-struct TopologicalMeshTraits : OpenMesh::DefaultTraits
-{
+struct TopologicalMeshTraits : OpenMesh::DefaultTraits {
     using Point = Ra::Core::Vector3;
     using Normal = Ra::Core::Vector3;
 
@@ -38,8 +35,7 @@ struct TopologicalMeshTraits : OpenMesh::DefaultTraits
 /// vertex graph, using a half-edge representation.
 ///
 /// This integration is inspired by: https://gist.github.com/Unril/03fa353d0461ed6bd41d
-class RA_CORE_API TopologicalMesh : public OpenMesh::PolyMesh_ArrayKernelT<TopologicalMeshTraits>
-{
+class RA_CORE_API TopologicalMesh : public OpenMesh::PolyMesh_ArrayKernelT<TopologicalMeshTraits> {
   private:
     using base = OpenMesh::PolyMesh_ArrayKernelT<TopologicalMeshTraits>;
     using base::PolyMesh_ArrayKernelT;
@@ -48,6 +44,8 @@ class RA_CORE_API TopologicalMesh : public OpenMesh::PolyMesh_ArrayKernelT<Topol
     std::vector<OpenMesh::HPropHandleT<Vector2>> m_vec2Pph;
     std::vector<OpenMesh::HPropHandleT<Vector3>> m_vec3Pph;
     std::vector<OpenMesh::HPropHandleT<Vector4>> m_vec4Pph;
+
+    friend class LoopSubdivider;
 
   public:
     EIGEN_MAKE_ALIGNED_OPERATOR_NEW
@@ -60,7 +58,7 @@ class RA_CORE_API TopologicalMesh : public OpenMesh::PolyMesh_ArrayKernelT<Topol
     explicit TopologicalMesh( const Ra::Core::TriangleMesh& triMesh );
 
     /// Construct an empty topological mesh
-    explicit TopologicalMesh(){};
+    explicit TopologicalMesh() {}
 
     /// Obtain a triangleMesh from a topological mesh.
     /// This is a costly operation.
