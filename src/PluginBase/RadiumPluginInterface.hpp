f16b92fd7f89fee33a792457f5d782f10da06d07
hoshiryu <florian.canezin@irit.fr>
2018-09-04 11:18:37 +0200

    Add/Rework documentation for RadiumPluginInterface ;

--- src/PluginBase/RadiumPluginInterface.hpp
+++ src/PluginBase/RadiumPluginInterface.hpp
@@ -13,41 +13,44 @@ class QOpenGLContext;
 class QString;
 
 namespace Ra {
+namespace Asset {
+class FileLoaderInterface;
+} // namespace Asset
+
 namespace Engine {
 class RadiumEngine;
-}
+class Renderer;
+} // namespace Engine
+
 namespace GuiBase {
 class SelectionManager;
-}
+} // namespace GuiBase
+
 namespace Gui {
 class PickingManager;
 class Viewer;
-}
+} // namespace Gui
+} // namespace Ra
 
-namespace Engine {
-class Renderer;
-}
-
-namespace Asset {
-class FileLoaderInterface;
-}
-
+namespace Ra {
 /// Data passed to the plugin constructor.
 struct PluginContext {
-    Engine::RadiumEngine* m_engine=nullptr;
-    GuiBase::SelectionManager* m_selectionManager=nullptr;
-    Gui::PickingManager* m_pickingManager=nullptr;
-    Gui::Viewer* m_viewer=nullptr;
+    Engine::RadiumEngine* m_engine = nullptr;
+    GuiBase::SelectionManager* m_selectionManager = nullptr;
+    Gui::PickingManager* m_pickingManager = nullptr;
+    Gui::Viewer* m_viewer = nullptr;
 };
 
 namespace Plugins {
+/// The RadiumPluginInterface define the interface to be implemented by the
+/// RadiumEngine's MainApplication Plugins.
 class RadiumPluginInterface {
   public:
     virtual ~RadiumPluginInterface() {}
 
     /**
      * @brief Pass arguments for plugin initialization.
-     * This method is supposed to create the system (<emph>new</emph> it)
+     * This method is supposed to create the system (\em new it)
      * and then call Ra::Engine::RadiumEngine::registerSystem().
      * Without doing this, your system won't do anything
      * (unless you just want to add UI elements)
@@ -56,7 +59,7 @@ class RadiumPluginInterface {
     virtual void registerPlugin( const PluginContext& context ) = 0;
 
     /**
-     * @brief Tells wether the plugin wants to add a widget
+     * @brief Tells whether the plugin wants to add a widget
      * (inside the UI tab widget) or not. If it does, it must provide
      * a name for it, and getWidget() will be called.
      * @param name Name of the tab to be added, if needed.
@@ -74,7 +77,7 @@ class RadiumPluginInterface {
     virtual QWidget* getWidget() = 0;
 
     /**
-     * @brief Tells wether the plugin wants to add a menu
+     * @brief Tells whether the plugin wants to add a menu
      * or not. If it does, getMenu() will be called.
      * @return True if the plugin wants to add a menu, false otherwise
      */
@@ -88,7 +91,7 @@ class RadiumPluginInterface {
     virtual QMenu* getMenu() = 0;
 
     /**
-     * @brief Tells wether the plugin wants to add actions
+     * @brief Tells whether the plugin wants to add actions
      * or not. If it does, getAction() will be called for each one of them.
      * @param nb the number of action the plugin wants to add.
      * @return True if the plugin wants to add a menu, false otherwise
@@ -101,21 +104,40 @@ class RadiumPluginInterface {
      */
     virtual QAction* getAction( int id ) = 0;
 
+    /**
+     * @brief Tells whether the plugin wants to add Renderers to the Engine.
+     * @return True if the plugin wants to add Renderers.
+     */
     virtual bool doAddRenderer() { return false; }
 
     /**
-     * @brief addRenderers
+     * @brief Creates the Renderers to be added to the Engine.
      *
-     * \param rds the TODO
-     * \warning Allocated renderers are given to the application and
-     * SHOULD not be destroyed by the plugin
+     * \warning Allocated Renderers are given to the application and
+     * SHOULD not be destroyed by the plugin.
      */
     virtual void addRenderers( std::vector<std::shared_ptr<Engine::Renderer>>* /*rds*/ ) {}
 
+    /**
+     * @brief Tells whether the plugin wants to add FileLoaders.
+     * @returnTrue if the plugin wants to add FileLoaders.
+     */
     virtual bool doAddFileLoader() { return false; }
 
+    /**
+     * @brief Creates the FileLoaders to be added.
+     *
+     * \warning Allocated FileLoaders are given to the application and
+     * SHOULD not be destroyed by the plugin.
+     */
     virtual void
     addFileLoaders( std::vector<std::shared_ptr<Asset::FileLoaderInterface>>* /*fl*/ ) {}
+
+    /**
+     * @brief Tells whether the plugin wants to add OpenGL resources.
+     * @return True if the plugin wants to add OpenGL resources.
+     */
+    virtual bool doAddROpenGLInitializer() { return false; }
 
     /**
      * @brief openGlInitialize
@@ -124,22 +146,24 @@ class RadiumPluginInterface {
      * @see https://herbsutter.com/2013/06/05/gotw-91-solution-smart-pointer-parameters/ for the
      * reason a const QOpenGLContext * is given \warning Allocated renderers are given to the
      * application and SHOULD not be destroyed by the plugin
-     * @note Plugins that want to manage OpenGL resources MUST ENSURE at the time they create/acces these resources
-     * that the current openGLContext is the one pass when initializing OpenGL here and stored in the PluginContext ...
-     * @note When this function is called from the application, The active OpenGL Context is the one that will be
-     * used for rendering.
+     * @note Plugins that want to manage OpenGL resources MUST ENSURE at the time
+     * they create/acces these resources that the current openGLContext is the
+     * one pass when initializing OpenGL here and stored in the PluginContext ...
+     * @note When this function is called from the application, The active OpenGL
+     * Context is the one that will be used for rendering.
      * @todo QOpenGLContext the openGLContext as it is redundant with the PluginContext ?
      *
-     * Developers of plugins that need to manage OpenGL resources in a direct way (recomended for plugins that manage
-     * OpenGL resources for rendering or interaction with rendering) must implement this method and memorize the viewer
-     * of the PluginContext. Each time they need to create or destroy an OpenGL resource, they must activate this
-     * context (viewer->makeCurrent()) and realeas it after usage (viewer->doneCurrent())
-     * @see Ra::Gui::Viewer
+     * Developers of plugins that need to manage OpenGL resources in a direct way
+     * (recomended for plugins that manage OpenGL resources for rendering or interaction
+     * with rendering) must implement this method and memorize the viewer of the
+     * PluginContext. Each time they need to create or destroy an OpenGL resource,
+     * they must activate this context (viewer->makeCurrent()) and realeas it
+     * after usage (viewer->doneCurrent()).
+     * @see Ra::Gui::Viewer.
      */
     virtual void openGlInitialize( const PluginContext& context ) {}
-
-    virtual bool doAddROpenGLInitializer() { return false; }
 };
+
 } // namespace Plugins
 } // namespace Ra
 
