32ac4f9628948f40534d525ca647c8dbe24f4a49
hoshiryu <florian.canezin@irit.fr>
2018-09-17 10:18:02 +0200

    Removing DCEL and HalfEdgeData once and for all ; Also fix 2-4 edge split on triangles for TopologicalMesh ;

--- src/Core/Mesh/DCEL/Operations/EdgeSplit.hpp
+++
@@ -1,9 +0,0 @@
-#include <Core/Mesh/DCEL/Dcel.hpp>
-#include <Core/RaCore.hpp>
-namespace Ra {
-namespace Core {
-namespace DcelOperations {
-RA_CORE_API[[deprecated]] void splitEdge( Dcel& dcel, Index edgeIndex, Scalar fraction );
-}
-} // namespace Core
-} // namespace Ra
--- src/Core/Mesh/TopologicalTriMesh/Operations/EdgeSplit.hpp
+++ src/Core/Mesh/TopologicalTriMesh/Operations/EdgeSplit.hpp
@@ -8,10 +8,22 @@
 
 namespace Ra {
 namespace Core {
-namespace TMOperations {
-void splitEdge( TopologicalMesh& topologicalMesh, TopologicalMesh::EdgeHandle edgeHandle,
-                Scalar fraction );
-}
+
+class TMOperations {
+  public:
+    /**
+     * \brief Apply a 2-4 edge split.
+     * \param mesh The mesh on which to apply the edge split.
+     * \param eh The handle to the edge to split.
+     * \param f The interpolation factor to place the new point on the edge.
+     *          Must be in [0,1].
+     * \return True if the edge has been split, false otherwise.
+     * \note Only applies on edges between 3 triangles, and if \p f is in [0,1].
+     * \note Mesh attributes are linearly interpolated on the newly created halfedge.
+     */
+    static bool splitEdge( TopologicalMesh& mesh, TopologicalMesh::EdgeHandle eh, Scalar f );
+};
+
 } // namespace Core
 } // namespace Ra
 
