f5c0980d3c2b6b9374e742b1827199a0e806a944
hoshiryu <florian.canezin@irit.fr>
2018-09-18 11:15:12 +0200

    Replace CameraManipPlugin::load with createCamera ;

--- Plugins/CameraManip/src/CameraManipPlugin.cpp
+++ Plugins/CameraManip/src/CameraManipPlugin.cpp
@@ -10,6 +10,7 @@
 
 #include <Engine/ItemModel/ItemEntry.hpp>
 #include <Engine/Managers/CameraManager/CameraManager.hpp>
+#include <Engine/Managers/EntityManager/EntityManager.hpp>
 
 #include <GuiBase/SelectionManager/SelectionManager.hpp>
 #include <GuiBase/Viewer/TrackballCamera.hpp>
@@ -20,12 +21,17 @@
 
 namespace CameraManipPlugin {
 
-CameraManipPluginC::CameraManipPluginC() : m_selectionManager( nullptr ) {}
+CameraManipPluginC::CameraManipPluginC() :
+    m_widget( nullptr ),
+    m_engine( nullptr ),
+    m_selectionManager( nullptr ),
+    m_viewer( nullptr ) {}
 
 CameraManipPluginC::~CameraManipPluginC() {}
 
 void CameraManipPluginC::registerPlugin( const Ra::PluginContext& context ) {
     // register selection context
+    m_engine = context.m_engine;
     m_selectionManager = context.m_selectionManager;
     m_viewer = context.m_viewer;
     connect( m_selectionManager, &Ra::GuiBase::SelectionManager::currentChanged, this,
@@ -43,8 +49,8 @@ QWidget* CameraManipPluginC::getWidget() {
              &CameraManipPluginC::useSelectedCamera );
     connect( m_widget->ui->m_saveCamera, &QPushButton::clicked, this,
              &CameraManipPluginC::saveCamera );
-    connect( m_widget->ui->m_loadCamera, &QPushButton::clicked, this,
-             &CameraManipPluginC::loadCamera );
+    connect( m_widget->ui->m_createCamera, &QPushButton::clicked, this,
+             &CameraManipPluginC::createCamera );
     return m_widget;
 }
 
@@ -104,69 +110,30 @@ void CameraManipPluginC::saveCamera() {
     outFile << camera->getFOV() << " " << camera->getZNear() << " " << camera->getZFar() << " "
             << camera->getZoomFactor() << " " << camera->getAspect() << std::endl;
     outFile << std::endl;
-    outFile << "#Radium_camera_manip_state" << std::endl;
-    outFile << manip->getTrackballCenter().transpose() << " " << manip->getTrackballRadius()
-            << std::endl;
 }
 
-void CameraManipPluginC::loadCamera() {
-    QSettings settings;
-    QString path = settings.value( "CameraManip::camera_file", QDir::homePath() ).toString();
-    path = QFileDialog::getOpenFileName( nullptr, "Open Camera", path, "*.cam" );
-    if ( path.size() == 0 )
-    {
-        return;
-    }
-    settings.setValue( "CameraManip::camera_file", path );
-
-    std::ifstream inFile( path.toStdString() );
-    if ( !inFile.is_open() )
-    {
-        LOG( logWARNING ) << "Could not open file to load the camera: " << path.toStdString();
-        return;
-    }
-
-    std::string str;
-    int type;
-    Scalar M[16];                     // 4x4 view matrix;
-    Scalar fov, znear, zfar, zoom, a; // camera data
-    Scalar x, y, z, d;                // manip data
-
-    inFile >> str;
-    bool result = !inFile.fail();
-    inFile >> type;
-    result = !inFile.fail();
-    for ( uint i = 0; i < 16; ++i )
-    {
-        inFile >> M[i];
-        result &= !inFile.fail();
-    }
-    inFile >> fov >> znear >> zfar >> zoom >> a >> str >> x >> y >> z >> d;
-    result &= !inFile.fail();
-
-    if ( !result )
-    {
-        LOG( logWARNING ) << "Could not load data from camera file: " << path.toStdString();
-        return;
-    }
-
+void CameraManipPluginC::createCamera() {
+    // Create new entity with camera component only
+    auto camMngr =
+        static_cast<Ra::Engine::CameraManager*>( m_engine->getSystem( "DefaultCameraManager" ) );
+    std::string camName = "CAMERA_" + std::to_string( camMngr->count() );
+    auto entity = m_engine->getEntityManager()->createEntity( camName );
+    Ra::Engine::Camera* cam =
+        new Ra::Engine::Camera( entity, camName, m_viewer->width(), m_viewer->height() );
+    // Copy Camera data
     auto manip = static_cast<Ra::Gui::TrackballCamera*>( m_viewer->getCameraInterface() );
     auto camera = manip->getCamera();
-
-    Ra::Core::Matrix4 frame;
-    frame << M[0], M[1], M[2], M[3], M[4], M[5], M[6], M[7], M[8], M[9], M[10], M[11], M[12], M[13],
-        M[14], M[15];
-    Ra::Core::Transform T( frame );
-    camera->setType( Ra::Engine::Camera::ProjType( type ) );
-    camera->setFrame( T );
-    camera->setFOV( fov );
-    camera->setZNear( znear );
-    camera->setZFar( zfar );
-    camera->setZoomFactor( zoom );
-
-    manip->setCamera( camera );
-    manip->setTrackballRadius( d );
-    manip->setTrackballCenter( Ra::Core::Vector3( x, y, z ) );
+    cam->setType( camera->getType() );
+    cam->setFrame( camera->getFrame() );
+    cam->setFOV( camera->getFOV() );
+    cam->setZNear( camera->getZNear() );
+    cam->setZFar( camera->getZFar() );
+    cam->setZoomFactor( camera->getZoomFactor() );
+    cam->initialize();
+    cam->show( true );
+    // Register entity and camera in Camera manager
+    camMngr->addCamera( cam );
+    camMngr->registerComponent( entity, cam );
 }
 
 void CameraManipPluginC::onCurrentChanged( const QModelIndex& current, const QModelIndex& prev ) {
