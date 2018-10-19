e8016a6da96cd96011bdf1318f2e891dec01ea32
Mathias Paulin <mathias.paulin@irit.fr>
2018-10-19 16:04:55 +0200

    sRGB gamma correction

--- Shaders/HdrToLdr/Hdr2Ldr.frag.glsl
+++ Shaders/HdrToLdr/Hdr2Ldr.frag.glsl
@@ -4,11 +4,24 @@ in vec2 varTexcoord;
 
 uniform sampler2D screenTexture;
 uniform float gamma;
+
+// from sRGB color space specification
+float tosRGB(float c) {
+  if (c <= 0.0031308) {
+    return 12.92*c;
+  } else {
+    return 1.055*pow(c, 1/gamma) - 0.055;
+  }
+}
 void main()
 {
     vec2 size = vec2(textureSize(screenTexture, 0));
     vec4 pixel = vec4(texelFetch(screenTexture, ivec2(varTexcoord.xy * size), 0));
-    if (pixel.a != 0)
-        pixel.rgb=pow(pixel.rgb, vec3(1/gamma));
+    // Pixel is linear, transform to sRGB
+    if (pixel.a != 0) {
+        pixel.r = tosRGB(pixel.r);
+        pixel.g = tosRGB(pixel.g);
+        pixel.b = tosRGB(pixel.b);
+    }
     fragColor = pixel;
 }
