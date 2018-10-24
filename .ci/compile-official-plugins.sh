2ffc710aaee7b6f8bd04a930762cf7fcfbfcc359
nmellado <nmellado0@gmail.com>
2018-10-24 14:08:30 +0200

    First draft of the cron-based official plugin CI system

---
+++ .ci/compile-official-plugins.sh
@@ -0,0 +1,41 @@
+declare -a repositories=(
+	"https://github.com/STORM-IRIT/Radium-PluginExample.git"
+	)
+
+# \param $1 input a repository url
+function getRepositoryName()
+{
+	local mails=$(echo $1 | tr "/" "\n")
+	local supername="${mails[-1]}"
+	return "${supername::-4}"
+}
+
+# Stop on error
+set -e
+
+cd $CHECKOUT_PATH
+
+mkdir plugins
+cd plugins
+
+ # Loop over plugins and: fetch, compile
+for repoUrl in "${repositories[@]}"
+do
+   local repoName=getRepositoryName $repoUrl
+   echo "Processing $repoName"
+
+   # fetch repo
+   git clone $repoUrl $repoName
+   cd $repoName
+   git submodule init
+   git submodule update --recursive
+
+   # build plugin
+   mkdir build
+   cd build
+
+   # \FIXME the CMAKE_MODULE_PATH needs to be updated once the installation procedure is working
+   cmake .. -DCMAKE_C_COMPILER=$CC -DCMAKE_CXX_COMPILER=$CXX -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DCMAKE_PREFIX_PATH=${PREFIX_PATH} -DCMAKE_MODULE_PATH=$CHECKOUT_PATH/Radium-Engine/cmake
+   make -j 4
+
+done
