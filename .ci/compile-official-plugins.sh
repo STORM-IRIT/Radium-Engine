0e9ae4f2992ca5a5e7950f2ac7081647532f8426
Nicolas Mellado <nmellado0@gmail.com>
2018-10-25 09:12:54 +0200

    Add mechanism to detect when plugins compilation fails
    Also, build plugins in folder with unique name

--- .ci/compile-official-plugins.sh
+++ .ci/compile-official-plugins.sh
@@ -17,8 +17,11 @@ set -e
 
 cd $CHECKOUT_PATH
 
-mkdir plugins
-cd plugins
+NEW_UUID=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+PLUGIN_FOLDER="plugins-$NEW_UUID"
+
+mkdir $PLUGIN_FOLDER
+cd $PLUGIN_FOLDER
 
  # Loop over plugins and: fetch, compile
 for repoUrl in "${repositories[@]}"
@@ -40,4 +43,10 @@ do
    cmake .. -DCMAKE_C_COMPILER=$CC -DCMAKE_CXX_COMPILER=$CXX -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DCMAKE_PREFIX_PATH=${PREFIX_PATH} -DCMAKE_MODULE_PATH=$CHECKOUT_PATH/Radium-Engine/cmake
    make -j 4
 
+   echo "Plugin $repoName done"
 done
+
+export SUPPORTED_PLUGINS_BUILD_OK="true"
+
+echo "All plugins have been compiled."
+
