c5a22f5669788b516978b5f4f8fa8326aa5ca61b
hoshiryu <florian.canezin@irit.fr>
2019-04-23 10:41:52 +0200

    Add STBS skinning technique ;

---
+++ src/Core/Animation/StretchableTwistableBoneSkinning.hpp
@@ -0,0 +1,45 @@
+#ifndef RADIUMENGINE_STBS_HPP
+#define RADIUMENGINE_STBS_HPP
+
+#include <Core/Animation/HandleWeight.hpp>
+#include <Core/Animation/Pose.hpp>
+#include <Core/Animation/Skeleton.hpp>
+#include <Core/Containers/AlignedStdVector.hpp>
+#include <Core/Containers/VectorArray.hpp>
+#include <Core/Math/DualQuaternion.hpp>
+#include <Core/Types.hpp>
+
+namespace Ra {
+namespace Core {
+namespace Animation {
+
+using DQList = AlignedStdVector<DualQuaternion>;
+
+/** \name STBS
+ * Formula from: "Stretchable, Twistable Bones For Skeletal Shape Deformation" 2011 - Jacobson &
+ * Sorkine.
+ */
+/// \{
+
+/**
+ * Apply the linear interpolation of transformations to the vertices of inMesh (STBS version).
+ */
+void RA_CORE_API linearBlendSkinningSTBS( const Vector3Array& inMesh, const Pose& pose,
+                                          const Skeleton& poseSkel, const Skeleton& restSkel,
+                                          const WeightMatrix& weightLBS,
+                                          const WeightMatrix& weightSTBS, Vector3Array& outMesh );
+
+/**
+ * Computes the dual quaternions from the given pose, skeleton states and sets of skinning weights
+ * (STBS version).
+ */
+void RA_CORE_API computeDQSTBS( const Pose& pose, const Skeleton& poseSkel,
+                                const Skeleton& restSkel, const WeightMatrix& weight,
+                                const WeightMatrix& weightSTBS, DQList& DQ );
+/// \}
+
+} // namespace Animation
+} // namespace Core
+} // namespace Ra
+
+#endif // RADIUMENGINE_STBS_HPP
