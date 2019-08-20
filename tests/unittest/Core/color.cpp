8880cf8979857c798fed0461e1f7bf770e3adb38
Nicolas Mellado <nmellado0@gmail.com>
2019-08-20 16:48:09 +0200

    Fix class color and its documentation
    Update tests

--- tests/unittest/Core/color.cpp
+++ tests/unittest/Core/color.cpp
@@ -1,64 +1,58 @@
 #include <Core/Utils/Color.hpp>
-#include <Tests.hpp>
+#include <catch2/catch.hpp>
 
-#include <algorithm>
-#include <functional>
-#include <random>
-
-namespace Ra {
-namespace Testing {
-
-void run() {
-    using namespace Core::Utils;
-    Color color1 = Color::Red(), color2 = Color::Green();
-    Color white = color1 + color2 + Color::Blue(); // white = 1,1,1,3
-
-    RA_VERIFY( white.rgb().isApprox( Color::White().rgb() ), "Invalid color presets" );
+TEST_CASE( "Core/Utils/Color", "[Core][Core/Utils][Color]" ) {
+    using namespace Ra::Core::Utils;
 
     Scalar alpha = 0.5;
-    // interpolate rgba
+
+    Color color1 = Color::Red(), color2 = Color::Green();
+    Color white =
+        Color::fromRGB( color1.rgb() + color2.rgb() + Color::Blue().rgb() ); // white = 1,1,1,1
     Color grey = ( alpha * Color::Black() + ( 1 - alpha ) * white );
-    RA_VERIFY( grey.rgb().isApprox( Color::Grey( 0.5 ).rgb() ),
-               "Invalid color presets or interpolation 1" );
 
-    // interpolate rgb
-    grey = Color::fromRGB( alpha * Color::Black().rgb() + ( 1 - alpha ) * white.rgb() );
-    RA_VERIFY( grey.isApprox( Color::Grey( 0.5 ) ), "Invalid color presets or interpolation 2" );
+    SECTION( "Test color presets" ) {
+        REQUIRE( white.rgb().isApprox( Color::White().rgb() ) );
+        REQUIRE( white.hasValidAlpha() );
 
-    grey =
-        Color::fromRGB( alpha * Color::Black().rgb() + ( 1 - alpha ) * white.rgb(), Scalar( 1 ) );
-    RA_VERIFY( grey.isApprox( Color::Grey( 0.5 ) ), "Invalid color presets or interpolation 3" );
+        Color white2 = color1 + color2 + Color::Blue(); // white = 1,1,1,3
+        REQUIRE( !white2.hasValidAlpha() );
 
-    Color grey2 = Color::fromRGB( Eigen::Matrix<Scalar, 3, 1>::Constant( 0.5 ) );
-    RA_VERIFY( grey.isApprox( grey2 ), "Conversion from RBG failed" );
-
-    Color grey3;
-    grey3 << .5, .5, .5, 1.;
-    RA_VERIFY( grey.isApprox( grey3 ), "Conversion from comma operator failed" );
-
-    uint32_t half = 0.5 * 255;
-    uint32_t one = 255;
-    auto rgba = ( half << 24 ) | ( half << 16 ) | ( half << 8 ) | ( one << 0 );
-    auto argb = ( one << 24 ) | ( half << 16 ) | ( half << 8 ) | ( half << 0 );
-    RA_VERIFY( grey3.toRGBA32() == rgba, "Conversion to RGBA32 failed" );
-    RA_VERIFY( grey3.toARGB32() == argb, "Conversion to ARGB32 failed" );
-}
-} // namespace Testing
-} // namespace Ra
-
-int main( int argc, const char** argv ) {
-    using namespace Ra;
-
-    if ( !Testing::init_testing( 1, argv ) )
-    {
-        return EXIT_FAILURE;
+        REQUIRE( grey.isApprox( Color::Grey( 0.5 ) ) );
     }
 
-#pragma omp parallel for
-    for ( int i = 0; i < Testing::g_repeat; ++i )
-    {
-        CALL_SUBTEST( ( Testing::run() ) );
-    }
+    SECTION( "Test color presets and interpolation" ) {
 
-    return EXIT_SUCCESS;
+        // interpolate rgba
+        REQUIRE( grey.rgb().isApprox( Color::Grey( 0.5 ).rgb() ) );
+
+        // interpolate rgb
+        grey = Color::fromRGB( alpha * Color::Black().rgb() + ( 1 - alpha ) * white.rgb() );
+        REQUIRE( grey.isApprox( Color::Grey( 0.5 ) ) );
+
+        grey = Color::fromRGB( alpha * Color::Black().rgb() + ( 1 - alpha ) * white.rgb(),
+                               Scalar( 1 ) );
+        REQUIRE( grey.isApprox( Color::Grey( 0.5 ) ) );
+    }
+    SECTION( "Test color conversion" ) {
+        Color grey2 = Color::fromRGB( Eigen::Matrix<Scalar, 3, 1>::Constant( 0.5 ) );
+
+        // Conversion from RBG failed
+        REQUIRE( grey.isApprox( grey2 ) );
+
+        Color grey3;
+        grey3 << .5, .5, .5, 1.;
+        // Conversion from comma operator failed
+        REQUIRE( grey.isApprox( grey3 ) );
+
+        uint32_t half = uint32_t( 0.5 * 255 );
+        uint32_t one  = 255;
+        auto rgba     = ( half << 24 ) | ( half << 16 ) | ( half << 8 ) | ( one << 0 );
+        auto argb     = ( one << 24 ) | ( half << 16 ) | ( half << 8 ) | ( half << 0 );
+
+        // Conversion to RGBA32 failed
+        REQUIRE( grey3.toRGBA32() == rgba );
+        // Conversion to ARGB32 failed
+        REQUIRE( grey3.toARGB32() == argb );
+    }
 }
