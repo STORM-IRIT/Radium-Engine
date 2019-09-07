92cb7b751a9f6df73ff4cc49200c1259d55fa740
dlyr <github@dlyr.fr>
2019-09-07 07:50:52 +0200

    Add observer utility function.
    Also improove observer doc, and test.

--- src/Core/Utils/Observable.hpp
+++ src/Core/Utils/Observable.hpp
@@ -9,34 +9,63 @@ namespace Ra {
 namespace Core {
 namespace Utils {
 
-/// Simple observable implementation with void observer( Args... ) notification
+/// Simple observable implementation with void observer(Args...) notification.
+/// Typical usage is to derive from Observer with the intended \p Args as in
+/// \code{.cpp}
+/// class MyClass : public Oberservable<int> [...]
+/// MyClass c;
+/// c.attach(functor)
+/// \endcode
+/// Also a class can have Observable members, than act as slots
+/// \code{.cpp}
+/// class MyClass {
+/// public :
+///   Oberservable<int> slot0;
+///   Oberservable<> slot1;
+///   void f(){ slot0.notify(10); }
+/// };
+/// MyClass c;
+/// c.slot0.attach(functor)
+/// c.slot1.attach(functor)
+/// \endcode
+
 template <typename... Args>
 class Observable
 {
   public:
     /// Observer functor type
     using Observer = std::function<void( Args... )>;
-    Observable()   = default;
 
-    /// attach an \p observer that will be call on subsecant notify
-    /// \return an unique int to identify the observer, could be used to pass to Obeservable::detach
+    /// Default constructor ... do nothing ;)
+    Observable() = default;
+
+    /// Attach an \p observer that will be call on subsecant call to notify()
+    /// \return An unique int to identify the observer, could be used to pass to Obeservable::detach
     int attach( Observer observer ) {
         m_observers.insert( std::make_pair( ++m_currentId, observer ) );
         return m_currentId;
     }
 
-    /// notify (call) each attached observer
-    void notify( Args... p ) {
+    /// Utility function that extract the observer from a class member.
+    /// Attach an \p observer that will be call on subsecant call to notify()
+    /// \return An unique int to identify the observer, could be used to pass to Obeservable::detach
+    template <typename T>
+    int attachMember( T* object, void ( T::*observer )( Args... ) ) {
+        return attach( std::bind( observer, object, std::placeholders::_1 ) );
+    }
+
+    /// Notify (i.e. call) each attached observer with argument \p p
+    void notify( Args... p ) const {
         for ( const auto& o : m_observers )
             o.second( std::forward<Args>( p )... );
     }
 
-    /// detach all observers
+    /// Detach all observers
     void detachAll() { m_observers.clear(); }
 
-    /// detach the \p observerId, observerId must have been saved from a
+    /// Detach the \p observerId, observerId must have been saved from a
     /// previous call to attach
-    void detach( int id ) { m_observers.erase( id ); }
+    void detach( int observerId ) { m_observers.erase( observerId ); }
 
   private:
     std::map<int, Observer> m_observers;
--- tests/CoreTests/src/observer.cpp
+++ tests/CoreTests/src/observer.cpp
@@ -9,11 +9,16 @@ using Ra::Core::Utils::Observable;
 class ObservableTest : public Observable<>
 {};
 
+class ObservableTest2 : public Observable<int>
+{};
+
 class A
 {
   public:
     void f() { m_a++; }
+    void f2( int a ) { m_a = a; }
     static void g() { m_b++; }
+    static void g2( int b ) { m_b = b; }
 
     int m_a{0};
     static int m_b;
@@ -23,11 +28,13 @@ int A::m_b = 0;
 
 void run() {
     ObservableTest test;
+    ObservableTest2 test2;
     A a;
     int c  = 0;
     A::m_b = 0;
 
-    using Observer = std::function<void( void )>;
+    using Observer  = std::function<void( void )>;
+    using Observer2 = std::function<void( int )>;
 
     auto bf = std::bind( &A::f, &a );
 
@@ -49,7 +56,7 @@ void run() {
 
     RA_VERIFY( c == 2, "Notify failed." );
     RA_VERIFY( a.m_a == 2, "Notify failed." );
-    RA_VERIFY( A::m_b == 1, "Notify failed." );
+    RA_VERIFY( A::m_b == 1, "Detach failed." );
 
     test.attach( A::g );
     test.attach( A::g );
@@ -60,12 +67,36 @@ void run() {
     RA_VERIFY( c == 4, "Notify failed." );
     RA_VERIFY( a.m_a == 4, "Notify failed." );
     RA_VERIFY( A::m_b == 3, "Notify failed." );
+
     test.detachAll();
     test.notify();
 
-    RA_VERIFY( c == 4, "Notify failed." );
-    RA_VERIFY( a.m_a == 4, "Notify failed." );
-    RA_VERIFY( A::m_b == 3, "Notify failed." );
+    RA_VERIFY( c == 4, "DetachAll failed." );
+    RA_VERIFY( a.m_a == 4, "DetachAll failed." );
+    RA_VERIFY( A::m_b == 3, "DetachAll failed." );
+
+    test2.attachMember( &a, &A::f2 );
+    test2.attach( &A::g2 );
+    test2.attach( [&c]( int pc ) { c = pc; } );
+
+    test2.notify( 5 );
+
+    RA_VERIFY( c == 5, "Notify failed." );
+    RA_VERIFY( a.m_a == 5, "Notify failed." );
+    RA_VERIFY( A::m_b == 5, "Notify failed." );
+
+    test2.notify( 6 );
+
+    RA_VERIFY( c == 6, "Notify failed." );
+    RA_VERIFY( a.m_a == 6, "Notify failed." );
+    RA_VERIFY( A::m_b == 6, "Notify failed." );
+
+    test2.detachAll();
+    test2.notify( 7 );
+
+    RA_VERIFY( c == 6, "DetachAll failed." );
+    RA_VERIFY( a.m_a == 6, "DetachAll failed." );
+    RA_VERIFY( A::m_b == 6, "DtachAll failed." );
 }
 
 } // namespace Testing
