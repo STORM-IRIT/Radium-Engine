406913d1ff8714777cb44427ddd1cb3d3d50d562
nmellado <nmellado0@gmail.com>
2019-09-12 11:18:41 +0200

    Add PluginBase to buildchain

---
+++ cmake/filelistPluginBase.cmake
@@ -0,0 +1,14 @@
+
+set( pluginbase_sources
+    RadiumPluginInterface.cpp
+    )
+
+set( pluginbase_headers
+    PluginContext.hpp
+    RadiumPluginInterface.hpp
+    RaPluginBase.hpp
+    )
+
+set( pluginbase_inlines
+    )
+
