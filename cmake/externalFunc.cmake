95a85de1717524035301316e466ad3ea7b08e029
nmellado <nmellado0@gmail.com>
2019-09-12 16:48:34 +0200

    Add parallel builds for externals

--- cmake/externalFunc.cmake
+++ cmake/externalFunc.cmake
@@ -7,12 +7,17 @@ macro(addExternalFolder NAME FOLDER )
         COMMAND ${CMAKE_COMMAND} -E make_directory ${CMAKE_CURRENT_BINARY_DIR}/external/cmake
     )
 
-    message(STATUS "[addExternalFolder] Run cmake with options ${ARGN}")
+    if( "${ARGN}" )
+        message(STATUS "[addExternalFolder] Configure cmake project with options ${ARGN}")
+    else ()
+        message(STATUS "[addExternalFolder] Configure cmake project")
+    endif()
+
     execute_process(
         COMMAND ${CMAKE_COMMAND} ${FOLDER}
             -DCMAKE_GENERATOR=${CMAKE_GENERATOR}
-                    -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
-                    -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX}
+            -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
+            -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX}
             -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}
             -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
             -DCMAKE_CXX_FLAGS=${CMAKE_CXX_FLAGS}
@@ -22,7 +27,6 @@ macro(addExternalFolder NAME FOLDER )
             -DCMAKE_SHARED_LINKER_FLAGS=${CMAKE_SHARED_LINKER_FLAGS}
             -DCMAKE_PREFIX_PATH=${CMAKE_PREFIX_PATH}
             -DCMAKE_MODULE_PATH=${CMAKE_MODULE_PATH}
-            -DCMAKE_BUILD_PARALLEL_LEVEL=${CMAKE_BUILD_PARALLEL_LEVEL}
             ${ARGN}
         WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/external
     )
@@ -35,10 +39,26 @@ macro(addExternalFolder NAME FOLDER )
             message(INFO "[addExternalFolder] Enable compatibility mode for VS Generator" )
     endif()
 
+    if( ${CMAKE_VERSION} VERSION_GREATER_EQUAL 3.12 )
     execute_process(
-        COMMAND ${CMAKE_COMMAND} --build . --config ${CMAKE_BUILD_TYPE} --target ${RadiumExternalMakeTarget}
+        COMMAND ${CMAKE_COMMAND} --build . -j ${RADIUM_BUILD_EXTERNAL_PARALLEL_LEVEL} --config ${CMAKE_BUILD_TYPE} --target ${RadiumExternalMakeTarget}
         WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/external
-    )
+        )
+    else()
+        set( TOOL_OPTIONS "" )
+        if( ${generator_lower} MATCHES "|makefile|" OR ${generator_lower} MATCHES "|ninja|" )
+          set( TOOL_OPTIONS -- -j${RADIUM_BUILD_EXTERNAL_PARALLEL_LEVEL} )
+        elseif( ${generator_lower} MATCHES "|visual|" )
+          set( TOOL_OPTIONS -- /m )
+        else()
+          message(WARNING "[addExternalFolder] Parallel build are not supported with your generator ${CMAKE_GENERATOR}")
+        endif()
+        execute_process(
+          COMMAND ${CMAKE_COMMAND} --build . --config ${CMAKE_BUILD_TYPE} --target ${RadiumExternalMakeTarget} ${TOOL_OPTIONS}
+          WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/external
+          )
+    endif()
+
     message(STATUS "[addExternalFolder] Build ended")
 
     #Configure package
