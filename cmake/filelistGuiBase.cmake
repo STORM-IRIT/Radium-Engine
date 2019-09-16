e268d447b8be09aec934807c4369a30f84c94fb4
Nicolas Mellado <nmellado0@gmail.com>
2019-09-16 17:00:16 +0200

    Add Guibase to builchain

---
+++ cmake/filelistGuiBase.cmake
@@ -0,0 +1,51 @@
+
+set( guibase_sources
+    SelectionManager/SelectionManager.cpp
+    TimerData/FrameTimerData.cpp
+    TransformEditor/TransformEditor.cpp
+    TreeModel/EntityTreeModel.cpp
+    TreeModel/TreeModel.cpp
+    Utils/KeyMappingManager.cpp
+    Utils/Keyboard.cpp
+    Utils/PickingManager.cpp
+    Viewer/Gizmo/Gizmo.cpp
+    Viewer/Gizmo/GizmoManager.cpp
+    Viewer/Gizmo/RotateGizmo.cpp
+    Viewer/Gizmo/ScaleGizmo.cpp
+    Viewer/Gizmo/TranslateGizmo.cpp
+    Viewer/CameraManipulator.cpp
+    Viewer/TrackballCameraManipulator.cpp
+    Viewer/FlightCameraManipulator.cpp
+    Viewer/Viewer.cpp
+    Viewer/WindowQt.cpp
+    BaseApplication.cpp
+    )
+
+set( guibase_headers
+    SelectionManager/SelectionManager.hpp
+    TimerData/FrameTimerData.hpp
+    TransformEditor/TransformEditor.hpp
+    TreeModel/EntityTreeModel.hpp
+    TreeModel/TreeModel.hpp
+    Utils/KeyMappingManager.hpp
+    Utils/Keyboard.hpp
+    Utils/PickingManager.hpp
+    Utils/qt_utils.hpp
+    Viewer/Gizmo/Gizmo.hpp
+    Viewer/Gizmo/GizmoManager.hpp
+    Viewer/Gizmo/RotateGizmo.hpp
+    Viewer/Gizmo/ScaleGizmo.hpp
+    Viewer/Gizmo/TranslateGizmo.hpp
+    Viewer/CameraManipulator.hpp
+    Viewer/TrackballCameraManipulator.hpp
+    Viewer/FlightCameraManipulator.hpp
+    Viewer/Viewer.hpp
+    Viewer/WindowQt.hpp
+    BaseApplication.hpp
+    MainWindowInterface.hpp
+    RaGuiBase.hpp
+    )
+
+set( guibase_inlines
+    )
+
