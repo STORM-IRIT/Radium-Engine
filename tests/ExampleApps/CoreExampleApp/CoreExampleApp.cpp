a1a30363e80f76ccb89bff44117127cfc3aee026
Mathias Paulin <mathias.paulin@irit.fr>
2019-10-29 22:20:33 +0100

    CoreExampleApp prints the directory returned by resource locator.

--- tests/ExampleApps/CoreExampleApp/CoreExampleApp.cpp
+++ tests/ExampleApps/CoreExampleApp/CoreExampleApp.cpp
@@ -1,5 +1,6 @@
 #include <Core/Geometry/MeshPrimitives.hpp>
 #include <Core/Geometry/TopologicalMesh.hpp>
+#include <Core/Resources/Resources.hpp>
 #include <Core/Utils/Log.hpp>
 
 int main( int /*argc*/, char** /*argv*/ ) {
@@ -8,5 +9,7 @@ int main( int /*argc*/, char** /*argv*/ ) {
     auto box  = Geometry::makeSharpBox( {1., 1., 1.} );
     auto topo = Geometry::TopologicalMesh( box );
 
+    auto resourcesPaths = Resources::getBaseDir();
+    LOG( Utils::logINFO ) << "Used Radium Core lib : " << resourcesPaths;
     LOG( Utils::logINFO ) << topo.has_vertex_texcoords1D();
 }
