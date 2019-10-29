e895cc048937ff6d631eb4c57ecab615ac14c9db
Mathias Paulin <mathias.paulin@irit.fr>
2019-10-29 22:19:32 +0100

    Core - relocatable resources: add cpplocate external dependency and implement ResourceLocator

--- src/Core/Resources/Resources.hpp
+++ src/Core/Resources/Resources.hpp
@@ -12,15 +12,28 @@ namespace Core {
  *    - default shaders
  *    - ...
  *
- *    @todo define and implement a portable interface for resource management.
+ *   @todo ensure that resources are installed or linked in the parent directory of the
+ * libRadiumCore dynamic library.
+ *
+ *   @todo Generalize the locator such that one can find an offsetted directory from a dynamic
+ *   library containing an arbitrary symbol. (easy with cpplocate)
  */
 namespace Resources {
 /// Get the base prefix to access the Radium resources in a filesystem.
-RA_CORE_API const char* getBaseDir();
+RA_CORE_API std::string getBaseDir();
 
 // allow to manage several resource locators
 RA_CORE_API class ResourcesLocator
-{};
+{
+  public:
+    /// Construct a resource locator tha will offset the path to the dynamic library that contains
+    /// the given symbol by the fgiven offset.
+    explicit ResourcesLocator( void* symbol, const std::string& offset = "../" );
+    const std::string& getBasePath();
+
+  private:
+    std::string m_basePath;
+};
 } // namespace Resources
 } // namespace Core
 } // namespace Ra
