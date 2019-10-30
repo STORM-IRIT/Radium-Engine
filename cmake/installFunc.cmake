294d1689a63ad85a80b27bd9c5731881f9f18095
Mathias Paulin <mathias.paulin@irit.fr>
2019-10-30 12:46:28 +0100

    Engine resources are installed in the Resources directory and linked to the buildtree so that an application might be executed from the buildtree or after install

--- cmake/installFunc.cmake
+++ cmake/installFunc.cmake
@@ -13,9 +13,12 @@ endmacro()
 #      -> install in bin/Shaders/foo.glsl
 # You must pass parameter using quotes
 macro( installLibShaders FILES )
+    # build configuration : link shaders to Resources/Shaders
+    file(MAKE_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}/../Resources")
+    file(CREATE_LINK ${RADIUM_SHADERS_DIR} "${CMAKE_CURRENT_BINARY_DIR}/../Resources/Shaders" COPY_ON_ERROR SYMBOLIC)
     foreach ( file ${FILES} )
         get_filename_component( file_dir ${file} DIRECTORY )
-        install( FILES ${RADIUM_SHADERS_DIR}/${file} DESTINATION bin/Shaders/${file_dir} )
-#        file( COPY ${RADIUM_SHADERS_DIR}/${file} DESTINATION ../../tests/ExampleApps/Shaders/${file_dir})
+        # installed library
+        install(FILES ${RADIUM_SHADERS_DIR}/${file} DESTINATION Resources/Shaders/${file_dir})
     endforeach()
 endmacro()
