0142275c5598a63d192087d09dcfe45bc0f05710
Mathias Paulin <mathias.paulin@irit.fr>
2019-10-30 15:40:31 +0100

    cmake general resource installation function

--- cmake/installFunc.cmake
+++ cmake/installFunc.cmake
@@ -7,18 +7,53 @@ macro( installLibHeaders TARGET_DIR_NAME FILES )
     endforeach()
 endmacro()
 
-# Copy shaders to buid and install directories
-# installLibShaders "foo.glsl"
-#      -> copy to build/tests/ExampleApps/Shaders
-#      -> install in bin/Shaders/foo.glsl
-# You must pass parameter using quotes
-macro( installLibShaders FILES )
-    # build configuration : link shaders to Resources/Shaders
+# install resources : link the resource dir into the buildtree and install the required files into the <bundle>/Resources
+# If called only with a directory, all the files in this directory and subdirs will be installed
+# to be called with
+# installResources ( TARGET theTarget
+#                    DIRECTORY theBaseRessourceDirectory
+#                    [FILES [file1 [file2 ... ]]]
+#
+include(CMakeParseArguments)
+function(installResources)
+    cmake_parse_arguments(
+            PARSED_ARGS
+            ""
+            "TARGET;DIRECTORY"
+            "FILES"
+            ${ARGN}
+    )
+    if (NOT PARSED_ARGS_DIRECTORY)
+        message(FATAL_ERROR " [installResources] You must provide a resource directory")
+    endif (NOT PARSED_ARGS_DIRECTORY)
+    get_filename_component(rsc_dir ${PARSED_ARGS_DIRECTORY} NAME)
+    get_filename_component(buildtree_dir ${CMAKE_CURRENT_BINARY_DIR} DIRECTORY)
+    message(STATUS " [installResources] Linking resources directory ${PARSED_ARGS_DIRECTORY} for target ${PARSED_ARGS_TARGET} into ${buildtree_dir}/Resources/${rsc_dir}")
     file(MAKE_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}/../Resources")
-    file(CREATE_LINK ${RADIUM_SHADERS_DIR} "${CMAKE_CURRENT_BINARY_DIR}/../Resources/Shaders" COPY_ON_ERROR SYMBOLIC)
-    foreach ( file ${FILES} )
+    if (${CMAKE_VERSION} VERSION_GREATER_EQUAL 3.14)
+        # this will be executed only at configure time
+        file(CREATE_LINK ${PARSED_ARGS_DIRECTORY} "${buildtree_dir}/Resources/${rsc_dir}" COPY_ON_ERROR SYMBOLIC)
+    else ()
+        if (MSVC OR MSVC_IDE OR MINGW)
+            add_custom_command(TARGET ${TRGT}
+                    POST_BUILD
+                    COMMAND ${CMAKE_COMMAND} -E copy_directory ${PARSED_ARGS_DIRECTORY} "${buildtree_dir}/Resources/${rsc_dir}"
+                    )
+        else (MSVC OR MSVC_IDE OR MINGW)
+            add_custom_command(TARGET ${TRGT}
+                    POST_BUILD
+                    COMMAND ${CMAKE_COMMAND} -E create_symlink ${PARSED_ARGS_DIRECTORY} "${buildtree_dir}/Resources/${rsc_dir}"
+                    )
+        endif (MSVC OR MSVC_IDE OR MINGW)
+    endif (${CMAKE_VERSION} VERSION_GREATER_EQUAL 3.14)
+    # build the list of files to install
+    if (NOT PARSED_ARGS_FILES)
+        file(GLOB_RECURSE PARSED_ARGS_FILES RELATIVE ${PARSED_ARGS_DIRECTORY} ${PARSED_ARGS_DIRECTORY}/*)
+    endif (NOT PARSED_ARGS_FILES)
+    message(STATUS " [installResources] Installing files ${PARSED_ARGS_FILES}")
+    foreach (file ${PARSED_ARGS_FILES})
         get_filename_component( file_dir ${file} DIRECTORY )
         # installed library
-        install(FILES ${RADIUM_SHADERS_DIR}/${file} DESTINATION Resources/Shaders/${file_dir})
+        install(FILES ${PARSED_ARGS_DIRECTORY}/${file} DESTINATION Resources/${rsc_dir}/${file_dir})
     endforeach()
-endmacro()
+endfunction(installResources)
