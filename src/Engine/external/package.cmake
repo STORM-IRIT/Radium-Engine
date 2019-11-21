bc86753b5e8e161273b1c0e9e8cb5a2eac3df645
Nicolas Mellado <nmellado0@gmail.com>
2019-11-21 13:54:54 +0100

    Force using the dependencies packaged with Radium

--- src/Engine/external/package.cmake
+++ src/Engine/external/package.cmake
@@ -1,3 +1,3 @@
-set(glm_DIR ${CMAKE_INSTALL_PREFIX}/lib/cmake/glm/)
+set(glm_DIR ${CMAKE_INSTALL_PREFIX}/lib/${CMAKE_LIBRARY_ARCHITECTURE}/cmake/glm/)
 set(glbinding_DIR ${CMAKE_INSTALL_PREFIX})
 set(globjects_DIR ${CMAKE_INSTALL_PREFIX})
