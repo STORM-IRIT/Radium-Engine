6a2a7359113c2aa28b8c8990c0daaf44c8946d1e
dlyr <github@dlyr.fr>
2019-12-17 15:44:39 +0100

    Fix warning "unused".

--- src/GuiBase/Viewer/Gizmo/RotateGizmo.cpp
+++ src/GuiBase/Viewer/Gizmo/RotateGizmo.cpp
@@ -127,7 +127,7 @@ void RotateGizmo::selectConstraint( int drawableIdx ) {
 Core::Transform RotateGizmo::mouseMove( const Engine::Camera& cam,
                                         const Core::Vector2& nextXY,
                                         bool stepped,
-                                        bool whole ) {
+                                        bool /*whole*/ ) {
     static const Scalar step = Ra::Core::Math::Pi / 10_ra;
 
     if ( m_selectedAxis == -1 ) return m_transform;
