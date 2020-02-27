c75e5c45ac423ad63cbedde75964de4bcd3bbbf6
dlyr <github@dlyr.fr>
2020-02-27 18:25:54 +0100

    scripts to generate filelist.

--- cmake/filelistIO.cmake
+++ cmake/filelistIO.cmake
@@ -1,61 +1,60 @@
-
 set( io_sources
     CameraLoader/CameraLoader.cpp
-    )
+)
 
 set( io_headers
     CameraLoader/CameraLoader.hpp
     RaIO.hpp
-    )
+)
 
 set( io_inlines
-    )
+   
+)
 
 if(RADIUM_IO_DEPRECATED)
-    list(APPEND io_sources
-        deprecated/OBJFileManager.cpp
-        deprecated/OFFFileManager.cpp
-        )
+list(APPEND io_sources
+    deprecated/OBJFileManager.cpp
+    deprecated/OFFFileManager.cpp
+)
 
-    list(APPEND io_headers
-        deprecated/FileManager.hpp
-        deprecated/OBJFileManager.hpp
-        deprecated/OFFFileManager.hpp
-        )
+list(APPEND io_headers
+    deprecated/FileManager.hpp
+    deprecated/OBJFileManager.hpp
+    deprecated/OFFFileManager.hpp
+)
 
-    list(APPEND io_inlines
-        deprecated/FileManager.inl
-        )
+list(APPEND io_inlines
+    deprecated/FileManager.inl
+)
 endif(RADIUM_IO_DEPRECATED)
 
 if( RADIUM_IO_ASSIMP )
-    list(APPEND io_sources
-        AssimpLoader/AssimpAnimationDataLoader.cpp
-        AssimpLoader/AssimpCameraDataLoader.cpp
-        AssimpLoader/AssimpFileLoader.cpp
-        AssimpLoader/AssimpGeometryDataLoader.cpp
-        AssimpLoader/AssimpHandleDataLoader.cpp
-        AssimpLoader/AssimpLightDataLoader.cpp
-        )
+list(APPEND io_sources
+    AssimpLoader/AssimpAnimationDataLoader.cpp
+    AssimpLoader/AssimpCameraDataLoader.cpp
+    AssimpLoader/AssimpFileLoader.cpp
+    AssimpLoader/AssimpGeometryDataLoader.cpp
+    AssimpLoader/AssimpHandleDataLoader.cpp
+    AssimpLoader/AssimpLightDataLoader.cpp
+)
 
-    list(APPEND io_headers
-        AssimpLoader/AssimpAnimationDataLoader.hpp
-        AssimpLoader/AssimpCameraDataLoader.hpp
-        AssimpLoader/AssimpFileLoader.hpp
-        AssimpLoader/AssimpGeometryDataLoader.hpp
-        AssimpLoader/AssimpHandleDataLoader.hpp
-        AssimpLoader/AssimpLightDataLoader.hpp
-        AssimpLoader/AssimpWrapper.hpp
-        )
+list(APPEND io_headers
+    AssimpLoader/AssimpAnimationDataLoader.hpp
+    AssimpLoader/AssimpCameraDataLoader.hpp
+    AssimpLoader/AssimpFileLoader.hpp
+    AssimpLoader/AssimpGeometryDataLoader.hpp
+    AssimpLoader/AssimpHandleDataLoader.hpp
+    AssimpLoader/AssimpLightDataLoader.hpp
+    AssimpLoader/AssimpWrapper.hpp
+)
 endif( RADIUM_IO_ASSIMP )
 
-
 if( RADIUM_IO_TINYPLY )
-    list(APPEND io_sources
-        TinyPlyLoader/TinyPlyFileLoader.cpp
-        )
+list(APPEND io_sources
+    TinyPlyLoader/TinyPlyFileLoader.cpp
+)
 
-    list(APPEND io_headers
-        TinyPlyLoader/TinyPlyFileLoader.hpp
-        )
+list(APPEND io_headers
+    TinyPlyLoader/TinyPlyFileLoader.hpp
+)
 endif( RADIUM_IO_TINYPLY )
