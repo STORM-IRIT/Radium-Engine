2c899845ee8c167a81a9a626dfd6283668b1727f
dlyr <github@dlyr.fr>
2020-03-26 17:14:34 +0100

    script: update format

--- scripts/format-all.sh
+++ scripts/format-all.sh
@@ -2,4 +2,4 @@
 
 ROOT=`git rev-parse --show-toplevel`
 
-find "${ROOT}/src/" "${ROOT}/Applications/"  "${ROOT}/Plugins/" \( -name \*.cpp -or -name \*.hpp -or -name \*.inl \) -exec clang-format -i -style=file  "{}" \;
+find "${ROOT}/src/" "${ROOT}/tests/"  "${ROOT}/Shaders/" \( -name \*.cpp -or -name \*.hpp -or -name \*.inl -or -name \*.glsl \) -exec clang-format -i -style=file  "{}" \;
