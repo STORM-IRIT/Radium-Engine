67cdc1f6c60ae252f330ad37743ef9f289c460b6
nmellado <nmellado0@gmail.com>
2020-07-06 11:29:34 +0200

    [cmake] Externals: use relative paths from package file

--- src/Engine/external/package.cmake
+++ src/Engine/external/package.cmake
@@ -1,4 +1,10 @@
-set(glm_DIR ${CMAKE_INSTALL_PREFIX}/lib/${CMAKE_LIBRARY_ARCHITECTURE}/cmake/glm/)
-set(glbinding_DIR ${CMAKE_INSTALL_PREFIX}/share/glbinding/)
-set(globjects_DIR ${CMAKE_INSTALL_PREFIX}/share/globjects/)
-set(Eigen3_DIR ${CMAKE_INSTALL_PREFIX}/share/eigen3/cmake/)
+set(glm_sub_DIR lib/${CMAKE_LIBRARY_ARCHITECTURE}/cmake/glm/ CACHE INTERNAL "")
+set(glbinding_sub_DIR share/glbinding/ CACHE INTERNAL "")
+set(globjects_sub_DIR share/globjects/ CACHE INTERNAL "")
+set(Eigen3_sub_DIR share/eigen3/cmake/ CACHE INTERNAL "")
+
+set(glm_DIR ${CMAKE_INSTALL_PREFIX}/${glm_sub_DIR})
+set(glbinding_DIR ${CMAKE_INSTALL_PREFIX}/${glbinding_sub_DIR})
+set(globjects_DIR ${CMAKE_INSTALL_PREFIX}/${globjects_sub_DIR})
+set(Eigen3_DIR ${CMAKE_INSTALL_PREFIX}/${Eigen3_sub_DIR})
+
