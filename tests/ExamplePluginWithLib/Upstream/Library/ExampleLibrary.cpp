abf7851b3f82bc19f6d7fe142d05bb35d3cf5271
Mathias Paulin <mathias.paulin@irit.fr>
2020-07-23 22:05:53 +0200

    Add doc and remove superfluous code in Upstream Library.

--- tests/ExamplePluginWithLib/Upstream/Library/ExampleLibrary.cpp
+++ tests/ExamplePluginWithLib/Upstream/Library/ExampleLibrary.cpp
@@ -10,7 +10,9 @@
 
 namespace ExampleLibrary {
 
+// This symbol will be used to identify the library and find its directory using Ra::Core::Resources::ResourcesLocator.
 static int ExampleLibraryMagick = 0x0F0F0F0F;
+
 void ExampleMessage( const std::string& message ) {
     auto resourcesPath = Ra::Core::Resources::ResourcesLocator(
         reinterpret_cast<void*>( &ExampleLibraryMagick ), "Resources/LibraryResources", "../" );
