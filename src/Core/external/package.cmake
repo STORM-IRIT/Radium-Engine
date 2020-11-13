9fe23757f8d1b0f014d52ac39905cf69a091210b
dlyr <github@dlyr.fr>
2020-11-13 01:27:21 +0100

    [cmake] Parent scope propagation, add cpplocate and openmesh.

--- src/Core/external/package.cmake
+++ src/Core/external/package.cmake
@@ -1,9 +1,15 @@
-set(OpenMesh_sub_DIR share/OpenMesh/cmake/ CACHE INTERNAL "")
-set(cpplocate_sub_DIR share/cpplocate/ CACHE INTERNAL "")
 
 if (NOT DEFINED Eigen3_DIR)
     set(Eigen3_sub_DIR share/eigen3/cmake/ CACHE INTERNAL "")
     set(Eigen3_DIR ${CMAKE_INSTALL_PREFIX}/${Eigen3_sub_DIR})
 endif ()
-set(OpenMesh_DIR ${CMAKE_INSTALL_PREFIX}/${OpenMesh_sub_DIR})
-set(cpplocate_DIR ${CMAKE_INSTALL_PREFIX}/${cpplocate_sub_DIR})
+
+if (NOT DEFINED OpenMesh_DIR)
+    set(OpenMesh_sub_DIR share/OpenMesh/cmake/ CACHE INTERNAL "")
+    set(OpenMesh_DIR ${CMAKE_INSTALL_PREFIX}/${OpenMesh_sub_DIR})
+endif()
+
+if (NOT DEFINED cpplocate_DIR)
+    set(cpplocate_sub_DIR share/cpplocate/ CACHE INTERNAL "")
+    set(cpplocate_DIR ${CMAKE_INSTALL_PREFIX}/${cpplocate_sub_DIR})
+endif()
