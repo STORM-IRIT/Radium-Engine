21c17288001ace62b008bb86e2dc87b2a1364fa2
dlyr <github@dlyr.fr>
2020-11-14 09:08:32 +0100

    [cmake] IO externals

--- src/IO/external/package.cmake
+++ src/IO/external/package.cmake
@@ -1,6 +1,9 @@
-set(assimp_sub_DIR lib/cmake/assimp-5.0/ CACHE INTERNAL "")
-set(tinyply_sub_DIR lib/cmake/tinyply/ CACHE INTERNAL "")
+if(NOT DEFINED assimp_DIR)
+    set(assimp_sub_DIR lib/cmake/assimp-5.0/ CACHE INTERNAL "")
+    set(assimp_DIR ${CMAKE_INSTALL_PREFIX}/${assimp_sub_DIR})
+endif()
 
-set(assimp_DIR ${CMAKE_INSTALL_PREFIX}/${assimp_sub_DIR})
-set(tinyply_DIR ${CMAKE_INSTALL_PREFIX}/${tinyply_sub_DIR})
-
+if(NOT DEFINED tinyply_DIR)
+    set(tinyply_sub_DIR lib/cmake/tinyply/ CACHE INTERNAL "")
+    set(tinyply_DIR ${CMAKE_INSTALL_PREFIX}/${tinyply_sub_DIR})
+endif()
