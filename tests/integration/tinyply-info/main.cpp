f574c71343d609df13dd6354c22871913f7188c7
dlyr <github@dlyr.fr>
2021-01-06 15:49:50 +0100

    [tests] more examples for tinyply.

--- tests/integration/tinyply-info/main.cpp
+++ tests/integration/tinyply-info/main.cpp
@@ -15,6 +15,10 @@ int main( int /*argc*/, char** /*argv*/ ) {
 
     TinyPlyFileLoader loader;
     std::unique_ptr<FileData>( loader.loadFile( "../data/inputs/simple.ply" ) );
+    std::unique_ptr<FileData>( loader.loadFile( "../data/inputs/simple_normals.ply" ) );
     std::unique_ptr<FileData>( loader.loadFile( "../data/inputs/buggy-1.ply" ) );
+    std::unique_ptr<FileData>( loader.loadFile( "../data/inputs/test_color_nonormals.ply" ) );
+    std::unique_ptr<FileData>( loader.loadFile( "../data/inputs/test_color_normals.ply" ) );
+    std::unique_ptr<FileData>( loader.loadFile( "../data/inputs/test_nocolor_normals.ply" ) );
     return 0;
 }
