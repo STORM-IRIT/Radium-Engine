de89beaab80dd3198611c4a4a9989bda9c615658
dlyr <github@dlyr.fr>
2021-03-02 23:32:38 +0100

    [tests] Integration KeyMappingManager save file
    integration settings
    
    Co-authored-by: Nicolas Mellado <nmellado0@gmail.com>

---
+++ tests/integration/KeyMappingManager/main.cpp
@@ -0,0 +1,28 @@
+#include <Core/Resources/Resources.hpp>
+#include <Core/Utils/StdFilesystem.hpp>
+#include <Gui/Utils/KeyMappingManager.hpp>
+
+#include <QtGlobal>
+
+using namespace Ra;
+using namespace Ra::Gui;
+using Idx = KeyMappingManager::KeyMappingAction;
+using Ctx = KeyMappingManager::Context;
+
+int main( int argc, char** argv ) {
+
+    /// need a base dir in argv[1]
+    if ( argc != 2 ) { return -1; }
+    std::string base {argv[1]};
+
+    QSettings settings( "RadiumIntegrationTests", "KeyMappingManager" );
+    settings.clear();
+    KeyMappingManager::createInstance();
+    auto mgr = Gui::KeyMappingManager::getInstance();
+    auto optionalPath {Core::Resources::getRadiumResourcesPath()};
+    auto resourcesRootDir {optionalPath.value_or( "[[Default ressource path not found]]" )};
+
+    mgr->loadConfiguration( "../data/inputs/keymapping-valid.xml" );
+    if ( !mgr->saveConfiguration( base + "/keymapping-valid.xml" ) ) return -1;
+    return 0;
+}
