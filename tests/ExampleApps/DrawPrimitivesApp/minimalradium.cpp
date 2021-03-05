8b0172eb6734500b70f5016e75f0415f5540265b
dlyr <github@dlyr.fr>
2021-03-05 15:35:52 +0100

    [tests] clean dead code.

--- tests/ExampleApps/DrawPrimitivesApp/minimalradium.cpp
+++ tests/ExampleApps/DrawPrimitivesApp/minimalradium.cpp
@@ -635,11 +635,7 @@ void MinimalComponent::initialize() {
 
 /// This system will be added to the engine. Every frame it will
 /// add a task to be executed, calling the spin function of the component.
-void MinimalSystem::generateTasks( Ra::Core::TaskQueue*, const Ra::Engine::FrameInfo& ) {
-
-    // We check that our component is here.
-    CORE_ASSERT( m_components.size() == 1, "System incorrectly initialized" );
-}
+void MinimalSystem::generateTasks( Ra::Core::TaskQueue*, const Ra::Engine::FrameInfo& ) {}
 
 void MinimalSystem::addComponent( Ra::Engine::Scene::Entity* ent, MinimalComponent* comp ) {
     registerComponent( ent, comp );
