1d697a9f10901ca3c67021248d679bb43c4a4245
dlyr <github@dlyr.fr>
2021-03-05 12:25:34 +0100

    [shaders] wireframe manual clip tentative

--- Shaders/Lines/Wireframe.geom.glsl
+++ Shaders/Lines/Wireframe.geom.glsl
@@ -21,13 +21,58 @@ out vec4 gColor;
 out float pixelWidthDiv2;
 out float slopeY2;
 uniform vec2 viewport;
+vec4 vColor[2];
+
+float clipf( in float x0, in float x1, in float dx, in float w, in out int clipped ) {
+
+    if ( w < x1 )
+    {
+        clipped = 1;
+        return ( w - x0 ) / dx;
+    }
+
+    if ( x1 < -w )
+    {
+        clipped = -1;
+        return ( -w - x0 ) / dx;
+    }
+    return 1.;
+}
+
+vec4 clipp( in vec3 dir, in vec4 p0, in vec4 p1, in out vec3 clipped ) {
+    vec4 r = p1;
+    r.xyz  = p0.xyz + clipf( p0.x, r.x, dir.x, r.w, clipped.x ) * dir;
+    r.xyz  = p0.xyz + clipf( p0.y, r.y, dir.y, r.w, clipped.y ) * dir;
+    r.xyz  = p0.xyz + clipf( p0.z, r.z, dir.z, r.w, clipped.z ) * dir;
+    return r;
+}
 
 void main() {
     vec3 vp = vec3( viewport, 1. );
-    vec3 p0 = vPosition[0].xyz / vPosition[0].w * vp;
-    vec3 p1 = vPosition[1].xyz / vPosition[1].w * vp;
+    vec3 c0 = vec3( 0 );
+    vec3 c1 = vec3( 0 );
 
-    float pixelWidth = 3.;
+    vec4 css0 = gl_in[0].gl_Position;
+    vec4 css1 = gl_in[1].gl_Position;
+
+    vec4 scss0 = css0 * css1.w;
+    vec4 scss1 = css1 * css0.w;
+
+    vec3 dir = ( scss1 - scss0 ).xyz;
+    vec4 ip0 = clipp( -dir, scss1, scss0, c0 );
+    vec4 ip1 = clipp( dir, scss0, scss1, c1 );
+    //  ip0 = scss0;
+    // ip1 = scss1;
+    float epsilon = 0.001;
+    if ( ( c0.x != 0 && c0.x == c1.x ) || ( c0.y != 0 && c0.y == c1.y ) ||
+         ( c0.z != 0 && c0.z == c1.z ) )
+        return;
+    vec3 p0 = ip0.xyz * vp;
+    if ( abs( ip0.w ) > epsilon ) p0 /= ip0.w;
+    vec3 p1 = ip1.xyz * vp;
+    if ( abs( ip1.w ) > epsilon ) p1 /= ip1.w;
+
+    float pixelWidth = 1.;
     pixelWidthDiv2   = pixelWidth / 2.;
     vec2 slope       = normalize( vec2( p1.xy - p0.xy ) );
     slopeY2          = slope.y * slope.y;
@@ -38,7 +83,6 @@ void main() {
     vec4 c = vec4( ( p1 + n ) / vp, 1. );
     vec4 d = vec4( ( p1 - n ) / vp, 1. );
 
-    vec4 vColor[2];
     vColor[0] = vec4( vec3( .7 ), -pixelWidthDiv2 - 1. );
     vColor[1] = vec4( vec3( .7 ), +pixelWidthDiv2 + 1. );
 
