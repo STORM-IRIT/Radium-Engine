3a988b95c64a95321f728b7129cae1a3452b7000
dlyr <github@dlyr.fr>
2021-03-12 08:39:28 +0100

    [tests] draw prim wedge collapse

--- tests/ExampleApps/DrawPrimitivesApp/minimalradium.cpp
+++ tests/ExampleApps/DrawPrimitivesApp/minimalradium.cpp
@@ -40,6 +40,7 @@ const bool ENABLE_DISKS     = true;
 const bool ENABLE_NORMALS   = true;
 const bool ENABLE_POLYS     = true;
 const bool ENABLE_LOGO      = true;
+const bool ENABLE_COLLAPSE  = true;
 
 using namespace Ra;
 using namespace Ra::Core;
@@ -727,6 +728,179 @@ void MinimalComponent::initialize() {
                 addRenderObject( renderObject );
             }
         }
+    }
+
+    if ( ENABLE_COLLAPSE )
+    {
+
+        using namespace Ra::Core;
+        using namespace Ra::Core::Utils;
+        using namespace Ra::Core::Geometry;
+        auto findHalfedge = []( TopologicalMesh& topo,
+                                const Vector3& from,
+                                const Vector3& to ) -> optional<TopologicalMesh::HalfedgeHandle> {
+            bool found;
+            TopologicalMesh::HalfedgeHandle he;
+            for ( auto he_iter = topo.halfedges_begin(); he_iter != topo.halfedges_end();
+                  ++he_iter )
+            {
+
+                if ( topo.point( topo.to_vertex_handle( he_iter ) ) == to &&
+                     topo.point( topo.from_vertex_handle( he_iter ) ) == from )
+                {
+                    found = true;
+                    he    = *he_iter;
+                }
+            }
+            if ( found ) return he;
+            return {};
+        };
+
+        auto addMesh = [this, colorBoost, shadedRt, plainRt]( Vector3 pos, TopologicalMesh topo1 ) {
+            topo1.checkIntegrity();
+            auto mesh1 = topo1.toTriangleMesh();
+            std::shared_ptr<Mesh> poly( new Mesh( "TEST", std::move( mesh1 ) ) );
+
+            auto renderObject2 = RenderObject::createRenderObject(
+                "TEST", this, RenderObjectType::Geometry, poly, plainRt );
+            renderObject2->setLocalTransform( Transform {
+                Translation( Vector3( pos ) ) * Eigen::UniformScaling<Scalar>( 0.02_ra )} );
+
+            addRenderObject( renderObject2 );
+        };
+
+        Vector3Array points {
+            {00._ra, 00._ra, 00._ra},
+            {10._ra, 00._ra, 00._ra},
+            {05._ra, 05._ra, 00._ra},
+            {05._ra, 10._ra, 00._ra},
+            {15._ra, 05._ra, 00._ra},
+            {10._ra, 08._ra, 00._ra},
+            {10._ra, 12._ra, 00._ra},
+            {15._ra, 10._ra, 00._ra},
+        };
+        Vector3Array points2 = {points[0], points[0], points[1], points[1], points[1], points[2],
+                                points[2], points[2], points[2], points[3], points[3], points[3],
+                                points[4], points[4], points[5], points[5], points[5], points[5],
+                                points[5], points[5], points[6], points[6], points[7], points[7]};
+
+        Vector4Array colors = {
+            {0_ra, 0_ra, 0_ra, 1_ra},    {1_ra, 1_ra, 1_ra, 1_ra},    {2_ra, 2_ra, 2_ra, 1_ra},
+            {3_ra, 3_ra, 3_ra, 1_ra},    {4_ra, 4_ra, 4_ra, 1_ra},    {5_ra, 5_ra, 5_ra, 1_ra},
+            {6_ra, 6_ra, 6_ra, 1_ra},    {7_ra, 7_ra, 7_ra, 1_ra},    {8_ra, 8_ra, 8_ra, 1_ra},
+            {9_ra, 9_ra, 9_ra, 1_ra},    {10_ra, 10_ra, 10_ra, 1_ra}, {11_ra, 11_ra, 11_ra, 1_ra},
+            {12_ra, 12_ra, 12_ra, 1_ra}, {13_ra, 13_ra, 13_ra, 1_ra}, {14_ra, 14_ra, 14_ra, 1_ra},
+            {15_ra, 15_ra, 15_ra, 1_ra}, {16_ra, 16_ra, 16_ra, 1_ra}, {17_ra, 17_ra, 17_ra, 1_ra},
+            {18_ra, 18_ra, 18_ra, 1_ra}, {19_ra, 19_ra, 19_ra, 1_ra}, {20_ra, 20_ra, 20_ra, 1_ra},
+            {21_ra, 21_ra, 21_ra, 1_ra}, {22_ra, 22_ra, 22_ra, 1_ra}, {23_ra, 23_ra, 23_ra, 1_ra},
+        };
+
+        for ( auto& c : colors )
+        {
+            c = colorBoost * Vector4 {dis01( gen ), dis01( gen ), dis01( gen ), 1_ra};
+        }
+
+        VectorArray<Vector3ui> indices1 {
+            {0, 2, 1}, {0, 3, 2}, {1, 2, 5}, {2, 3, 5}, {1, 5, 4}, {3, 6, 5}, {5, 6, 7}, {4, 5, 7}};
+        Vector3uArray indices3 = {{0, 2, 1}, {1, 2, 5}, {1, 5, 4}, {3, 6, 5}, {5, 6, 7}, {4, 5, 7}};
+
+        Vector3uArray indices4 = {
+            {0, 2, 5}, {3, 14, 6}, {4, 12, 15}, {11, 18, 20}, {17, 22, 21}, {16, 13, 23}};
+
+        VectorArray<Vector3ui> indices2 {{0, 5, 2},
+                                         {1, 9, 8},
+                                         {3, 6, 14},
+                                         {7, 10, 19},
+                                         {4, 15, 12},
+                                         {11, 20, 18},
+                                         {17, 21, 22},
+                                         {16, 23, 13}};
+        Vector4Array colors2 {24, Color::White()};
+        for ( const auto& face : indices2 )
+        {
+            colors2[face[0]] = colors[face[0]];
+            colors2[face[1]] = colors[face[0]];
+            colors2[face[2]] = colors[face[0]];
+        }
+        TriangleMesh mesh1;
+
+        TopologicalMesh topo1;
+        optional<TopologicalMesh::HalfedgeHandle> optHe;
+        Vector3 pos;
+
+        auto addMergeScene =
+            [findHalfedge, addMesh, &cellCorner, toCellCenter, cellSize, nCellX, nCellY](
+                const Vector3Array& points,
+                const Vector4Array& colors,
+                const Vector3uArray& indices1,
+                const Vector3& from,
+                const Vector3& to ) {
+                TriangleMesh mesh1;
+
+                TopologicalMesh topo1;
+                optional<TopologicalMesh::HalfedgeHandle> optHe;
+                Vector3 pos;
+
+                mesh1.setVertices( points );
+                mesh1.addAttrib( Mesh::getAttribName( Mesh::VERTEX_COLOR ),
+                                 Vector4Array {colors.begin(), colors.begin() + points.size()} );
+                mesh1.setIndices( indices1 );
+                topo1 = TopologicalMesh {mesh1};
+                optHe = findHalfedge( topo1, from, to );
+
+                pos = cellCorner + toCellCenter;
+                addMesh( pos, topo1 );
+
+                updateCellCorner( cellCorner, cellSize, nCellX, nCellY );
+                pos = cellCorner + toCellCenter;
+                topo1.collapseWedge( *optHe );
+                addMesh( pos, topo1 );
+
+                topo1 = TopologicalMesh {mesh1};
+                optHe = findHalfedge( topo1, from, to );
+                pos   = cellCorner + toCellCenter + Vector3 {0_ra, .25_ra, 0_ra};
+                topo1.collapseWedge( *optHe, true );
+                addMesh( pos, topo1 );
+            };
+
+        // With "continuous" wedges.
+        addMergeScene( points, colors, indices1, points[5], points[2] );
+
+        updateCellCorner( cellCorner, cellSize, nCellX, nCellY );
+
+        // with "flat face" wedges
+        addMergeScene( points2, colors2, indices2, points[5], points[2] );
+
+        updateCellCorner( cellCorner, cellSize, nCellX, nCellY );
+
+        // boundary
+        // With "continuous" wedges.
+        addMergeScene( points, colors, indices3, points[5], points[2] );
+
+        updateCellCorner( cellCorner, cellSize, nCellX, nCellY );
+        // with "flat face" wedges
+        addMergeScene( points2, colors2, indices4, points[5], points[2] );
+
+        // other way round
+        updateCellCorner( cellCorner, cellSize, nCellX, nCellY );
+
+        // With "continuous" wedges.
+        addMergeScene( points, colors, indices1, points[2], points[5] );
+
+        updateCellCorner( cellCorner, cellSize, nCellX, nCellY );
+
+        // with "flat face" wedges
+        addMergeScene( points2, colors2, indices2, points[2], points[5] );
+
+        updateCellCorner( cellCorner, cellSize, nCellX, nCellY );
+        //
+        //        // boundary
+        //        // With "continuous" wedges.
+        addMergeScene( points, colors, indices3, points[2], points[5] );
+        //
+        updateCellCorner( cellCorner, cellSize, nCellX, nCellY );
+        //        // with "flat face" wedges
+        addMergeScene( points2, colors2, indices4, points[2], points[5] );
     }
 }
 
