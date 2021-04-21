79b736fe314e1bd6bbd9447173ce3d380da0d6e6
dlyr <github@dlyr.fr>
2021-04-21 11:45:54 +0200

    [tests] add section to camera test.

--- tests/unittest/Engine/camera.cpp
+++ tests/unittest/Engine/camera.cpp
@@ -22,38 +22,40 @@ TEST_CASE( "Engine/Camera" ) {
 
     REQUIRE( dir.normalized().isApprox( cam->getDirection() ) );
 
-    /// \todo add section when cleanup is ok, now it locks tests in case of failure ...
-    //    SECTION( "aabb" ) {
-    Ra::Core::Aabb aabb {Ra::Core::Vector3 {3, 3, 3}, Ra::Core::Vector3 {6, 6, 6}};
-    cam->fitZRange( aabb );
-    REQUIRE(
-        ( Math::areApproxEqual( cam->getZNear(), ( cam->getPosition() - aabb.min() ).norm() ) ||
-          cam->getZNear() == cam->m_minZNear ) );
-    REQUIRE(
-        Math::areApproxEqual( cam->getZFar(), ( cam->getPosition() - aabb.max() ).norm(), 16_ra ) );
-    REQUIRE( ( cam->getZNear() > cam->m_minZNear ||
-               Math::areApproxEqual( cam->getZNear(), cam->m_minZNear ) ) );
-    REQUIRE( ( ( cam->getZFar() - cam->getZNear() ) > cam->m_minZRange ||
-               Math::areApproxEqual( ( cam->getZFar() - cam->getZNear() ), cam->m_minZRange ) ) );
+    SECTION( "fit aabb" ) {
+        Ra::Core::Aabb aabb {Ra::Core::Vector3 {3, 3, 3}, Ra::Core::Vector3 {6, 6, 6}};
+        cam->fitZRange( aabb );
+        REQUIRE(
+            ( Math::areApproxEqual( cam->getZNear(), ( cam->getPosition() - aabb.min() ).norm() ) ||
+              cam->getZNear() == cam->m_minZNear ) );
+        REQUIRE( Math::areApproxEqual(
+            cam->getZFar(), ( cam->getPosition() - aabb.max() ).norm(), 16_ra ) );
+        REQUIRE( ( cam->getZNear() > cam->m_minZNear ||
+                   Math::areApproxEqual( cam->getZNear(), cam->m_minZNear ) ) );
+        REQUIRE(
+            ( ( cam->getZFar() - cam->getZNear() ) > cam->m_minZRange ||
+              Math::areApproxEqual( ( cam->getZFar() - cam->getZNear() ), cam->m_minZRange ) ) );
 
-    aabb = {Ra::Core::Vector3 {-1, -1, -1}, Ra::Core::Vector3 {1, 1, 1}};
-    cam->fitZRange( aabb );
-    REQUIRE( Math::areApproxEqual( cam->getZNear(), cam->m_minZNear ) );
-    REQUIRE( ( Math::areApproxEqual( cam->getZFar(), ( cam->getPosition() - aabb.max() ).norm() ) ||
-               Math::areApproxEqual( cam->getZFar() - cam->getZNear(), cam->m_minZRange ) ) );
-    REQUIRE( ( cam->getZNear() > cam->m_minZNear ||
-               Math::areApproxEqual( cam->getZNear(), cam->m_minZNear ) ) );
-    REQUIRE( ( ( cam->getZFar() - cam->getZNear() ) > cam->m_minZRange ||
-               Math::areApproxEqual( ( cam->getZFar() - cam->getZNear() ), cam->m_minZRange ) ) );
+        aabb = {Ra::Core::Vector3 {-1, -1, -1}, Ra::Core::Vector3 {1, 1, 1}};
+        cam->fitZRange( aabb );
+        REQUIRE( Math::areApproxEqual( cam->getZNear(), cam->m_minZNear ) );
+        REQUIRE(
+            ( Math::areApproxEqual( cam->getZFar(), ( cam->getPosition() - aabb.max() ).norm() ) ||
+              Math::areApproxEqual( cam->getZFar() - cam->getZNear(), cam->m_minZRange ) ) );
+        REQUIRE( ( cam->getZNear() > cam->m_minZNear ||
+                   Math::areApproxEqual( cam->getZNear(), cam->m_minZNear ) ) );
+        REQUIRE(
+            ( ( cam->getZFar() - cam->getZNear() ) > cam->m_minZRange ||
+              Math::areApproxEqual( ( cam->getZFar() - cam->getZNear() ), cam->m_minZRange ) ) );
 
-    aabb = {Ra::Core::Vector3 {-2, -1, 1}, Ra::Core::Vector3 {-4, -1, 0}};
-    cam->fitZRange( aabb );
-    REQUIRE( Math::areApproxEqual( cam->getZNear(), cam->m_minZNear ) );
-    REQUIRE( Math::areApproxEqual( cam->getZFar(), cam->getZNear() + cam->m_minZRange ) );
+        aabb = {Ra::Core::Vector3 {-2, -1, 1}, Ra::Core::Vector3 {-4, -1, 0}};
+        cam->fitZRange( aabb );
+        REQUIRE( Math::areApproxEqual( cam->getZNear(), cam->m_minZNear ) );
+        REQUIRE( Math::areApproxEqual( cam->getZFar(), cam->getZNear() + cam->m_minZRange ) );
 
-    aabb = {Ra::Core::Vector3 {0, -3, 0}, Ra::Core::Vector3 {1, -2, 1}};
-    cam->fitZRange( aabb );
-    REQUIRE( Math::areApproxEqual( cam->getZNear(), cam->m_minZNear ) );
-    REQUIRE( Math::areApproxEqual( cam->getZFar(), cam->getZNear() + cam->m_minZRange ) );
-    //}
+        aabb = {Ra::Core::Vector3 {0, -3, 0}, Ra::Core::Vector3 {1, -2, 1}};
+        cam->fitZRange( aabb );
+        REQUIRE( Math::areApproxEqual( cam->getZNear(), cam->m_minZNear ) );
+        REQUIRE( Math::areApproxEqual( cam->getZFar(), cam->getZNear() + cam->m_minZRange ) );
+    }
 }
