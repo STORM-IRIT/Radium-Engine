d819c8713694bc8119350ef8750223de51a475c1
dlyr <github@dlyr.fr>
2021-04-29 08:57:05 +0200

    [engine] switch storage to vector.
    so that camera at index 0 is always the "default"

--- src/Engine/Scene/DefaultCameraManager.cpp
+++ src/Engine/Scene/DefaultCameraManager.cpp
@@ -3,6 +3,8 @@
 #include <Engine/RadiumEngine.hpp>
 #include <Engine/Scene/CameraComponent.hpp>
 
+#include <algorithm>
+
 namespace Ra {
 namespace Engine {
 namespace Scene {
@@ -12,6 +14,7 @@ DefaultCameraManager::DefaultCameraManager() {
 }
 
 const CameraComponent* DefaultCameraManager::getCamera( size_t cam ) const {
+    //    return ( *m_data )[cam];
     return ( *m_data )[cam];
 }
 
@@ -22,33 +25,38 @@ void DefaultCameraManager::addCamera( CameraComponent* cam ) {
 DefaultCameraStorage::DefaultCameraStorage() = default;
 
 void DefaultCameraStorage::add( CameraComponent* cam ) {
-    m_Cameras.emplace( cam->getType(), cam );
+    //    m_data.emplace( cam->getType(), cam );
+    m_data.push_back( cam );
 }
 
 void DefaultCameraStorage::remove( CameraComponent* cam ) {
-    auto range = m_Cameras.equal_range( cam->getType() );
-    for ( auto i = range.first; i != range.second; ++i )
-    {
-        if ( i->second == cam )
-        {
-            m_Cameras.erase( i );
-            break;
-        }
-    }
+    //    auto range = m_data.equal_range( cam->getType() );
+    //    for ( auto i = range.first; i != range.second; ++i )
+    //    {
+    //        if ( i->second == cam )
+    //        {
+    //            m_data.erase( i );
+    //            break;
+    //        }
+    //    }
+    auto itr = std::find( m_data.begin(), m_data.end(), cam );
+    if ( itr != m_data.end() ) m_data.erase( itr );
 }
 
 size_t DefaultCameraStorage::size() const {
-    return m_Cameras.size();
+    return m_data.size();
 }
 
 void DefaultCameraStorage::clear() {
-    m_Cameras.clear();
+    m_data.clear();
 }
 
 CameraComponent* DefaultCameraStorage::operator[]( unsigned int n ) {
-    auto iterator = m_Cameras.begin();
-    std::advance( iterator, n );
-    return iterator->second;
+
+    //    auto iterator = m_data.begin();
+    //    std::advance( iterator, n );
+    //    return iterator->second;
+    return m_data[n];
 }
 
 } // namespace Scene
