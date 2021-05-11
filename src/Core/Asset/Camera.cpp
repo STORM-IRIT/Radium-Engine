142eb7d95f5f4d32d75d719ff6a179010b1a388c
dlyr <github@dlyr.fr>
2021-05-11 14:15:42 +0200

    [core] move project to cpp.

--- src/Core/Asset/Camera.cpp
+++ src/Core/Asset/Camera.cpp
@@ -194,6 +194,28 @@ Core::Ray Camera::getRayFromScreen( const Core::Vector2& pix ) const {
     return Core::Ray::Through( getPosition(), unProject( pix ) );
 }
 
+Core::Vector2 Camera::project( const Core::Vector3& p ) const {
+    Core::Vector4 point   = Core::Vector4::Ones();
+    point.head<3>()       = p;
+    Core::Vector4 vpPoint = getProjMatrix() * getViewMatrix() * point;
+
+    return Core::Vector2( getWidth() * 0.5_ra * ( 1_ra + vpPoint.x() ),
+                          getHeight() * 0.5_ra * ( 1_ra - vpPoint.y() ) );
+}
+
+Core::Vector3 Camera::unProject( const Core::Vector2& pix ) const {
+    const Scalar localX = ( 2_ra * pix.x() ) / getWidth() - 1_ra;
+    // Y is "inverted" (goes downwards)
+    const Scalar localY = -( 2_ra * pix.y() ) / getHeight() + 1_ra;
+
+    // Multiply the point in screen space by the inverted projection matrix
+    // and then by the inverted view matrix ( = m_frame) to get it in world space.
+    // NB : localPoint needs to be a vec4 to be multiplied by the proj matrix.
+    const Core::Vector4 localPoint( localX, localY, -getZNear(), 1_ra );
+    const Core::Vector4 unproj = getProjMatrix().inverse() * localPoint;
+    return getFrame() * unproj.head<3>();
+}
+
 } // namespace Asset
 } // namespace Core
 } // namespace Ra
