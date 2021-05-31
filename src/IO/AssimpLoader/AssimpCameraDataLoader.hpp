0dcf972a3227d46fe39b78fcdd88830f153c5855
dlyr <github@dlyr.fr>
2021-05-31 15:01:27 +0200

    [io] cleanup assimp camera loader.

--- src/IO/AssimpLoader/AssimpCameraDataLoader.hpp
+++ src/IO/AssimpLoader/AssimpCameraDataLoader.hpp
@@ -46,9 +46,6 @@ class RA_IO_API AssimpCameraDataLoader : public Core::Asset::DataLoader<Core::As
     /// Fill \p data with the CameraData from \p camera.
     void loadCameraData( const aiScene* scene, const aiCamera& camera, Core::Asset::Camera& data );
 
-    /// Fill \p data with the camera name from \p camera.
-    void fetchName( const aiCamera& camera, Core::Asset::Camera& data ) const;
-
     /// Return the Camera transformation, in world space, for \p data from \p the scene.
     Core::Matrix4 loadCameraFrame( const aiScene* scene,
                                    const Core::Matrix4& parentFrame,
