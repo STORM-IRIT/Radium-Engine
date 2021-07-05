8d8c397ed4036fa59457d5c8ffe2bc190b10e7f9
dlyr <github@dlyr.fr>
2021-07-05 12:37:39 +0200

    [tests] more volume loader unit test, refactor a bit.

--- tests/unittest/IO/volumeloader.cpp
+++ tests/unittest/IO/volumeloader.cpp
@@ -1,10 +1,12 @@
 #include <Core/Asset/FileData.hpp>
 #include <Core/Asset/VolumeData.hpp>
 #include <Core/Geometry/Volume.hpp>
+#include <Core/Math/Math.hpp>
 #include <IO/VolumesLoader/VolumeLoader.hpp>
 #include <catch2/catch.hpp>
 
 TEST_CASE( "IO/VolumesLoader", "[IO]" ) {
+    using namespace Ra::Core;
     using namespace Ra::IO;
     using namespace Ra::Core::Asset;
     using namespace Ra::Core::Geometry;
@@ -38,9 +40,33 @@ TEST_CASE( "IO/VolumesLoader", "[IO]" ) {
         auto handle_openvdb = loader.handleFileExtension( "vdb" );
         REQUIRE( !handle_openvdb );
     }
-
+    SECTION( "Loading PVM unkown file" ) {
+        auto loadedFile = loader.loadFile( "unknown.pvm" );
+        REQUIRE( loadedFile == nullptr );
+    }
     SECTION( "Loading PVM data file" ) {
-        std::unique_ptr<FileData> loadedFile( loader.loadFile( "data/Lobster.pvm" ) );
+        auto loadedFile = loader.loadFile( "data/Bucky.pvm" );
+        REQUIRE( loadedFile != nullptr );
+        auto volumeFiledata = loadedFile->getVolumeData();
+        REQUIRE( volumeFiledata.size() == 1 );
+        auto abstractvolume = volumeFiledata[0]->volume;
+        REQUIRE( abstractvolume != nullptr );
+        REQUIRE( abstractvolume->getType() == AbstractVolume::VolumeStorageType::DISCRETE_DENSE );
+
+        auto volumeData = dynamic_cast<VolumeGrid*>( abstractvolume );
+        REQUIRE( volumeData != nullptr );
+
+        auto volumeSize = volumeData->size();
+        REQUIRE(
+            ( ( volumeSize[0] == 32 ) && ( volumeSize[1] == 32 ) && ( volumeSize[2] == 32 ) ) );
+
+        auto binsize = volumeData->binSize();
+        REQUIRE( ( Math::areApproxEqual( binsize[0], 1_ra ) &&
+                   Math::areApproxEqual( binsize[1], 1_ra ) &&
+                   Math::areApproxEqual( binsize[2], 1_ra ) ) );
+    }
+    SECTION( "Loading PVM data file" ) {
+        auto loadedFile = loader.loadFile( "data/Lobster.pvm" );
         REQUIRE( loadedFile != nullptr );
         auto volumeFiledata = loadedFile->getVolumeData();
         REQUIRE( volumeFiledata.size() == 1 );
@@ -56,7 +82,8 @@ TEST_CASE( "IO/VolumesLoader", "[IO]" ) {
             ( ( volumeSize[0] == 301 ) && ( volumeSize[1] == 324 ) && ( volumeSize[2] == 56 ) ) );
 
         auto binsize = volumeData->binSize();
-        REQUIRE(
-            ( ( binsize[0] == 1._ra ) && ( binsize[1] == 1._ra ) && ( binsize[2] == 1.4_ra ) ) );
+        REQUIRE( ( Math::areApproxEqual( binsize[0], 1._ra ) &&
+                   Math::areApproxEqual( binsize[1], 1._ra ) &&
+                   Math::areApproxEqual( binsize[2], 1.4_ra ) ) );
     }
 }
