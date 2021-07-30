54379586f9a4a54c4bc8e127e5af940f99da4213
Nicolas Mellado <nmellado0@gmail.com>
2021-07-30 14:12:31 +0200

    [core] Revert use of mmalloc for non-arm platforms

--- src/Core/Containers/AlignedAllocator.hpp
+++ src/Core/Containers/AlignedAllocator.hpp
@@ -3,6 +3,8 @@
 
 #ifdef _WIN32
 #    include <malloc.h>
+#elif not ( defined ARCH_ARM32 || defined ARCH_ARM64 )
+#    include <mm_malloc.h>
 #endif
 #include <cstddef>
 #include <cstdint>
@@ -87,7 +89,7 @@ class AlignedAllocator
         // be thrown in the case of integer overflow.
         CORE_ASSERT( n <= max_size(), "Integer overflow" );
 
-#ifdef _WIN32
+#if defined _WIN32 || not ( defined ARCH_ARM32 || defined ARCH_ARM64 )
         void* const pv = _mm_malloc( n * sizeof( T ), Alignment );
 #else
         void* const pv = aligned_alloc( Alignment, n * sizeof( T ) );
@@ -102,7 +104,7 @@ class AlignedAllocator
     }
 
     void deallocate( T* const p, const std::size_t /*n*/ ) const {
-#ifdef _WIN32
+#if defined _WIN32 || not ( defined ARCH_ARM32 || defined ARCH_ARM64 )
         _mm_free( p );
 #else
         free( p );
