f6f414d07e5aca13e6e7302ee1840a22b5aa1419
Mathias Paulin <mathias.paulin@irit.fr>
2022-01-05 12:43:56 +0100

    Format

--- Shaders/Picking/PickingPoints.geom.glsl
+++ Shaders/Picking/PickingPoints.geom.glsl
@@ -68,8 +68,7 @@ void main() {
     uv[2] = vec2( +1, -1 );
     uv[3] = vec2( +1, +1 );
 
-    for ( int idx = 0; idx < 4; ++idx )
-    {
+    for ( int idx = 0; idx < 4; ++idx ) {
         gl_Position   = transform.proj * transform.view * vec4( point[idx], 1 );
         out_position  = point[idx];
         out_normal    = normal;
