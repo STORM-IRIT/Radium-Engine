8b5f664df9961ecfde56160d495059a81ee566d9
dlyr <github@dlyr.fr>
2022-03-09 13:09:49 +0100

    [core] add explicit to ObjectWithSemantic ctor.

--- src/Core/Utils/ObjectWithSemantic.hpp
+++ src/Core/Utils/ObjectWithSemantic.hpp
@@ -56,9 +56,9 @@ class RA_CORE_API ObjectWithSemantic
 
   protected:
     template <class... SemanticNames>
-    inline ObjectWithSemantic( SemanticNames... names ) : m_names { names... } {}
+    inline explicit ObjectWithSemantic( SemanticNames... names ) : m_names { names... } {}
 
-    inline ObjectWithSemantic( const SemanticNameCollection& otherNames ) :
+    inline explicit ObjectWithSemantic( const SemanticNameCollection& otherNames ) :
         m_names { otherNames } {}
 
   private:
