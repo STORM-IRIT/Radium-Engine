d245ba102893f0a6c43c5def88164b70106056ed
nmellado <nmellado@users.noreply.github.com>
2022-04-21 12:36:46 +0000

    deploy: 474fe5ec3e789ba71716d71b8e4fcd1634c2f467

--- release-candidate/MeshPrimitives_8inl.js
+++ release-candidate/MeshPrimitives_8inl.js
@@ -1,5 +1,5 @@
 var MeshPrimitives_8inl =
 [
-    [ "makeParametricSphere", "MeshPrimitives_8inl.html#a3924dd04a97acc1a70488f1898879528", null ],
-    [ "makeParametricTorus", "MeshPrimitives_8inl.html#a04686f466f9df16ac6424ee42c601931", null ]
+    [ "makeParametricSphere", "MeshPrimitives_8inl.html#a8df5bdede5cff803cf7c4b8eb7a74cd8", null ],
+    [ "makeParametricTorus", "MeshPrimitives_8inl.html#a1ed51b14187cfc796e6199a4db5a3ae8", null ]
 ];
