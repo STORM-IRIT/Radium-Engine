ea143259a0795db61b4b0c2be136561e0b4df46b
dlyr <github@dlyr.fr>
2022-04-27 10:37:38 +0200

    [scripts] update list dep to new cmake format

--- scripts/list_dep.py
+++ scripts/list_dep.py
@@ -9,32 +9,50 @@ p = Path('.')
 
 filenames=list(p.glob('**/CMakeLists.txt'))
 
-name = re.compile(r'Add\([ ]*(.*)')
+name = re.compile(r'Add\([\s]*(.*)')
 git = re.compile(r'GIT_REPOSITORY (.*)')
-tag = re.compile(r'GIT_TAG (.*)')
-option = re.compile(r'(-D.*)')
-optionfilter = re.compile(r'indent')
+tag = re.compile(r'[^\)]*GIT_TAG (.*)')
+option = re.compile(r'[^\)](-D\S*)')
 installdir = re.compile(r'set\([^ ]* (${CMAKE_INSTALL_PREFIX}.*)\)')
 installdir = re.compile(r'set\([^ ]* (.*)\)')
+optionfilter = re.compile(r'indent')
 
 dep = {}
 for filename in filenames:
     with open(filename, "r") as f:
         currentname="dummy"
-        for line in f:
-            match = name.search(line)
-            if match:
-                currentname=match.group(1)
-                dep[currentname]={}
-            for key,regex  in zip(['git', 'tag', 'option','installdir'], [git, tag, option, installdir]):
-                match = regex.search(line)
-                if match and not optionfilter.search(line) and currentname != 'dummy':
+        filetext = f.read()
+        start=0
+
+        match = name.search(filetext)
+        while match:
+            currentname=match.group(1)
+            dep[currentname]={}
+            start = match.end(1)
+            newstart = start
+            for key,regex  in zip(['git', 'tag', 'installdir'], [git, tag, installdir]):
+                match2 = regex.search(filetext, start)
+                if match2 and currentname != 'dummy':
                     if key in dep[currentname]:
-                        dep[currentname][key] += " "+match.group(1)
+                        dep[currentname][key] += " "+match2.group(1)
                     else:
-                        dep[currentname][key] = match.group(1)
+                        dep[currentname][key] = match2.group(1)
+                    newstart = max(match2.end(1), newstart)
+            key,regex = 'option', option
+            match3 = regex.search(filetext, start)
+            while match3:
+                if match3 and not optionfilter.search(match3.group(1)) and currentname != 'dummy':
+                    if key in dep[currentname]:
+                        dep[currentname][key] += " "+match3.group(1)
+                    else:
+                        dep[currentname][key] = match3.group(1)
+                newstart = max(match3.end(1), newstart)
+                match3 = regex.search(filetext, newstart)
             if  currentname != 'dummy' and not 'installdir' in dep[currentname]:
                 dep[currentname]['installdir']="default"
+            start = newstart
+            match = name.search(filetext, start)
+
 
 for key in dep:
     print(f" *  `{key}_DIR`")
