f2096419187effba3da95ef9d5dbb2fa7241ea12
dlyr <github@dlyr.fr>
2022-05-09 19:27:12 +0200

    [engine] use templated adater for compile time function def.

--- src/Engine/Data/ShaderProgram.hpp
+++ src/Engine/Data/ShaderProgram.hpp
@@ -47,9 +47,6 @@ class RA_ENGINE_API ShaderProgram final
     /// Uniform setters
     template <typename T>
     void setUniform( const char* name, const T& value ) const;
-    template <typename T, typename E>
-    void setUniform( const char* name, const T& value ) const;
-
     void setUniform( const char* name, Texture* tex, int texUnit ) const;
 
     //! use automatic texture unit computation
