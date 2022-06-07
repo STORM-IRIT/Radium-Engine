87d13160e8721aa05b7fc2f6e980adce688dfe5e
dlyr <github@dlyr.fr>
2022-06-07 12:02:30 +0200

    [tests] update tests WIP

--- tests/unittest/Core/geometryData.cpp
+++ tests/unittest/Core/geometryData.cpp
@@ -1,3 +1,4 @@
+#include "Core/Geometry/StandardAttribNames.hpp"
 #include <Core/Asset/GeometryData.hpp>
 #include <catch2/catch.hpp>
 
@@ -9,25 +10,25 @@ TEST_CASE( "Core/Asset/GeometryData", "[Core][Core/Asset][GeometryData]" ) {
     SECTION( "Normal test" ) {
         auto geometry = new GeometryData();
         auto& normal  = geometry->getNormals();
+        auto& name    = Ra::Core::Geometry::getAttribName( MeshAttrib::VERTEX_NORMAL );
+
         REQUIRE( normal.empty() );
-        REQUIRE(
-            geometry->getAttribManager().getAttribBase( MeshAttrib::VERTEX_NORMAL )->isLocked() );
+        REQUIRE( geometry->getAttribManager().getAttribBase( name )->isLocked() );
 
         normal.resize( 1, Ra::Core::Vector3::Zero() );
         normal[0] = Ra::Core::Vector3().setRandom();
         auto save = normal[0];
-        geometry->getAttribManager().getAttribBase( MeshAttrib::VERTEX_NORMAL )->unlock();
+        geometry->getAttribManager().getAttribBase( name )->unlock();
         REQUIRE( !normal.empty() );
+        REQUIRE( !geometry->getAttribManager().getAttribBase( name )->isLocked() );
 
-        auto& name        = Ra::Core::Geometry::getAttribName( MeshAttrib::VERTEX_NORMAL );
         auto attriHandler = geometry->getAttribManager().findAttrib<Ra::Core::Vector3>( name );
         const auto& data  = geometry->getAttribManager().getAttrib( attriHandler ).data();
         REQUIRE( save == data[0] );
 
         auto geometry2 = new GeometryData();
         geometry2->setNormals( normal );
-        auto& name2        = Ra::Core::Geometry::getAttribName( MeshAttrib::VERTEX_NORMAL );
-        auto attriHandler2 = geometry2->getAttribManager().findAttrib<Ra::Core::Vector3>( name2 );
+        auto attriHandler2 = geometry2->getAttribManager().findAttrib<Ra::Core::Vector3>( name );
         const auto& attrib = geometry2->getAttribManager().getAttrib( attriHandler2 );
         REQUIRE( attrib.data().size() == 1 );
         REQUIRE( attrib.data()[0] == save );
@@ -45,122 +46,147 @@ TEST_CASE( "Core/Asset/GeometryData", "[Core][Core/Asset][GeometryData]" ) {
         auto save = vertex[0];
         geometry->getAttribManager().getAttribBase( MeshAttrib::VERTEX_POSITION )->unlock();
         REQUIRE( !vertex.empty() );
+        REQUIRE( !geometry->getAttribManager()
+                      .getAttribBase( MeshAttrib::VERTEX_POSITION )
+                      ->isLocked() );
 
-        auto& name        = Ra::Core::Geometry::getAttribName( MeshAttrib::VERTEX_POSITION );
-        auto attriHandler = geometry->getAttribManager().findAttrib<Ra::Core::Vector3>( name );
-        const auto& data  = geometry->getAttribManager().getAttrib( attriHandler ).data();
+        auto& name       = Ra::Core::Geometry::getAttribName( MeshAttrib::VERTEX_POSITION );
+        auto attriHandle = geometry->getAttribManager().findAttrib<Ra::Core::Vector3>( name );
+        const auto& data = geometry->getAttribManager().getAttrib( attriHandle ).data();
         REQUIRE( save == data[0] );
 
         auto geometry2 = new GeometryData();
-        geometry2->setVertices( vertex );
-        auto& name2        = Ra::Core::Geometry::getAttribName( MeshAttrib::VERTEX_POSITION );
-        auto attriHandler2 = geometry2->getAttribManager().findAttrib<Ra::Core::Vector3>( name2 );
-        const auto& attrib = geometry2->getAttribManager().getAttrib( attriHandler2 );
+        geometry2->getMultiIndexedGeometry().setVertices(
+            geometry->getMultiIndexedGeometry().vertices() );
+        auto attriHandle2  = geometry2->getAttribManager().findAttrib<Ra::Core::Vector3>( name );
+        const auto& attrib = geometry2->getAttribManager().getAttrib( attriHandle2 );
         REQUIRE( attrib.data().size() == 1 );
         REQUIRE( attrib.data()[0] == save );
     }
 
     SECTION( "Tangent, BiTangent, TexCoord tests" ) {
-        auto geometry = new GeometryData();
+        auto geom               = new GeometryData();
+        auto& tangentAttribName = getAttribName( Ra::Core::Geometry::MeshAttrib::VERTEX_TANGENT );
+        auto tangentAttribHandle =
+            geom->getMultiIndexedGeometry().addAttrib<Ra::Core::Vector3>( tangentAttribName );
+        auto& tangents =
+            geom->getMultiIndexedGeometry().getAttrib( tangentAttribHandle ).getDataWithLock();
 
-        // Tangent test
-        auto& tangents = geometry->getTangents();
         REQUIRE( tangents.empty() );
-        REQUIRE(
-            geometry->getAttribManager().getAttribBase( MeshAttrib::VERTEX_TANGENT )->isLocked() );
+        REQUIRE( geom->getAttribManager().getAttribBase( tangentAttribName )->isLocked() );
 
         tangents.resize( 1, Ra::Core::Vector3::Zero() );
         tangents[0]  = Ra::Core::Vector3().setRandom();
         auto saveTan = tangents[0];
-        geometry->getAttribManager().getAttribBase( MeshAttrib::VERTEX_TANGENT )->unlock();
-        REQUIRE( !tangents.empty() );
+        geom->getAttribManager().getAttribBase( tangentAttribName )->unlock();
 
-        auto& nameTan = Ra::Core::Geometry::getAttribName( MeshAttrib::VERTEX_TANGENT );
-        auto attribHandlerTan =
-            geometry->getAttribManager().findAttrib<Ra::Core::Vector3>( nameTan );
-        const auto& dataTan = geometry->getAttribManager().getAttrib( attribHandlerTan ).data();
+        REQUIRE( !tangents.empty() );
+        REQUIRE( !geom->getAttribManager().getAttribBase( tangentAttribName )->isLocked() );
+
+        auto tangentAttribHandle2 =
+            geom->getAttribManager().findAttrib<Ra::Core::Vector3>( tangentAttribName );
+        const auto& dataTan = geom->getAttribManager().getAttrib( tangentAttribHandle2 ).data();
         REQUIRE( saveTan == dataTan[0] );
 
-        auto geoTanTest = new GeometryData();
-        geoTanTest->setTangents( tangents );
-        auto attribHandlerTan2 =
-            geoTanTest->getAttribManager().findAttrib<Ra::Core::Vector3>( nameTan );
-        const auto& attribTan = geoTanTest->getAttribManager().getAttrib( attribHandlerTan2 );
-        REQUIRE( attribTan.data().size() == 1 );
-        REQUIRE( attribTan.data()[0] == saveTan );
+        auto geom2 = new GeometryData();
+        auto geom2TangentAttribHandle2 =
+            geom2->getMultiIndexedGeometry().addAttrib<Ra::Core::Vector3>( tangentAttribName );
+        geom2->getMultiIndexedGeometry()
+            .getAttrib<Ra::Core::Vector3>( tangentAttribHandle2 )
+            .setData( dataTan );
 
-        // BiTangent test
-        auto& biTangents = geometry->getBiTangents();
-        REQUIRE( biTangents.empty() );
-        REQUIRE( geometry->getAttribManager()
-                     .getAttribBase( MeshAttrib::VERTEX_BITANGENT )
-                     ->isLocked() );
+        auto tangentAttribHandle3 =
+            geom2->getAttribManager().findAttrib<Ra::Core::Vector3>( tangentAttribName );
 
-        biTangents.resize( 1, Ra::Core::Vector3::Zero() );
-        biTangents[0]  = Ra::Core::Vector3().setRandom();
-        auto saveBiTan = biTangents[0];
-        geometry->getAttribManager().getAttribBase( MeshAttrib::VERTEX_BITANGENT )->unlock();
-        REQUIRE( !biTangents.empty() );
+        const auto& tangents2 = geom2->getAttribManager().getAttrib( tangentAttribHandle3 );
 
-        auto& nameBiTan = Ra::Core::Geometry::getAttribName( MeshAttrib::VERTEX_BITANGENT );
-        auto attribHandlerBiTan =
-            geometry->getAttribManager().findAttrib<Ra::Core::Vector3>( nameBiTan );
-        const auto& dataBiTan = geometry->getAttribManager().getAttrib( attribHandlerBiTan ).data();
-        REQUIRE( saveBiTan == dataBiTan[0] );
+        REQUIRE( tangents2.data().size() == 1 );
+        REQUIRE( tangents2.data()[0] == saveTan );
+        /// \todo to be continued
+        /*
 
-        auto geoBiTanTest = new GeometryData();
-        geoBiTanTest->setBitangents( biTangents );
-        auto attribHandlerBiTan2 =
-            geoBiTanTest->getAttribManager().findAttrib<Ra::Core::Vector3>( nameBiTan );
-        const auto& attribBiTan = geoBiTanTest->getAttribManager().getAttrib( attribHandlerBiTan2 );
+                // BiTangent test
+                auto& biTangents = geometry->getBiTangents();
+                auto& biTangentAttribName =
+                    Ra::Core::Geometry::getAttribName( MeshAttrib::VERTEX_BITANGENT );
+                REQUIRE( biTangents.empty() );
+                REQUIRE( geometry->getAttribManager().getAttribBase( biTangentAttribName
+           )->isLocked() );
 
-        REQUIRE( attribBiTan.data().size() == 1 );
-        REQUIRE( attribBiTan.data()[0] == saveBiTan );
+                biTangents.resize( 1, Ra::Core::Vector3::Zero() );
+                biTangents[0]  = Ra::Core::Vector3().setRandom();
+                auto saveBiTan = biTangents[0];
+                geometry->getAttribManager().getAttribBase( biTangentAttribName )->unlock();
+                REQUIRE( !biTangents.empty() );
 
-        // TexCoords test
-        auto& texCoords = geometry->getTexCoords();
-        REQUIRE( texCoords.empty() );
-        REQUIRE(
-            geometry->getAttribManager().getAttribBase( MeshAttrib::VERTEX_TEXCOORD )->isLocked() );
 
-        texCoords.resize( 1, Ra::Core::Vector3::Zero() );
-        texCoords[0]       = Ra::Core::Vector3().setRandom();
-        auto saveTexCoords = texCoords[0];
-        geometry->getAttribManager().getAttribBase( MeshAttrib::VERTEX_TEXCOORD )->unlock();
-        REQUIRE( !texCoords.empty() );
+                        auto attribHandlerBiTan =
+                            geometry->getAttribManager().findAttrib<Ra::Core::Vector3>(
+           biTangentAttribName
+                ); const auto& dataBiTan = geometry->getAttribManager().getAttrib(
+           attribHandlerBiTan
+                ).data(); REQUIRE( saveBiTan == dataBiTan[0] );
 
-        auto& nameTexCoords = Ra::Core::Geometry::getAttribName( MeshAttrib::VERTEX_TEXCOORD );
-        auto attribHandlerTexCoords =
-            geometry->getAttribManager().findAttrib<Ra::Core::Vector3>( nameTexCoords );
-        const auto& dataTexCoords =
-            geometry->getAttribManager().getAttrib( attribHandlerTexCoords ).data();
-        REQUIRE( saveTexCoords == dataTexCoords[0] );
+                        auto geoBiTanTest = new GeometryData();
+                        geoBiTanTest->setBitangents( biTangents );
+                        auto attribHandlerBiTan2 =
+                            geoBiTanTest->getAttribManager().findAttrib<Ra::Core::Vector3>(
+                biTangentAttribName ); const auto& attribBiTan =
+           geoBiTanTest->getAttribManager().getAttrib( attribHandlerBiTan2 );
 
-        auto geoTexCoordsTest = new GeometryData();
-        geoTexCoordsTest->setTextureCoordinates( texCoords );
-        auto attribHandlerTexCoords2 =
-            geoTexCoordsTest->getAttribManager().findAttrib<Ra::Core::Vector3>( nameTexCoords );
-        const auto& attribTexCoords =
-            geoTexCoordsTest->getAttribManager().getAttrib( attribHandlerTexCoords2 );
-        REQUIRE( attribTexCoords.data().size() == 1 );
-        REQUIRE( attribTexCoords.data()[0] == saveTexCoords );
+                        REQUIRE( attribBiTan.data().size() == 1 );
+                        REQUIRE( attribBiTan.data()[0] == saveBiTan );
 
-        geometry->getAttrib<Ra::Core::Vector3>( MeshAttrib::VERTEX_TANGENT ).getDataWithLock();
-        REQUIRE(
-            geometry->getAttribManager().getAttribBase( MeshAttrib::VERTEX_TANGENT )->isLocked() );
-        {
-            auto unlocker = geometry->getAttribManager().getUnlocker();
-            geometry->getAttrib<Ra::Core::Vector3>( MeshAttrib::VERTEX_BITANGENT )
-                .getDataWithLock();
-            REQUIRE( geometry->getAttribManager()
-                         .getAttribBase( MeshAttrib::VERTEX_BITANGENT )
-                         ->isLocked() );
-        }
-        REQUIRE(
-            geometry->getAttribManager().getAttribBase( MeshAttrib::VERTEX_TANGENT )->isLocked() );
-        REQUIRE( !geometry->getAttribManager()
-                      .getAttribBase( MeshAttrib::VERTEX_BITANGENT )
-                      ->isLocked() );
+                        // TexCoords test
+                        auto& texCoords = geometry->getTexCoords();
+                        REQUIRE( texCoords.empty() );
+                        REQUIRE(
+                            geometry->getAttribManager().getAttribBase( MeshAttrib::VERTEX_TEXCOORD
+                )->isLocked() );
+
+                        texCoords.resize( 1, Ra::Core::Vector3::Zero() );
+                        texCoords[0]       = Ra::Core::Vector3().setRandom();
+                        auto saveTexCoords = texCoords[0];
+                        geometry->getAttribManager().getAttribBase( MeshAttrib::VERTEX_TEXCOORD
+           )->unlock(); REQUIRE( !texCoords.empty() );
+
+                        auto& nameTexCoords = Ra::Core::Geometry::getAttribName(
+           MeshAttrib::VERTEX_TEXCOORD
+                ); auto attribHandlerTexCoords =
+           geometry->getAttribManager().findAttrib<Ra::Core::Vector3>( nameTexCoords ); const auto&
+           dataTexCoords = geometry->getAttribManager().getAttrib( attribHandlerTexCoords ).data();
+           REQUIRE( saveTexCoords == dataTexCoords[0] );
+
+                        auto geoTexCoordsTest = new GeometryData();
+                        geoTexCoordsTest->setTextureCoordinates( texCoords );
+                        auto attribHandlerTexCoords2 =
+                            geoTexCoordsTest->getAttribManager().findAttrib<Ra::Core::Vector3>(
+                nameTexCoords ); const auto& attribTexCoords =
+                            geoTexCoordsTest->getAttribManager().getAttrib( attribHandlerTexCoords2
+           ); REQUIRE( attribTexCoords.data().size() == 1 ); REQUIRE( attribTexCoords.data()[0] ==
+           saveTexCoords );
+
+                        geometry->getMultiIndexedGeometry()
+                            .getAttrib<Ra::Core::Vector3>( tangentAttribName )
+                            .getDataWithLock();
+                        REQUIRE( geometry->getAttribManager().getAttribBase( tangentAttribName
+           )->isLocked()
+                );
+                        {
+                            auto unlocker = geometry->getAttribManager().getUnlocker();
+                            geometry->getMultiIndexedGeometry()
+                                .getAttrib<Ra::Core::Vector3>( getAttribName(
+           MeshAttrib::VERTEX_BITANGENT ) ) .getDataWithLock(); REQUIRE(
+           geometry->getAttribManager() .getAttribBase( getAttribName( MeshAttrib::VERTEX_BITANGENT
+           ) )
+                                         ->isLocked() );
+                        }
+                        REQUIRE( geometry->getAttribManager().getAttribBase( tangentAttribName
+           )->isLocked()
+                ); REQUIRE( !geometry->getAttribManager().getAttribBase( biTangentAttribName
+           )->isLocked()
+                );
+                */
     }
 
     SECTION( "Edges test " ) {
@@ -200,7 +226,8 @@ TEST_CASE( "Core/Asset/GeometryData", "[Core][Core/Asset][GeometryData]" ) {
 
         auto geometry2 = GeometryData();
         geometry2.setFaces( faces );
-        // geometry2.setIndexedData(GeometryData::GeometryType::POLY_MESH, faces, "in_face");
+        //        geometry2.setIndexedData( GeometryData::GeometryType::POLY_MESH, faces, "in_face"
+        //        );
         REQUIRE( geometry2.getFaces().size() == 1 );
         REQUIRE( geometry.getFaces()[0] == save );
     }
