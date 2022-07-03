d8e473ef8aba2c45c5ab538cb86d4b8dec258582
David Vanderhaeghe <github@dlyr.fr>
2022-07-03 17:36:26 +0200

    [engine] Update comments.
    Co-authored-by: Mathias Paulin <MathiasPaulin@users.noreply.github.com>

--- src/Engine/Data/Mesh.hpp
+++ src/Engine/Data/Mesh.hpp
@@ -113,12 +113,12 @@ class RA_ENGINE_API AttribArrayDisplayable : public Displayable
     virtual Core::Geometry::AttribArrayGeometry& getAttribArrayGeometry()             = 0;
     ///@}
 
-    /// get opengl's vbo handle (uint) corresponding to attrib \b name.
-    /// if vbo is not initialized or name do not correponds to an actual attrib name, the returned
+    /// \brief Get opengl's vbo handle (uint) corresponding to attrib \b name.
+    /// If vbo is not initialized or name do not correponds to an actual attrib name, the returned
     /// optional is empty
     Ra::Core::Utils::optional<gl::GLuint> getVboHandle( const std::string& name );
-    /// get opengl's vao handle (uint)
-    /// if vao is not initialized, the returned optional is empty
+    /// \brief Get opengl's vao handle (uint)
+    /// If vao is not initialized, the returned optional is empty
     Ra::Core::Utils::optional<gl::GLuint> getVaoHandle();
 
   protected:
