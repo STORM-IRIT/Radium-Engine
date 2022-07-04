b840ce135229fa08faef3ffae7eddec7a255e1a9
Mafo369 <themafo369@gmail.com>
2022-07-04 18:27:29 +0200

    [tests] Add curve editor example

---
+++ examples/CurveEditor/Gui/MyViewer.cpp
@@ -0,0 +1,13 @@
+#include "MyViewer.hpp"
+
+MyViewer::MyViewer() : Ra::Gui::Viewer() {
+
+}
+
+void MyViewer::mouseDoubleClickEvent(QMouseEvent *event) {
+    emit onMouseDoubleClick( event );
+}
+
+MyViewer::~MyViewer() {
+
+}
