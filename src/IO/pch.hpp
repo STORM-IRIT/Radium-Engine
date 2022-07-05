6a9eef0d9cf423bddd374778f6cac41f82253d6e
dlyr <github@dlyr.fr>
2022-07-05 08:51:22 +0200

    [cmake] use pch.hpp as pre complied header list

---
+++ src/IO/pch.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <Core/Asset/DataLoader.hpp>
+#include <Core/Asset/FileData.hpp>
+#include <Core/Asset/FileLoaderInterface.hpp>
+#include <Core/Types.hpp>
+#include <Core/Utils/Log.hpp>
+#include <assimp/mesh.h>
+#include <assimp/scene.h>
+#include <fstream>
+#include <iostream>
+#include <set>
+#include <string>
