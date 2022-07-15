e14784522cf76e467b5969e304d73bc7cfb175dc
dlyr <github@dlyr.fr>
2022-07-15 13:31:54 +0200

    [cmake] Take into account QT_DEFAULT_MAJOR_VERSION.
    Finding both qt in this case failed (moc problem ?).

--- cmake/QtFunctions.cmake
+++ cmake/QtFunctions.cmake
@@ -20,7 +20,9 @@ macro(find_qt_package)
         set(MY_OPTIONS_COMPONENTS "")
     endif()
 
-    find_package(Qt6 COMPONENTS ${MY_OPTIONS_COMPONENTS} QUIET)
+    if(NOT QT_DEFAULT_MAJOR_VERSION OR QT_DEFAULT_MAJOR_VERSION STREQUAL "6")
+        find_package(Qt6 COMPONENTS ${MY_OPTIONS_COMPONENTS} QUIET)
+    endif()
     if(NOT Qt6_FOUND)
         if(${MY_OPTIONS_REQUIRED})
             find_package(Qt5 5.15 COMPONENTS ${MY_OPTIONS_COMPONENTS} REQUIRED)
@@ -30,30 +32,7 @@ macro(find_qt_package)
     endif()
 endmacro()
 
-# Find Qt5 or Qt6 dependency Parameters: COMPONENTS <component_list>: optional parameter listing the
-# Qt packages (e.g. Core, Widgets REQUIRED: optional parameter propagated to find_package
-#
-# Usage example: find_package(Qt5 COMPONENTS Core Widgets OpenGL Xml REQUIRED)
-#
-# Qt5 and Qt6 can be retrieved using versionless targets introduced in Qt5.15:
-# https://doc.qt.io/qt-6/cmake-qt5-and-qt6-compatibility.html#versionless-targets
+# see find_qt_package
 macro(find_qt_dependency)
-    set(options REQUIRED)
-    set(oneValueArgs "")
-    set(multiValueArgs COMPONENTS)
-
-    cmake_parse_arguments(MY_OPTIONS "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
-
-    if(NOT MY_OPTIONS_COMPONENTS) # User didn't enter any component
-        set(MY_OPTIONS_COMPONENTS "")
-    endif()
-
-    find_package(Qt6 COMPONENTS ${MY_OPTIONS_COMPONENTS} QUIET)
-    if(NOT Qt6_FOUND)
-        if(${MY_OPTIONS_REQUIRED})
-            find_package(Qt5 5.15 COMPONENTS ${MY_OPTIONS_COMPONENTS} REQUIRED)
-        else()
-            find_package(Qt5 5.15 COMPONENTS ${MY_OPTIONS_COMPONENTS})
-        endif()
-    endif()
+    find_qt_package(${ARGN})
 endmacro()
