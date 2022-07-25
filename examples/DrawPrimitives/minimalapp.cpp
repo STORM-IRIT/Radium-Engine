5550f9a1e2885d9238071ea688dd5355b158af90
dlyr <github@dlyr.fr>
2022-07-25 07:50:26 +0200

    [examples] DrawPrimitives cleanup

--- examples/DrawPrimitives/minimalapp.cpp
+++ examples/DrawPrimitives/minimalapp.cpp
@@ -12,21 +12,12 @@
 using namespace Ra;
 using namespace Ra::Gui;
 
-MinimalApp::MinimalApp( int& argc, char** argv ) :
-    QApplication( argc, argv ),
-    m_engine( nullptr ),
-    m_taskQueue( nullptr ),
-    m_viewer( nullptr ),
-    m_frameTimer( nullptr ),
-    m_target_fps( 60 ) {
-    // Set application and organization names in order to ensure uniform
-    // QSettings configurations.
-    // \see http://doc.qt.io/qt-5/qsettings.html#QSettings-4
-    QCoreApplication::setOrganizationName( "STORM-IRIT" );
-    QCoreApplication::setApplicationName( "HelloRadium" );
+MinimalApp::MinimalApp( int& argc, char** argv ) : QApplication( argc, argv ) {
 
-    ///\todo update when a basic viewer is implemented ... (to call setupKeyMappingCallbacks)
-    Ra::Gui::KeyMappingManager::createInstance();
+    QCoreApplication::setOrganizationName( "STORM-IRIT" );
+    QCoreApplication::setApplicationName( "DrawPrimitives" );
+
+    KeyMappingManager::createInstance();
 }
 
 MinimalApp::~MinimalApp() {
@@ -49,19 +40,15 @@ void MinimalApp::initialize() {
     QSurfaceFormat::setDefaultFormat( format );
 
     // Initialize Engine.
-    m_engine.reset( Ra::Engine::RadiumEngine::createInstance() );
+    m_engine.reset( Engine::RadiumEngine::createInstance() );
     m_engine->initialize();
 
     // Initialize taskqueue.
-    m_taskQueue.reset( new Ra::Core::TaskQueue( std::thread::hardware_concurrency() - 1 ) );
+    m_taskQueue.reset( new Core::TaskQueue( std::thread::hardware_concurrency() - 1 ) );
+
     // Initialize viewer.
-
-    m_viewer.reset( new Ra::Gui::Viewer );
+    m_viewer.reset( new Viewer );
     CORE_ASSERT( m_viewer != nullptr, "GUI was not initialized" );
-    m_viewer->setObjectName( QStringLiteral( "m_viewer" ) );
-
-    auto viewerWidget = QWidget::createWindowContainer( m_viewer.get() );
-    viewerWidget->setAutoFillBackground( false );
 
     m_viewer->setupKeyMappingCallbacks();
     auto keyMappingManager = KeyMappingManager::getInstance();
@@ -69,27 +56,31 @@ void MinimalApp::initialize() {
         RotateAroundCameraManipulator::KeyMapping::configureKeyMapping );
 
     connect( m_viewer.get(),
-             &Ra::Gui::Viewer::requestEngineOpenGLInitialization,
+             &Viewer::requestEngineOpenGLInitialization,
              this,
              &MinimalApp::onRequestEngineOpenGLInitialization );
-    connect( m_viewer.get(), &Ra::Gui::Viewer::glInitialized, this, &MinimalApp::onGLInitialized );
-    connect( this, &QGuiApplication::lastWindowClosed, m_viewer.get(), &Gui::WindowQt::cleanupGL );
+    connect( m_viewer.get(), &Viewer::glInitialized, this, &MinimalApp::onGLInitialized );
+    connect( this, &QGuiApplication::lastWindowClosed, m_viewer.get(), &WindowQt::cleanupGL );
 
     CORE_ASSERT( m_viewer->getContext() != nullptr, "OpenGL context was not created" );
     CORE_ASSERT( m_viewer->getContext()->isValid(), "OpenGL was not initialized" );
 
     // Initialize timer for the spinning cube.
     m_frameTimer = new QTimer( this );
-    m_frameTimer->setInterval( 1000 / m_target_fps );
+    m_frameTimer->setInterval( 1000 / m_targetFps );
 
+    // Create a window container for the viewer.
+    // using viewer directly as main windows fail to have valid glbinding context
+    // on Debian bullseye, Qt version 6.2.4
+    auto viewerWidget = QWidget::createWindowContainer( m_viewer.get() );
+    viewerWidget->setAutoFillBackground( false );
     viewerWidget->resize( 500, 500 );
     viewerWidget->show();
 }
 
 void MinimalApp::onGLInitialized() {
     // add the renderer
-    std::shared_ptr<Ra::Engine::Rendering::Renderer> e(
-        new Ra::Engine::Rendering::ForwardRenderer() );
+    std::shared_ptr<Engine::Rendering::Renderer> e( new Engine::Rendering::ForwardRenderer() );
     m_viewer->addRenderer( e );
 
     m_viewer->setCameraManipulator( new RotateAroundCameraManipulator(
@@ -106,7 +97,7 @@ void MinimalApp::onRequestEngineOpenGLInitialization() {
 void MinimalApp::frame() {
     // We use a fixed time step, but it is also possible
     // to check the time from last frame.
-    const Scalar dt = 1.f / Scalar( m_target_fps );
+    const Scalar dt = 1.f / Scalar( m_targetFps );
 
     // Collect and run tasks
     m_engine->getTasks( m_taskQueue.get(), dt );
