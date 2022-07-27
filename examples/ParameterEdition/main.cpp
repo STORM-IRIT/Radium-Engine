2d356c7cd2900c95e99ac3a1629595c4b8fb0e58
Mathias Paulin <mathias.paulin@irit.fr>
2022-07-27 18:20:05 +0200

    [examples] Update ParameterEditionExample to new enum management

--- examples/ParameterEdition/main.cpp
+++ examples/ParameterEdition/main.cpp
@@ -1,5 +1,6 @@
 // Include Radium base application and its simple Gui
 
+#include <Core/Utils/TypesUtils.hpp>
 #include <Engine/Data/RenderParameters.hpp>
 #include <Gui/ParameterSetEditor/ParameterSetEditor.hpp>
 
@@ -13,14 +14,16 @@
 
 using namespace Ra::Engine::Data;
 using namespace Ra::Gui;
+using namespace Ra::Core;
 
-enum Values : int { VALUE_0 = 10, VALUE_1 = 20, VALUE_2 = 30 };
+/* Changed the underlying type to verify the good behavior of the edition */
+enum Values : unsigned int { VALUE_0 = 10, VALUE_1 = 20, VALUE_2 = 30 };
 
 template <typename RP>
 void printParameterValue( const RenderParameters& parameters, const ::std::string& p ) {
     if ( parameters.containsParameter<RP>( p ) )
         std::cout << parameters.getParameter<RP>( p ).m_value << " ("
-                  << typeid( typename RP::value_type ).name() << ")";
+                  << Utils::decypherType<typename RP::value_type>() << ")";
 }
 
 template <typename T, int N>
@@ -28,14 +31,14 @@ void printVectorParameterValue( const RenderParameters& parameters, const ::std:
     using RP = RenderParameters::TParameter<Eigen::Matrix<T, N, 1>>;
     if ( parameters.containsParameter<RP>( p ) )
         std::cout << parameters.getParameter<RP>( p ).m_value.transpose() << " ("
-                  << typeid( typename RP::value_type ).name() << ")";
+                  << Utils::decypherType<typename RP::value_type>() << ")";
 }
 
 void printColorParameterValue( const RenderParameters& parameters, const ::std::string& p ) {
     using RP = RenderParameters::ColorParameter;
     if ( parameters.containsParameter<RP>( p ) )
         std::cout << parameters.getParameter<RP>( p ).m_value.transpose() << " ("
-                  << typeid( typename RP::value_type ).name() << ")";
+                  << Utils::decypherType<typename RP::value_type>() << ")";
 }
 
 template <typename T, int N, int M>
@@ -44,7 +47,7 @@ void printMatrixParameterValue( const RenderParameters& parameters, const ::std:
     if ( parameters.containsParameter<RP>( p ) )
         std::cout << "\n"
                   << parameters.getParameter<RP>( p ).m_value << "\n ("
-                  << typeid( typename RP::value_type ).name() << ")";
+                  << Utils::decypherType<typename RP::value_type>() << ")";
 }
 
 template <typename T>
@@ -54,13 +57,21 @@ void printCollectionParameterValue( const RenderParameters& parameters, const ::
         std::cout << "\n";
         auto v = parameters.getParameter<RP>( p ).m_value;
         std::copy( v.begin(), v.end(), std::ostream_iterator<T>( std::cout, " " ) );
-        std::cout << "\n (" << typeid( typename RP::value_type ).name() << ")";
+        std::cout << "\n (" << Utils::decypherType<typename RP::value_type>() << ")";
     }
 }
 
-int main( int argc, char* argv[] )
+template <typename T>
+void printAllParameters( const RenderParameters& parameters ) {
+    auto& params = parameters.getParameterSet<T>();
+    std::cout << "Parameters for type " << Utils::decypherType<T>() << " : " << params.size()
+              << "\n";
+    for ( const auto& [key, p] : params ) {
+        std::cout << "\t" << key << " = " << p.m_value << "\n";
+    }
+}
 
-{
+int main( int argc, char* argv[] ) {
     //! [Filling json parameter descriptor]
     auto parameterSet_metadata = R"(
     {
@@ -71,7 +82,7 @@ int main( int argc, char* argv[] )
         },
     "enum": {
         "name": "My Enum",
-        "description": "unscoped enum, ranging from 0 to n",
+        "description": "unscoped enum, ranging from 10 to n30",
         "type": "enum",
         "values": [
           "VALUE_0",
@@ -133,15 +144,18 @@ int main( int argc, char* argv[] )
     layout->addWidget( &editor );
     //! [Creating the edition dialog]
 
+    //! [Management of string<->value conversion for enumeration parameters]
+    auto vnc = new RenderParameters::EnumConverter<Values>( { { Values::VALUE_0, "VALUE_0" },
+                                                              { Values::VALUE_1, "VALUE_1" },
+                                                              { Values::VALUE_2, "VALUE_2" } } );
+    auto valuesEnumConverter = std::shared_ptr<RenderParameters::EnumConverter<Values>>( vnc );
+    //! [Management of string<->value conversion for enumeration parameters]
+
+    //! [filling the parameter set to edit ]
     RenderParameters parameters;
-    std::map<std::string, int> enumConverter = { { "VALUE_0", Values::VALUE_0 },
-                                                 { "VALUE_1", Values::VALUE_1 },
-                                                 { "VALUE_2", Values::VALUE_2 } };
-
-    parameters.addEnumConverter( "enum", enumConverter );
-
+    parameters.addEnumConverter( "enum", valuesEnumConverter );
     parameters.addParameter( "bool", false );
-    parameters.addParameter( "enum", Values::VALUE_0 );
+    parameters.addParameter( "enum", Values::VALUE_1 );
     parameters.addParameter( "int", int( 0 ) );
     parameters.addParameter( "int_constrained", int( 0 ) );
     parameters.addParameter( "uint", (unsigned int)( 10 ) );
@@ -157,9 +171,18 @@ int main( int argc, char* argv[] )
         "Matrix3",
         Ra::Core::Matrix3 { { 0_ra, 0_ra, 0_ra }, { 1_ra, 1_ra, 1_ra }, { 2_ra, 2_ra, 2_ra } } );
     parameters.addParameter( "std::vector<int>", std::vector<int> { 0, 1, 2 } );
+    //! [filling the parameter set to edit ]
 
+    //! [Printing several parameters before edition ]
+    std::cout << "\nPrinting all parameters before editiong :\n";
+    printAllParameters<RenderParameters::IntParameter>( parameters );
+    printAllParameters<RenderParameters::BoolParameter>( parameters );
+    printAllParameters<RenderParameters::UIntParameter>( parameters );
+    printAllParameters<RenderParameters::ScalarParameter>( parameters );
+    //! [Printing several parameters before edition ]
+
+    //! [Filling the editor with the parameter set ]
     editor.setupFromParameters( parameters, parameterSet_metadata );
-
     auto printParameter = [&parameters]( const std::string& p ) {
         std::cout << "Parameter " << p << " was modified. New value is ";
         printParameterValue<RenderParameters::IntParameter>( parameters, p );
@@ -178,10 +201,17 @@ int main( int argc, char* argv[] )
         printMatrixParameterValue<Scalar, 4, 4>( parameters, p );
         std::cout << "\n";
     };
-
     QObject::connect( &editor, &ParameterSetEditor::parameterModified, printParameter );
-
     dialog.show();
+    //! [Filling the editor with the parameter set ]
 
-    return a.exec();
+    a.exec();
+
+    //! [Printing several parameters after edition ]
+    std::cout << "\nPrinting all parameters before quit : ";
+    printAllParameters<RenderParameters::IntParameter>( parameters );
+    printAllParameters<RenderParameters::BoolParameter>( parameters );
+    printAllParameters<RenderParameters::UIntParameter>( parameters );
+    printAllParameters<RenderParameters::ScalarParameter>( parameters );
+    //! [Printing several parameters after edition ]
 }
