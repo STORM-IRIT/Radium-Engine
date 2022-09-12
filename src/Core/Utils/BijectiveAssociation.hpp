6dc5ac9655e83774f8e2790b3ccfa0179bc95b13
dlyr <github@dlyr.fr>
2022-09-12 09:55:35 +0200

    [core] BijectiveAssociation doc and cleanup code.
    Use radium header for optional.

--- src/Core/Utils/BijectiveAssociation.hpp
+++ src/Core/Utils/BijectiveAssociation.hpp
@@ -3,6 +3,8 @@
 #include <initializer_list>
 #include <map>
 
+#include <Core/Utils/StdOptional.hpp>
+
 namespace Ra {
 namespace Core {
 namespace Utils {
@@ -25,76 +27,86 @@ class BijectiveAssociation
      * \brief Constructor from { <T1, T2> } pairs
      */
     explicit BijectiveAssociation( std::initializer_list<std::pair<key_type, value_type>> pairs );
+    /**
+     * \brief Creates an empty association.
+     */
     explicit BijectiveAssociation() = default;
 
     /**
-     * \brief Add a pair to the association.
-     * \return false if the pair was not added due to collision with an already inserted pair
+     * \brief Insert a pair to the association.
+     * \return false if the pair was not added due to collision with an already inserted pair.
      */
     bool insert( std::pair<key_type, value_type> p );
     /**
-     * \brief Convenient alias for element insertion (hides pair creation).
-
-Code is equivalent to `insert({v1, v2})`.
+     * \brief Convenient alias for `insert({key, value})`.
      * \see Ra::Core::Utils::BijectiveAssociation::insert(std::pair<key_type, value_type>)
      */
-    bool insert( key_type v1, value_type v2 );
+    bool insert( key_type key, value_type value );
 
     /**
      * \brief Replace a pair in the association.
      *
-     * Remove the association {p.first, old_value} and {old_key, p.second} and insert {p.first,
-     * p.second} Equivalent to \code{.cpp} remove({p.first, old_value}); remove({old_key,
-     * p.second}); insert(p); \endcode But faster
+     * Remove the association `{p.first, old_value}` and `{old_key, p.second}` and insert `{p.first,
+     * p.second}`.
+     * Faster version of
+     * \code{.cpp}
+     * remove({p.first, old_value});
+     * remove({old_key, p.second});
+     * insert(p);
+     * \endcode
      */
     void replace( std::pair<key_type, value_type> p );
+    /**
+     * \brief Convenient alias of `replace({key, value})`.
+     * \see Ra::Core::Utils::BijectiveAssociation::replace(std::pair<key_type, value_type>)
+     */
+    void replace( key_type key, value_type value );
 
     /**
-     * \brief Convenient alias for replace({v1, v2}).
-     * \sa Ra::Core::Utils::BijectiveAssociation::replace(std::pair<key_type, value_type>)
+     * \brief Remove a pair from the association.
+     *
+     * Remove only `{key, value}`. If \b key (resp \b value) is present but associated with a value
+     * (resp. key)
+     * != \b value (resp \b key), the association stay unchanged.
+     * \return true if the pair was in the association, and removed. False otherwise.
      */
-    void replace( key_type v1, value_type v2 );
-
+    bool remove( std::pair<key_type, value_type> p );
     /**
-     * \brief Remove a pair in the association.
+     * \brief Convenient alias for `remove({key, value})`.
+     * \see Ra::Core::Utils::BijectiveAssociation::remove(std::pair<key_type, value_type>)
      */
-    void remove( std::pair<key_type, value_type> p );
-    /**
-     * \brief Convenient alias for remove({v1, v2});
-     * \sa Ra::Core::Utils::BijectiveAssociation::remove(std::pair<key_type, value_type>)
-     */
-    void remove( key_type v1, value_type v2 );
+    bool remove( key_type key, value_type value );
 
     /**
      * \brief Gets the value associated to the key
      */
-    value_type operator()( const key_type& k ) const;
+    value_type operator()( const key_type& key ) const;
 
     /**
-     * \brief Gets the value associated to the key.
-     * \throw std::out_of_range if k is not present in the association as a key.
+     * \brief Gets the value associated to \b key.
+     * \throw std::out_of_range if \b key is not present in the association as a key.
      */
-    value_type value( const key_type& k ) const;
+    value_type value( const key_type& key ) const;
 
     /**
-     * \brief Gets the value associated to a key if it exists.
+     * \brief Gets the value associated to \b key if it exists.
      *
-     * Otherwise the optional doesn't contain a value, and evaluate to false.
+     * Otherwise the optional doesn't contain anything, and evaluate to false.
      */
-    std::optional<value_type> valueIfExists( const key_type& k ) const;
+    optional<value_type> valueIfExists( const key_type& key ) const;
 
     /**
      * \brief Gets the key associated to a value.
-     * \throw std::out_of_range if k is not present in the association as a value.
+     * \throw std::out_of_range if \b value is not present in the association as a value.
      */
-    key_type key( const value_type& k ) const;
+    key_type key( const value_type& value ) const;
 
     /**
-     * \brief Gets the key associated to a value if it exists.
+     * \brief Gets the key associated to \b value if it exists.
      *
-     * Otherwise the optional doesn't contain a value, and evaluate to false.
+     * Otherwise the optional doesn't contain anything, and evaluate to false.
      */
-    std::optional<key_type> keyIfExists( const value_type& k ) const;
+    optional<key_type> keyIfExists( const value_type& value ) const;
 
     /**
      * \brief Gets an const iterator at beginning of the key to value map.
@@ -107,7 +119,7 @@ Code is equivalent to `insert({v1, v2})`.
      * \brief Gets a const iterator at the end of the key to value map.
      */
     typename std::map<key_type, value_type>::const_iterator end() const noexcept;
-    ///\copydoc end()
+    /// \copydoc end()
     typename std::map<key_type, value_type>::const_iterator cend() const noexcept;
 
     /**
