94b4182687ef4667866081547ac09aa4f0987341
dlyr <github@dlyr.fr>
2022-09-14 17:02:29 +0200

    [core] Make singleton implem cleaner.
    Use deleter to allow private ctor dtor, use static inline.

--- src/Core/Utils/Singleton.hpp
+++ src/Core/Utils/Singleton.hpp
@@ -18,32 +18,28 @@
 /// Add this macro (followed by a semicolon) in your class header.
 /// The macro should appear first in the class, before any public:
 /// specifier.
-#define RA_SINGLETON_INTERFACE( TYPE )                                     \
-  public:                                                                  \
-    template <typename... Args>                                            \
-    static TYPE* createInstance( const Args&... args ) {                   \
-        replaceInstance( std::move( std::make_unique<TYPE>( args... ) ) ); \
-        return getInstance();                                              \
-    }                                                                      \
-    static TYPE* getInstance();                                            \
-    static void destroyInstance();                                         \
-                                                                           \
-  protected:                                                               \
-    static void replaceInstance( std::unique_ptr<TYPE>&& ptr );            \
-    TYPE( const TYPE& ) = delete;                                          \
-    void operator=( const TYPE& ) = delete
+#define RA_SINGLETON_INTERFACE( TYPE )                                                 \
+  protected:                                                                           \
+    TYPE( const TYPE& ) = delete;                                                      \
+    void operator=( const TYPE& ) = delete;                                            \
+    struct Deleter {                                                                   \
+        void operator()( TYPE* p ) const { delete p; }                                 \
+    };                                                                                 \
+    static std::unique_ptr<TYPE, Deleter> s_instance;                                  \
+                                                                                       \
+  public:                                                                              \
+    template <typename... Args>                                                        \
+    inline static TYPE* createInstance( const Args&... args ) {                        \
+        s_instance = std::unique_ptr<TYPE, Deleter>( new TYPE( args... ), Deleter() ); \
+        return getInstance();                                                          \
+    }                                                                                  \
+    inline static TYPE* getInstance() { return s_instance.get(); }                     \
+    inline static void destroyInstance() { s_instance.reset( nullptr ); }
 
 /// Add this macro in the singleton cpp, followed by a semicolon.
 // Limitations : TYPE cannot be a nested type
 // RA_SINGLETON_IMPLEMENTATION(A::MySingleton); will *not* work.
-#define RA_SINGLETON_IMPLEMENTATION( TYPE )                                 \
-    namespace TYPE##NS {                                                    \
-        std::unique_ptr<TYPE> s_instance { nullptr };                       \
-    }                                                                       \
-    void TYPE::replaceInstance( std::unique_ptr<TYPE>&& ptr ) {             \
-        TYPE##NS::s_instance = std::move( ptr );                            \
-    }                                                                       \
-    TYPE* TYPE::getInstance() { return TYPE##NS::s_instance.get(); }        \
-    void TYPE::destroyInstance() { TYPE##NS::s_instance.reset( nullptr ); } \
+#define RA_SINGLETON_IMPLEMENTATION( TYPE )                          \
+    std::unique_ptr<TYPE, TYPE::Deleter> TYPE::s_instance = nullptr; \
     class TYPE
 // The line above is just there to make the macro end with a ;
