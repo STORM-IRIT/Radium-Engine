826dc6c55c03a3f14c743dbecb44de34232eaa47
Mathias Paulin <mathias.paulin@irit.fr>
2022-09-29 18:46:45 +0200

    [dataflow-rendering] add rendergraph subcomponent

---
+++ src/Dataflow/Rendering/Renderer/RenderingGraph.cpp
@@ -0,0 +1,122 @@
+#include <Dataflow/Rendering/Renderer/RenderingGraph.hpp>
+
+#include <Engine/Rendering/RenderObject.hpp>
+#include <Engine/Rendering/RenderTechnique.hpp>
+
+using namespace Ra::Engine::Rendering;
+
+namespace Ra {
+namespace Dataflow {
+namespace Rendering {
+namespace Renderer {
+using namespace Ra::Dataflow::Rendering::Nodes;
+using namespace Ra::Dataflow::Core;
+
+void RenderingGraph::init() {
+    DataflowGraph::init();
+}
+
+bool RenderingGraph::addNode( Node* newNode ) {
+    auto added = DataflowGraph::addNode( newNode );
+    if ( added ) {
+        // Todo : is there something to do ?
+    }
+    return added;
+}
+
+bool RenderingGraph::removeNode( Node* node ) {
+    auto removed = DataflowGraph::removeNode( node );
+    if ( removed ) {
+        // Todo : is there something to do ?
+    }
+    return removed;
+}
+
+bool RenderingGraph::postCompilationOperation() {
+#if 0
+    m_renderingNodes.clear();
+    m_rtIndexedNodes.clear();
+    m_dataProviders.clear();
+    if ( m_displaySinkNode && m_displayObserverId != -1 ) {
+        m_displaySinkNode->detach( m_displayObserverId );
+        m_displayObserverId = -1;
+        m_displaySinkNode   = nullptr;
+    }
+    const auto& compiledNodes = getNodesByLevel();
+    int idx                   = 1; // The renderTechnique id = 0 is reserved for ui/debug objects
+    for ( const auto& lvl : *compiledNodes ) {
+        for ( auto n : lvl ) {
+            auto renderNode = dynamic_cast<RenderingNode*>( n );
+            if ( renderNode != nullptr ) {
+                m_renderingNodes.push_back( renderNode );
+                if ( renderNode->hasRenderTechnique() ) {
+                    renderNode->setIndex( idx++ );
+                    m_rtIndexedNodes.push_back( renderNode );
+                }
+                renderNode->setShaderProgramManager( m_shaderMngr );
+            }
+            else {
+                auto sceneNode = dynamic_cast<SceneNode*>( n );
+                if ( sceneNode ) { m_dataProviders.push_back( sceneNode ); }
+                else {
+                    // Manage all sinks ...
+                    auto displaySink = dynamic_cast<DisplaySinkNode*>( n );
+                    if ( displaySink ) {
+                        m_displaySinkNode = displaySink;
+                        // observe the displaySink
+                        m_displayObserverId =
+                            displaySink->attachMember( this, &RenderingGraph::observeSinks );
+                    }
+                }
+            }
+        }
+    }
+    /*
+    std::cout << "RenderingGraph::postCompilationOperation : got " <<
+                 m_renderingNodes.size() << " compiled rendering nodes with " <<
+                 m_rtIndexedNodes.size() << " render-passes, "  <<
+                 m_dataProviders.size() << " scene nodes. \n";
+    */
+#endif
+    return true;
+}
+
+#if 0
+void RenderingGraph::observeSinks( const std::vector<TextureType*>& graphOutput ) {
+    m_outputTextures = graphOutput;
+    /*
+    std::cout << "Available output textures are :" << std::endl;
+    int i = 0;
+    for (auto t : m_outputTextures ) {
+        std::cout << "\t tex " << i++ << " : ";
+        if (t) {
+            std::cout << t->getName() << std::endl;
+        } else {
+            std::cout << "nullptr" << std::endl;
+        }
+    }
+    */
+}
+#endif
+const std::vector<TextureType*>& RenderingGraph::getImagesOutput() const {
+    return m_outputTextures;
+}
+
+void RenderingGraph::clearNodes() {
+    DataflowGraph::clearNodes();
+    m_renderingNodes.clear();
+    m_rtIndexedNodes.clear();
+}
+
+void RenderingGraph::buildRenderTechnique( Ra::Engine::Rendering::RenderObject* ro ) const {
+    auto rt = std::make_shared<RenderTechnique>();
+    for ( const auto& rn : m_rtIndexedNodes ) {
+        rn->buildRenderTechnique( ro, *rt );
+    }
+    rt->updateGL();
+    ro->setRenderTechnique( rt );
+}
+} // namespace Renderer
+} // namespace Rendering
+} // namespace Dataflow
+} // namespace Ra
---
+++ src/Dataflow/Rendering/Renderer/RenderingGraph.hpp
@@ -0,0 +1,82 @@
+#pragma once
+#include <Dataflow/RaDataflow.hpp>
+
+#include <Dataflow/Core/DataflowGraph.hpp>
+
+#include <Dataflow/Rendering/Nodes/RenderingNode.hpp>
+
+#if 0
+#    include <RadiumRenderNodes/Nodes/Scene/SceneNode.hpp>
+#    include <RadiumRenderNodes/Nodes/Sinks/DisplaySinkNode.hpp>
+#endif
+
+namespace Ra {
+namespace Dataflow {
+namespace Rendering {
+namespace Renderer {
+using namespace Ra::Dataflow::Rendering::Nodes;
+using namespace Ra::Dataflow::Core;
+
+class RA_DATAFLOW_API RenderingGraph : public DataflowGraph
+{
+  public:
+    RenderingGraph( const std::string& name );
+    ~RenderingGraph() override = default;
+
+    void init() override;
+    bool addNode( Node* newNode ) override;
+    bool removeNode( Node* node ) override;
+    void clearNodes() override;
+
+    /// Sets the shader program manager
+    void setShaderProgramManager( Ra::Engine::Data::ShaderProgramManager* shaderMngr ) {
+        m_shaderMngr = shaderMngr;
+    }
+
+    /// Resize all the rendering output
+    void resize( uint32_t width, uint32_t height );
+
+    /// Set the scene accessors on the graph
+    void setDataSources( std::vector<RenderObjectType>* ros, std::vector<LightType>* lights );
+    /// Set the viewpoint on the graph
+    void setCameras( std::vector<CameraType>* cameras );
+
+    /// get the computed texture vector
+    const std::vector<TextureType*>& getImagesOutput() const;
+
+    /// Set render techniques needed by the rendering nodes
+    void buildRenderTechnique( Ra::Engine::Rendering::RenderObject* ro ) const;
+    /// Return the typename of the Graph
+    static const std::string getTypename();
+
+  protected:
+    bool postCompilationOperation() override;
+    void fromJsonInternal( const nlohmann::json& ) override;
+    void toJsonInternal( nlohmann::json& ) const override;
+
+  private:
+    /// The renderer's shader program manager
+    Ra::Engine::Data::ShaderProgramManager* m_shaderMngr;
+    /// List of nodes that requires some particular processing
+    std::vector<RenderingNode*> m_renderingNodes; // to resize
+    std::vector<RenderingNode*> m_rtIndexedNodes; // associate an index and buildRenderTechnique
+#if 0
+    /// List of nodes that serve as data provider
+    std::vector<SceneNode*> m_dataProviders;
+
+    // DisplaySink observerMethod : right now, observe only displaySink node
+    void observeSinks( const std::vector<TextureType*>& graphOutput );
+    /// The display sink node used to get rendered images
+    DisplaySinkNode* m_displaySinkNode { nullptr };
+#endif
+    /// ObserverId for displaySink;
+    int m_displayObserverId { -1 };
+    std::vector<TextureType*> m_outputTextures;
+};
+
+} // namespace Renderer
+} // namespace Rendering
+} // namespace Dataflow
+} // namespace Ra
+
+#include <Dataflow/Rendering/Renderer/RenderingGraph.inl>
