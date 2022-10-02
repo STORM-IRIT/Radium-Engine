531efb2f6247f7b22664b885b40ae625bc366192
Mathias Paulin <mathias.paulin@irit.fr>
2022-10-02 18:40:07 +0200

    [core] improve type utilities : add type traits utility and fix type demangling on windows

--- src/Core/Utils/TypesUtils.hpp
+++ src/Core/Utils/TypesUtils.hpp
@@ -25,6 +25,32 @@ const char* demangleType() noexcept;
 template <typename T>
 const char* demangleType( const T& ) noexcept;
 
+// Check if a type is a container with access to its element type and number
+// adapted from https://stackoverflow.com/questions/13830158/check-if-a-variable-type-is-iterable
+namespace detail {
+
+using std::begin;
+using std::end;
+
+template <typename T>
+auto is_container_impl( int )
+    -> decltype( begin( std::declval<T&>() ) !=
+                     end( std::declval<T&>() ), // begin/end and operator !=
+                 void(),                        // Handle evil operator ,
+                 std::declval<T&>().empty(),
+                 std::declval<T&>().size(),
+                 ++std::declval<decltype( begin( std::declval<T&>() ) )&>(), // operator ++
+                 void( *begin( std::declval<T&>() ) ),                       // operator*
+                 std::true_type {} );
+
+template <typename T>
+std::false_type is_container_impl( ... );
+
+} // namespace detail
+
+template <typename T>
+using is_container = decltype( detail::is_container_impl<T>( 0 ) );
+
 // TypeList taken and adapted from
 // https://github.com/AcademySoftwareFoundation/openvdb/blob/master/openvdb/openvdb/TypeList.h
 // Only took small part of TypeList utilities
