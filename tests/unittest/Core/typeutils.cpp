c91d8d49274175b15adde83c1e2d4124536e7fa7
Mathias Paulin <mathias.paulin@irit.fr>
2022-10-02 18:40:07 +0200

    [core] improve type utilities : add type traits utility and fix type demangling on windows

--- tests/unittest/Core/typeutils.cpp
+++ tests/unittest/Core/typeutils.cpp
@@ -1,3 +1,6 @@
+#include <Core/Containers/VectorArray.hpp>
+#include <Core/Types.hpp>
+#include <Core/Utils/Color.hpp>
 #include <Core/Utils/TypesUtils.hpp>
 #include <catch2/catch.hpp>
 
@@ -9,19 +12,15 @@
 namespace TypeTests {
 struct TypeName_struct {};
 } // namespace TypeTests
-TEST_CASE( "Core/Utils/TypesUtils", "[Core][Core/Utils][TypesUtils]" ) {
+TEST_CASE( "Core/Utils/TypesUtils", "[Core][Utils][TypesUtils]" ) {
     SECTION( "Demangle from typename" ) {
         using Ra::Core::Utils::demangleType;
 
         REQUIRE( std::string( demangleType<int>() ) == "int" );
         REQUIRE( std::string( demangleType<float>() ) == "float" );
         REQUIRE( std::string( demangleType<uint>() ) == "unsigned int" );
-        // TODO, verify type demangling on windows
-#ifndef _WIN32
         REQUIRE( std::string( demangleType<size_t>() ) == "unsigned long" );
-#else
-        REQUIRE( std::string( demangleType<size_t>() ) == "unsigned __int64" );
-#endif
+
         auto demangledName = std::string( demangleType<std::vector<int>>() );
         REQUIRE( demangledName == "std::vector<int, std::allocator<int>>" );
 
@@ -40,18 +39,27 @@ TEST_CASE( "Core/Utils/TypesUtils", "[Core][Core/Utils][TypesUtils]" ) {
         REQUIRE( std::string( demangleType( i ) ) == "int" );
         REQUIRE( std::string( demangleType( f ) ) == "float" );
         REQUIRE( std::string( demangleType( u ) ) == "unsigned int" );
-        // TODO, verify type demangling on windows
-#ifndef _WIN32
         REQUIRE( std::string( demangleType( s ) ) == "unsigned long" );
-#else
-        REQUIRE( std::string( demangleType( s ) ) == "unsigned __int64" );
-#endif
+
+#ifndef _WIN32
+        // this segfault on windows due to out_of_bound exception. why ???
         std::vector<int> v;
         auto demangledName = std::string( demangleType( v ) );
         REQUIRE( demangledName == "std::vector<int, std::allocator<int>>" );
-
+#endif
         TypeTests::TypeName_struct tns;
-        demangledName = std::string( demangleType( tns ) );
-        REQUIRE( demangledName == "TypeTests::TypeName_struct" );
+        auto demangledNameFromStruct = std::string( demangleType( tns ) );
+        REQUIRE( demangledNameFromStruct == "TypeTests::TypeName_struct" );
+    }
+
+    SECTION( "Type traits" ) {
+        using namespace Ra::Core::Utils;
+        REQUIRE( is_container<Scalar>::value == false );
+        REQUIRE( is_container<Ra::Core::Vector3>::value == false );
+        REQUIRE( is_container<Ra::Core::Utils::Color>::value == false );
+        REQUIRE( is_container<Ra::Core::VectorArray<Ra::Core::Vector3>>::value == true );
+        REQUIRE( is_container<std::array<unsigned int, 2>>::value == true );
+        REQUIRE( is_container<std::map<size_t, std::string>>::value == true );
+        REQUIRE( is_container<std::vector<Scalar>>::value == true );
     }
 }
