ec94b3022167f3144681bfed7bd382cc7a9cdacd
Mathias Paulin <mathias.paulin@irit.fr>
2022-10-05 23:45:44 +0200

    [dataflow] accelerate compilation
    Co-authored-by: Nessim DAHMANI <ndahmani@irit.fr>

--- src/Dataflow/Core/Nodes/Sources/CoreDataSources.hpp
+++ src/Dataflow/Core/Nodes/Sources/CoreDataSources.hpp
@@ -2,6 +2,7 @@
 #include <Dataflow/Core/Nodes/Sources/SingleDataSourceNode.hpp>
 
 #include <Core/Containers/VectorArray.hpp>
+#include <Core/Types.hpp>
 #include <Core/Utils/Color.hpp>
 
 #include <iostream>
@@ -91,15 +92,33 @@ class RA_DATAFLOW_API ColorSourceNode : public SingleDataSourceNode<Ra::Core::Ut
     static const std::string& getTypename();
 };
 
-using FloatArrayDataSource  = SingleDataSourceNode<Ra::Core::VectorArray<float>>;
-using DoubleArrayDataSource = SingleDataSourceNode<Ra::Core::VectorArray<double>>;
-using IntArrayDataSource    = SingleDataSourceNode<Ra::Core::VectorArray<int>>;
-using UIntArrayDataSource   = SingleDataSourceNode<Ra::Core::VectorArray<unsigned int>>;
-using ColorArrayDataSource  = SingleDataSourceNode<Ra::Core::VectorArray<Ra::Core::Utils::Color>>;
+// This macro does not end with semicolon. To be added when callin it
+#define DECLARE_ARRAYSOURCES( SUFFIX, TYPE ) \
+    using ArrayDataSource##SUFFIX = SingleDataSourceNode<Ra::Core::VectorArray<TYPE>>
+
+using namespace Ra::Core;
+
+DECLARE_ARRAYSOURCES( Float, float );
+DECLARE_ARRAYSOURCES( Double, double );
+DECLARE_ARRAYSOURCES( Int, int );
+DECLARE_ARRAYSOURCES( UInt, unsigned int );
+DECLARE_ARRAYSOURCES( Color, Utils::Color );
+DECLARE_ARRAYSOURCES( Vector2f, Vector2f );
+DECLARE_ARRAYSOURCES( Vector2d, Vector2d );
+DECLARE_ARRAYSOURCES( Vector3f, Vector3f );
+DECLARE_ARRAYSOURCES( Vector3d, Vector3d );
+DECLARE_ARRAYSOURCES( Vector4f, Vector4f );
+DECLARE_ARRAYSOURCES( Vector4d, Vector4d );
+DECLARE_ARRAYSOURCES( Vector2i, Vector2i );
+DECLARE_ARRAYSOURCES( Vector3i, Vector3i );
+DECLARE_ARRAYSOURCES( Vector4i, Vector4i );
+DECLARE_ARRAYSOURCES( Vector2ui, Vector2ui );
+DECLARE_ARRAYSOURCES( Vector3ui, Vector3ui );
+DECLARE_ARRAYSOURCES( Vector4ui, Vector4ui );
+
+#undef DECLARE_ARRAYSOURCES
 
 } // namespace Sources
 } // namespace Core
 } // namespace Dataflow
 } // namespace Ra
-
-#include <Dataflow/Core/Nodes/Sources/CoreDataSources.inl>
