92e05af0b288b7ea6030a204340290ef61803faa
dlyr <github@dlyr.fr>
2022-10-17 15:10:57 +0200

    [engine] rename lambda lock to prevent var name shadowing.

--- src/Engine/Data/Texture.cpp
+++ src/Engine/Data/Texture.cpp
@@ -221,14 +221,13 @@ void Texture::updateData( void* newData ) {
     if ( m_updateDataTaskId.isInvalid() ) {
         auto taskFunc = [this]() {
             this->updateGL();
-            std::lock_guard<std::mutex> lock( m_updateMutex );
+            std::lock_guard<std::mutex> taskLock( m_updateMutex );
             m_updateDataTaskId = Core::TaskQueue::TaskId::Invalid();
         };
         auto task          = std::make_unique<Core::FunctionTask>( taskFunc, getName() );
         m_updateDataTaskId = RadiumEngine::getInstance()->addGpuTask( std::move( task ) );
     }
 }
-
 
 // let the compiler warn about case fallthrough
 void Texture::updateParameters() {
