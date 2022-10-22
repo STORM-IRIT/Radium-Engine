fa75a0048987961e637acd710d0bd5db340c770b
dlyr <github@dlyr.fr>
2022-10-22 15:49:18 +0200

    [cmake] remove deprecated package.cmake

--- external/IO/package.cmake
+++
@@ -1,9 +0,0 @@
-if(NOT DEFINED assimp_DIR)
-    set(assimp_sub_DIR lib/cmake/assimp-5.0/ CACHE INTERNAL "")
-    set(assimp_DIR ${CMAKE_INSTALL_PREFIX}/${assimp_sub_DIR})
-endif()
-
-if(NOT DEFINED tinyply_DIR)
-    set(tinyply_sub_DIR lib/cmake/tinyply/ CACHE INTERNAL "")
-    set(tinyply_DIR ${CMAKE_INSTALL_PREFIX}/${tinyply_sub_DIR})
-endif()
