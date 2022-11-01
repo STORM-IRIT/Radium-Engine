6233c156ad5335d21b47cc42b910d9e19fc9f200
Mathias Paulin <mathias.paulin@irit.fr>
2022-11-01 18:20:08 +0100

    [externals] update externals configuration

--- external/Dataflow/package.cmake
+++
@@ -1,9 +0,0 @@
-if(NOT DEFINED stduuid_DIR)
-    set(stduuid_sub_DIR lib/cmake/stduuid CACHE INTERNAL "")
-    set(stduuid_DIR ${CMAKE_INSTALL_PREFIX}/${stduuid_sub_DIR})
-endif()
-
-if(NOT DEFINED RadiumNodeEditor_DIR)
-    set(RadiumNodeEditor_sub_DIR lib/cmake/RadiumNodeEditor CACHE INTERNAL "")
-    set(RadiumNodeEditor_DIR ${CMAKE_INSTALL_PREFIX}/${RadiumNodeEditor_sub_DIR})
-endif()
