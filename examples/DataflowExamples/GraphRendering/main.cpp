0cf44ac97534fca873e0b0af69fa9cda180d24a0
Mathias Paulin <mathias.paulin@irit.fr>
2022-11-03 19:16:43 +0100

    [examples][dataflow] add graph inspection in Graph rendering demo for debug purpose

--- examples/DataflowExamples/GraphRendering/main.cpp
+++ examples/DataflowExamples/GraphRendering/main.cpp
@@ -108,10 +108,72 @@ class DemoWindowFactory : public BaseApplication::WindowFactory
 };
 /* ----------------------------------------------------------------------------------- */
 // Renderer controller
+#include <Dataflow/Rendering/Nodes/Sinks/DisplaySinkNode.hpp>
 #include <Dataflow/Rendering/Nodes/Sources/Scene.hpp>
 
 class MyRendererController : public RenderGraphController
 {
+  private:
+    static void inspectGraph( DataflowGraph& g ) {
+        // Factories used by the graph
+        auto factories = g.getNodeFactories();
+        std::cout << "Used factories by the graph \"" << g.getInstanceName() << "\" with type \""
+                  << g.getTypeName() << "\" :\n";
+        for ( const auto& f : *( factories.get() ) ) {
+            std::cout << "\t" << f.first << "\n";
+        }
+
+        // Nodes of the graph
+        auto nodes = g.getNodes();
+        std::cout << "Nodes of the graph " << g.getInstanceName() << " (" << nodes->size()
+                  << ") :\n";
+        for ( const auto& n : *( nodes ) ) {
+            std::cout << "\t\"" << n->getInstanceName() << "\" of type \"" << n->getTypeName()
+                      << "\"\n";
+            // Inspect input, output and interfaces of the node
+            std::cout << "\t\tInput ports :\n";
+            for ( const auto& p : n->getInputs() ) {
+                std::cout << "\t\t\t\"" << p->getName() << "\" with type " << p->getTypeName()
+                          << "\n";
+            }
+            std::cout << "\t\tOutput ports :\n";
+            for ( const auto& p : n->getOutputs() ) {
+                std::cout << "\t\t\t\"" << p->getName() << "\" with type " << p->getTypeName()
+                          << "\n";
+            }
+            std::cout << "\t\tInterface ports :\n";
+            for ( const auto& p : n->getInterfaces() ) {
+                std::cout << "\t\t\t\"" << p->getName() << "\" with type " << p->getTypeName()
+                          << "\n";
+            }
+        }
+
+        // Nodes by level after the compilation
+        auto c  = g.compile();
+        auto cn = g.getNodesByLevel();
+        std::cout << "Nodes of the graph, sorted by level when compiling the graph :\n";
+        for ( size_t i = 0; i < cn->size(); ++i ) {
+            std::cout << "\tLevel " << i << " :\n";
+            for ( const auto n : ( *cn )[i] ) {
+                std::cout << "\t\t\"" << n->getInstanceName() << "\"\n";
+            }
+        }
+
+        // describe the graph interface : inputs and outputs port of the whole graph (not of the
+        // nodes)
+        std::cout << "Inputs and output nodes of the graph " << g.getInstanceName() << " :\n";
+        auto inputs = g.getAllDataSetters();
+        std::cout << "\tInput ports (" << inputs.size() << ") are :\n";
+        for ( auto& [ptrPort, portName, portType] : inputs ) {
+            std::cout << "\t\t\"" << portName << "\" accepting type \"" << portType << "\"\n";
+        }
+        auto outputs = g.getAllDataGetters();
+        std::cout << "\tOutput ports (" << outputs.size() << ") are :\n";
+        for ( auto& [ptrPort, portName, portType] : outputs ) {
+            std::cout << "\t\t\"" << portName << "\" generating type \"" << portType << "\"\n";
+        }
+    }
+
   public:
     using RenderGraphController::RenderGraphController;
 
@@ -128,9 +190,14 @@ class MyRendererController : public RenderGraphController
             m_renderGraph->setShaderProgramManager( m_shaderMngr );
             auto sceneNode = new SceneNode( "Scene" );
             m_renderGraph->addNode( sceneNode );
+            auto resultNode = new DisplaySinkNode( "Images" );
+            m_renderGraph->addNode( resultNode );
+
+            inspectGraph( *m_renderGraph );
         }
     };
 
+#if 0
     /// Resize function
     /// Called each time the renderer is resized
     void resize( int w, int h ) override {
@@ -144,7 +211,7 @@ class MyRendererController : public RenderGraphController
         LOG( logINFO ) << "MyRendererController::update";
         RenderGraphController::update( renderData );
     };
-
+#endif
     [[nodiscard]] std::string getRendererName() const override { return "Custom Node Renderer"; }
 };
 
