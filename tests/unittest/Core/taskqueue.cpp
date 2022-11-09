03741e4d5885109fb13f11befb0a02f7a154e79c
dlyr <github@dlyr.fr>
2022-11-09 09:59:53 +0100

    [tests] Better TaskQueue coverage.

--- tests/unittest/Core/taskqueue.cpp
+++ tests/unittest/Core/taskqueue.cpp
@@ -33,20 +33,27 @@ TEST_CASE( "Core/TaskQueue", "[Core][TaskQueue]" ) {
                       "\"task 5\"\n"
                       "\"task 6\"\n"
                       "}\n" );
-        taskQueue.startTasks();
-        taskQueue.waitForTasks();
-        taskQueue.flushTaskQueue();
-
+        SECTION( "parallel run" ) {
+            taskQueue.startTasks();
+            taskQueue.waitForTasks();
+            taskQueue.flushTaskQueue();
+        }
+        SECTION( "one thread run" ) { taskQueue.runTasksInThisThread(); }
         for ( int tidx = 0; tidx < arraySize; ++tidx ) {
             REQUIRE( array[tidx] == tidx );
         }
     }
     SECTION( "dependencies" ) {
+        TaskQueue::TaskId tids[arraySize];
         for ( int tidx = 0; tidx < 4; ++tidx ) {
             auto task =
                 std::make_unique<FunctionTask>( [&array, tidx]() { array[tidx] = tidx; },
                                                 std::string( "task " ) + std::to_string( tidx ) );
-            taskQueue.registerTask( std::move( task ) );
+            tids[tidx] = taskQueue.registerTask( std::move( task ) );
+            if ( tidx < 2 ) {
+                taskQueue.addPendingDependency( tids[tidx],
+                                                std::string( "task " ) + std::to_string( 4 ) );
+            }
         }
         for ( int tidx = 4; tidx < 6; ++tidx ) {
             int pred1 = 2 * ( tidx - 4 );
@@ -54,40 +61,28 @@ TEST_CASE( "Core/TaskQueue", "[Core][TaskQueue]" ) {
             auto task = std::make_unique<FunctionTask>(
                 [&array, tidx, pred1, pred2]() { array[tidx] = array[pred1] + array[pred2]; },
                 std::string( "task " ) + std::to_string( tidx ) );
-            auto tid = taskQueue.registerTask( std::move( task ) );
-            taskQueue.addDependency( std::string( "task " ) + std::to_string( pred1 ), tid );
-            taskQueue.addDependency( std::string( "task " ) + std::to_string( pred2 ), tid );
+            auto tid   = taskQueue.registerTask( std::move( task ) );
+            tids[tidx] = tid;
+            if ( tidx == 5 ) {
+                taskQueue.addDependency( std::string( "task " ) + std::to_string( pred1 ), tid );
+                taskQueue.addDependency( std::string( "task " ) + std::to_string( pred2 ), tid );
+            }
         }
         {
             auto task =
                 std::make_unique<FunctionTask>( [&array]() { array[6] = array[4] + array[5]; },
                                                 std::string( "task " ) + std::to_string( 6 ) );
-            auto tid = taskQueue.registerTask( std::move( task ) );
-            taskQueue.addDependency( std::string( "task " ) + std::to_string( 4 ), tid );
-            taskQueue.addDependency( std::string( "task " ) + std::to_string( 5 ), tid );
+            taskQueue.registerTask( std::move( task ) );
+            taskQueue.addDependency( tids[4], std::string( "task " ) + std::to_string( 6 ) );
+            taskQueue.addDependency( tids[5], std::string( "task " ) + std::to_string( 6 ) );
         }
-        std::ostringstream oss;
-        taskQueue.printTaskGraph( oss );
-        std::string s = oss.str();
-        REQUIRE( s == "digraph tasks {\n"
-                      "\"task 0\"\n"
-                      "\"task 1\"\n"
-                      "\"task 2\"\n"
-                      "\"task 3\"\n"
-                      "\"task 4\"\n"
-                      "\"task 5\"\n"
-                      "\"task 6\"\n"
-                      "\"task 0\" -> \"task 4\"\n"
-                      "\"task 1\" -> \"task 4\"\n"
-                      "\"task 2\" -> \"task 5\"\n"
-                      "\"task 3\" -> \"task 5\"\n"
-                      "\"task 4\" -> \"task 6\"\n"
-                      "\"task 5\" -> \"task 6\"\n"
-                      "}\n" );
-        taskQueue.startTasks();
-        taskQueue.waitForTasks();
-        taskQueue.flushTaskQueue();
 
+        SECTION( "parallel run" ) {
+            taskQueue.startTasks();
+            taskQueue.waitForTasks();
+            taskQueue.flushTaskQueue();
+        }
+        SECTION( "one thread run" ) { taskQueue.runTasksInThisThread(); }
         REQUIRE( array[0] == 0 );
         REQUIRE( array[1] == 1 );
         REQUIRE( array[2] == 2 );
