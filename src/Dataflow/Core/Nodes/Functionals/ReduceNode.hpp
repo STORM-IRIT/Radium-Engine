4064edfbac5f3efa3e41d77672ec4d4f23308c7a
Mathias Paulin <mathias.paulin@irit.fr>
2023-01-31 15:04:42 +0100

    [dataflow-core] remove .inl files

--- src/Dataflow/Core/Nodes/Functionals/ReduceNode.hpp
+++ src/Dataflow/Core/Nodes/Functionals/ReduceNode.hpp
@@ -84,9 +84,85 @@ class ReduceNode : public Node
     static const std::string& getTypename();
 };
 
+// -----------------------------------------------------------------
+// ---------------------- inline methods ---------------------------
+
+template <typename coll_t, typename v_t>
+ReduceNode<coll_t, v_t>::ReduceNode( const std::string& instanceName ) :
+    ReduceNode(
+        instanceName,
+        getTypename(),
+        []( const v_t& a, const v_t& ) -> v_t { return a; },
+        v_t {} ) {}
+
+template <typename coll_t, typename v_t>
+ReduceNode<coll_t, v_t>::ReduceNode( const std::string& instanceName,
+                                     ReduceOperator op,
+                                     v_t initialValue ) :
+    ReduceNode( instanceName, getTypename(), op, initialValue ) {}
+
+template <typename coll_t, typename v_t>
+void ReduceNode<coll_t, v_t>::setOperator( ReduceOperator op, v_t initialValue ) {
+    m_operator = op;
+    m_init     = initialValue;
+}
+
+template <typename coll_t, typename v_t>
+void ReduceNode<coll_t, v_t>::init() {
+    Node::init();
+    m_result = m_init;
+}
+
+template <typename coll_t, typename v_t>
+bool ReduceNode<coll_t, v_t>::execute() {
+    auto f   = m_portF->isLinked() ? m_portF->getData() : m_operator;
+    auto iv  = m_portInit->isLinked() ? m_portInit->getData() : m_init;
+    m_result = iv;
+    // The following test will always be true if the node was integrated in a compiled graph
+    if ( m_portIn->isLinked() ) {
+        const auto& inData = m_portIn->getData();
+        m_result           = std::accumulate( inData.begin(), inData.end(), iv, f );
+    }
+    return true;
+}
+
+template <typename coll_t, typename v_t>
+const std::string& ReduceNode<coll_t, v_t>::getTypename() {
+    static std::string demangledName =
+        std::string { "Reduce<" } + Ra::Dataflow::Core::simplifiedDemangledType<coll_t>() + ">";
+    return demangledName;
+}
+
+template <typename coll_t, typename v_t>
+ReduceNode<coll_t, v_t>::ReduceNode( const std::string& instanceName,
+                                     const std::string& typeName,
+                                     ReduceOperator op,
+                                     v_t initialValue ) :
+    Node( instanceName, typeName ), m_operator( op ), m_init( initialValue ) {
+
+    addInput( m_portIn );
+    m_portIn->mustBeLinked();
+    addInput( m_portF );
+    addInput( m_portInit );
+    addOutput( m_portOut, &m_result );
+}
+
+template <typename coll_t, typename v_t>
+void ReduceNode<coll_t, v_t>::toJsonInternal( nlohmann::json& data ) const {
+    data["comment"] =
+        std::string { "Reduce operator could not be serialized for " } + getTypeName();
+    LOG( Ra::Core::Utils::logWARNING ) // TODO make this logDEBUG
+        << "Unable to save data when serializing a " << getTypeName() << ".";
+}
+
+template <typename coll_t, typename v_t>
+bool ReduceNode<coll_t, v_t>::fromJsonInternal( const nlohmann::json& ) {
+    LOG( Ra::Core::Utils::logWARNING ) // TODO make this logDEBUG
+        << "Unable to read data when un-serializing a " << getTypeName() << ".";
+    return true;
+}
+
 } // namespace Functionals
 } // namespace Core
 } // namespace Dataflow
 } // namespace Ra
-
-#include <Dataflow/Core/Nodes/Functionals/ReduceNode.inl>
--- src/Dataflow/Core/TypeDemangler.hpp
+++ src/Dataflow/Core/TypeDemangler.hpp
@@ -1,6 +1,8 @@
 #pragma once
 #include <Dataflow/RaDataflow.hpp>
 
+#include <Core/Utils/TypesUtils.hpp>
+
 namespace Ra {
 namespace Dataflow {
 namespace Core {
@@ -13,8 +15,28 @@ const char* simplifiedDemangledType() noexcept;
 template <typename T>
 const char* simplifiedDemangledType( const T& ) noexcept;
 
+// -----------------------------------------------------------------
+// ---------------------- inline methods ---------------------------
+
+namespace TypeInternal {
+RA_DATAFLOW_API std::string makeTypeReadable( std::string );
+}
+
+template <typename T>
+const char* simplifiedDemangledType() noexcept {
+    static auto demangled_name = []() {
+        std::string demangledType =
+            TypeInternal::makeTypeReadable( Ra::Core::Utils::demangleType<T>() );
+        return demangledType;
+    }();
+    return demangled_name.data();
+}
+
+template <typename T>
+const char* simplifiedDemangledType( const T& ) noexcept {
+    return simplifiedDemangledType<T>();
+}
+
 } // namespace Core
 } // namespace Dataflow
 } // namespace Ra
-
-#include <Dataflow/Core/TypeDemangler.inl>
