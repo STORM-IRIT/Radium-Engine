8cfb0309cb106d0eb478f765f6e40e7ca3c0868f
Mathias Paulin <mathias.paulin@irit.fr>
2023-04-04 12:54:40 +0200

    [core][tests] fix Random sequences when Scalar = double

--- src/Core/Random/RandomPointSet.cpp
+++ src/Core/Random/RandomPointSet.cpp
@@ -19,7 +19,7 @@ Scalar VanDerCorputSequence::operator()( unsigned int bits ) {
     bits = ( ( bits & 0x33333333u ) << 2u ) | ( ( bits & 0xCCCCCCCCu ) >> 2u );
     bits = ( ( bits & 0x0F0F0F0Fu ) << 4u ) | ( ( bits & 0xF0F0F0F0u ) >> 4u );
     bits = ( ( bits & 0x00FF00FFu ) << 8u ) | ( ( bits & 0xFF00FF00u ) >> 8u );
-    return Scalar( float( bits ) * 2.3283064365386963e-10 ); // / 0x100000000
+    return Scalar( float( bits ) * 2.3283064365386963e-10_ra ); // / 0x100000000
 }
 
 FibonacciPointSet::FibonacciPointSet( size_t n ) : seq( n ) {}
@@ -42,7 +42,7 @@ Ra::Core::Vector2 HammersleyPointSet::operator()( size_t i ) {
 }
 
 MersenneTwisterPointSet::MersenneTwisterPointSet( size_t number ) :
-    gen( 0 ), seq( 0., 1. ), n( number ) {}
+    gen( 0 ), seq( 0._ra, 1._ra ), n( number ) {}
 
 size_t MersenneTwisterPointSet::range() {
     return n;
