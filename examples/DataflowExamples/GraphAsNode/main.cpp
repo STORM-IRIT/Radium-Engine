7b7225a4f1bcf8450c0f8f2524de2b13bc91d84e
dlyr <github@dlyr.fr>
2023-09-28 20:33:26 +0200

    [examples] Update GraphAsNode.

--- examples/DataflowExamples/GraphAsNode/main.cpp
+++ examples/DataflowExamples/GraphAsNode/main.cpp
@@ -1,6 +1,8 @@
-#include "Core/Containers/MakeShared.hpp"
+#include <Core/Containers/MakeShared.hpp>
 #include <Dataflow/Core/DataflowGraph.hpp>
+#include <Dataflow/Core/Nodes/Functionals/BinaryOpNode.hpp>
 #include <Dataflow/Core/Nodes/Functionals/FilterNode.hpp>
+#include <Dataflow/Core/Nodes/Functionals/FunctionNode.hpp>
 #include <Dataflow/Core/Nodes/Sinks/SinkNode.hpp>
 #include <Dataflow/Core/Nodes/Sources/CoreDataSources.hpp>
 
@@ -15,23 +17,66 @@ int main( int argc, char* argv[] ) {
 
     auto gAsNode = Ra::Core::make_shared<DataflowGraph>( "graphAsNode" );
 
-    auto predicateNode = gAsNode->addNode<Sources::ScalarUnaryPredicateSource>( "Selector" );
-    auto filterNode    = gAsNode->addNode<Functionals::FilterNode<RaVector>>( "Filter" );
+    // compute delta = b2 - 4ac;
 
-    Sources::ScalarUnaryPredicateSource::function_type pred = []( Scalar x ) { return x < 0.5; };
-    predicateNode->setData( pred );
+    auto b2 = gAsNode->addNode<Functionals::FunctionNode<Scalar>>( "b2" );
+    b2->setFunction( []( const Scalar& b ) { return b * b; } );
 
-    gAsNode->addLink( predicateNode->getFunctionPort(), filterNode->getPredicatePort() );
+    auto fourAC = gAsNode->addNode<Functionals::BinaryOpNode<Scalar>>(
+        "4ac", []( const Scalar& a, const Scalar& c ) { return 4_ra * a * c; } );
 
-    auto inputIdx  = gAsNode->addInput( filterNode->getInPort() );
-    auto outputIdx = gAsNode->addOutput( filterNode->getOutPort() );
+    auto b2minus4ac = gAsNode->addNode<Functionals::BinaryOpNode<Scalar>>(
+        "b2-4ac", []( const Scalar& x, const Scalar& y ) { return x - y; } );
+
+    auto forwardA = gAsNode->addNode<Functionals::FunctionNode<Scalar>>( "a" );
+    auto forwardB = gAsNode->addNode<Functionals::FunctionNode<Scalar>>( "b" );
+    auto forwardC = gAsNode->addNode<Functionals::FunctionNode<Scalar>>( "c" );
+
+    forwardA->getInPort()->setName( "a" );
+    forwardB->getInPort()->setName( "b" );
+    forwardC->getInPort()->setName( "c" );
+    b2minus4ac->getOutputPort()->setName( "delta" );
+
+    auto inputA = gAsNode->addInput( forwardA->getInPort() );
+    auto inputB = gAsNode->addInput( forwardB->getInPort() );
+    auto inputC = gAsNode->addInput( forwardC->getInPort() );
+    auto output = gAsNode->addOutput( b2minus4ac->getOutputPort() );
+
+    gAsNode->addLink( forwardB->getOutPort(), b2->getInPort() );
+    gAsNode->addLink( forwardA->getOutPort(), fourAC->getPortA() );
+    gAsNode->addLink( forwardC->getOutPort(), fourAC->getPortB() );
+
+    gAsNode->addLink( b2->getOutPort(), b2minus4ac->getPortA() );
+    gAsNode->addLink( fourAC->getOutputPort(), b2minus4ac->getPortB() );
+
+    std::cout << "== Graph as node ==\n";
+    std::cout << "inputs\n";
+    for ( const auto& n : gAsNode->getInputs() ) {
+        std::cout << "\t- " << n->getName() << " <" << n->getTypeName() << ">\n";
+    }
+
+    std::cout << "outputs\n";
+    for ( const auto& n : gAsNode->getOutputs() ) {
+        std::cout << "\t- " << n->getName() << " <" << n->getTypeName() << ">\n";
+    }
 
     DataflowGraph g { "mainGraph" };
-    auto sourceNode = g.addNode<Sources::SingleDataSourceNode<RaVector>>( "Source" );
-    auto sinkNode   = g.addNode<Sinks::SinkNode<RaVector>>( "Sink" );
-    auto graphNode  = g.addNode( gAsNode );
-    g.addLink( sourceNode->getOuputPort().get(), gAsNode->getInputByIndex<RaVector>( inputIdx ) );
-    g.addLink( gAsNode->getOutputByIndex<RaVector>( outputIdx ), sinkNode->getInPort().get() );
+    auto sourceNodeA = g.addNode<Sources::SingleDataSourceNode<Scalar>>( "a" );
+    auto sourceNodeB = g.addNode<Sources::SingleDataSourceNode<Scalar>>( "b" );
+    auto sourceNodeC = g.addNode<Sources::SingleDataSourceNode<Scalar>>( "c" );
+
+    auto resultNode = g.addNode<Sinks::SinkNode<Scalar>>( "delta" );
+
+    g.addNode( gAsNode );
+
+    g.addLink( sourceNodeA->getOuputPort().get(), gAsNode->getInputByIndex<Scalar>( inputA ) );
+    g.addLink( sourceNodeB->getOuputPort().get(), gAsNode->getInputByIndex<Scalar>( inputB ) );
+    g.addLink( sourceNodeC->getOuputPort().get(), gAsNode->getInputByIndex<Scalar>( inputC ) );
+    g.addLink( gAsNode->getOutputByIndex<Scalar>( output ), resultNode->getInPort().get() );
+
+    sourceNodeA->setData( 1 );
+    sourceNodeB->setData( 2 );
+    sourceNodeC->setData( 3 );
 
     if ( !g.compile() ) {
         std::cout << " compilation failed";
@@ -41,21 +86,12 @@ int main( int argc, char* argv[] ) {
     //! [Verifying the graph can be compiled]
     g.saveToJson( "illustrateDoc.json" );
 
-    ///! [Configure the interface ports (input and output of the graph)]
-    RaVector test( 10 );
-    std::mt19937 gen( 0 );
-    std::uniform_real_distribution<> dis( 0.0, 1.0 );
-    // Fill the vector with random numbers between 0 and 1
-    std::generate( test.begin(), test.end(), [&dis, &gen]() { return dis( gen ); } );
-
-    sourceNode->setData( test );
-
-    //! [Execute the graph]
     g.execute();
-    auto& result = sinkNode->getDataByRef();
-    std::cout << "Output values (reference): " << result.size() << "\n\t";
-    std::copy( result.begin(), result.end(), std::ostream_iterator<Scalar>( std::cout, " " ) );
-    std::cout << '\n';
+
+    auto& result = resultNode->getDataByRef();
+    std::cout << "Output value delta = " << *sourceNodeB->getData() << "²-4×"
+              << *sourceNodeA->getData() << "×" << *sourceNodeC->getData() << " = " << result
+              << "\n";
 
     return 0;
 }
