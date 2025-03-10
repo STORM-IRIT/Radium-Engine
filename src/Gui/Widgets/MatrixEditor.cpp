59e4c437b3ee9638e7772be42b78b7bec80ce267
dlyr <github@dlyr.fr>
2025-03-10 18:55:25 +0100

    [gui] Fix matrix editor set value before min.
    which prevent negative value init.

--- src/Gui/Widgets/MatrixEditor.cpp
+++ src/Gui/Widgets/MatrixEditor.cpp
@@ -16,16 +16,14 @@ MatrixEditor::MatrixEditor( const Ra::Core::MatrixN& matrix, int dec, QWidget* p
     QGridLayout* layout = new QGridLayout;
     layout->setContentsMargins( {} ); // margins are set to 0, see Qt documentation
     layout->setSpacing( 0 );
-
     for ( long i = 0; i < m_matrix.rows(); ++i ) {
         for ( int j = 0; j < m_matrix.cols(); ++j ) {
             auto& elem   = m_matrix( i, j );
             auto spinbox = new QDoubleSpinBox();
             spinbox->setDecimals( dec );
-            spinbox->setValue( elem );
             spinbox->setMinimum( std::numeric_limits<Scalar>::lowest() );
             spinbox->setMaximum( std::numeric_limits<Scalar>::max() );
-
+            spinbox->setValue( elem );
             layout->addWidget( spinbox, i, j );
             auto updateMatrixOnChange = [this, &elem]( double value ) {
                 elem = value;
