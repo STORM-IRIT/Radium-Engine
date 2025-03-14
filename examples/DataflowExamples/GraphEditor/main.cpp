56dfa1124c0203e30c9ea6f8ba812efbb2c893fe
dlyr <github@dlyr.fr>
2025-03-14 08:37:45 +0100

    [examples] dataflow set style in main (will be removed from widget).

--- examples/DataflowExamples/GraphEditor/main.cpp
+++ examples/DataflowExamples/GraphEditor/main.cpp
@@ -4,8 +4,26 @@
 
 #include <Dataflow/QtGui/GraphEditor/GraphEditorWindow.hpp>
 
+#include <QtNodes/ConnectionStyle>
+
+static void setStyle() {
+
+    QtNodes::ConnectionStyle::setConnectionStyle(
+        R"(
+  {
+    "ConnectionStyle": {
+
+      "UseDataDefinedColors": true
+    }
+  }
+  )" );
+}
+
 int main( int argc, char* argv[] ) {
     QApplication app( argc, argv );
+
+    setStyle();
+
     QCoreApplication::setOrganizationName( "STORM-IRIT" );
     QCoreApplication::setApplicationName( "Radium NodeGraph Editor" );
     QCoreApplication::setApplicationVersion( QT_VERSION_STR );
