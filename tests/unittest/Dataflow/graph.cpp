eb383acb9cd787843e91771f4cb633d71430ddac
dlyr <github@dlyr.fr>
2025-03-25 09:45:44 +0100

    [tests] dataflow, refactor (clean) graph tests

--- tests/unittest/Dataflow/graph.cpp
+++ tests/unittest/Dataflow/graph.cpp
@@ -20,11 +20,17 @@ void inspectGraph( const DataflowGraph& g ) {
         // Inspect input, output and interfaces of the node
         std::cout << "\t\tInput ports :\n";
         for ( const auto& p : n->getInputs() ) {
-            std::cout << "\t\t\t\"" << p->getName() << "\" with type " << p->getTypeName() << "\n";
+            std::cout << "\t\t\t\"" << p->getName() << "\" with type " << p->getTypeName();
+            if ( p->isLinked() ) {
+                std::cout << " linked to " << p->getLink()->getNode()->display_name() << " "
+                          << p->getLink()->getName();
+            }
+            std::cout << "\n";
         }
         std::cout << "\t\tOutput ports :\n";
         for ( const auto& p : n->getOutputs() ) {
-            std::cout << "\t\t\t\"" << p->getName() << "\" with type " << p->getTypeName() << "\n";
+            std::cout << "\t\t\t\"" << p->getName() << "\" with type " << p->getTypeName();
+            std::cout << "\n";
         }
     }
 
@@ -60,36 +66,36 @@ void inspectGraph( const DataflowGraph& g ) {
 }
 
 TEST_CASE( "Dataflow/Core/Graph", "[Dataflow][Core][Graph]" ) {
-    SECTION( "Creation of a graph" ) {
-        DataflowGraph g( "Test Graph" );
-        // Test not a json error detection
+    DataflowGraph g( "Test Graph" );
+    SECTION( "not a json" ) {
         auto result = g.loadFromJson( "data/Dataflow/NotAJsonFile.json" );
         REQUIRE( !result );
-        // Test loading empty graph
+    }
+    SECTION( "loading empty graph" ) {
         nlohmann::json emptyJson = {};
-        result                   = g.fromJson( emptyJson );
+        auto result              = g.fromJson( emptyJson );
         REQUIRE( result );
-
-        // missing identification of the graph (either id or instance)
+    }
+    SECTION( "missing instance" ) {
         nlohmann::json noId = { { "model", { "name", "Core DataflowGraph" } } };
-        result              = g.fromJson( noId );
+        auto result         = g.fromJson( noId );
         REQUIRE( !result );
         g.destroy();
-
-        // missing model of the graph
+    }
+    SECTION( "missing model" ) {
         nlohmann::json noModel = { { "instance", "No model in this node" } };
-        result                 = g.fromJson( noModel );
+        auto result            = g.fromJson( noModel );
         REQUIRE( !result );
         g.destroy();
-
-        // missing instance data  --> loads an empty graph
+    }
+    SECTION( "missing instance data -> loads an empty graph" ) {
         nlohmann::json noGraph = { { "instance", "Missing instance data for model" },
                                    { "model", { "name", "Core DataflowGraph" } } };
-        result                 = g.fromJson( noGraph );
+        auto result            = g.fromJson( noGraph );
         REQUIRE( result );
         g.destroy();
-
-        // trying to instance an unknown node type
+    }
+    SECTION( "trying to instance an unknown node type" ) {
         nlohmann::json NotANode = {
             { "instance", "graph with unknown node" },
             { "model",
@@ -98,11 +104,11 @@ TEST_CASE( "Dataflow/Core/Graph", "[Dataflow][Core][Graph]" ) {
                   { { "nodes",
                       { { { "instance", "NotANode" },
                           { "model", { { "name", "NotANode" } } } } } } } } } } };
-        result = g.fromJson( NotANode );
+        auto result = g.fromJson( NotANode );
         REQUIRE( !result );
         g.destroy();
-
-        // trying to instance an unknown node type
+    }
+    SECTION( "trying to instance an unknown node type" ) {
         nlohmann::json NoModelName = {
             { "instance", "graph with missing node model information" },
             { "model",
@@ -111,22 +117,22 @@ TEST_CASE( "Dataflow/Core/Graph", "[Dataflow][Core][Graph]" ) {
                   { { "nodes",
                       { { { "instance", "Unknown model" },
                           { "model", { { "extra", "NotaTypeName" } } } } } } } } } } };
-        result = g.fromJson( NoModelName );
+        auto result = g.fromJson( NoModelName );
         REQUIRE( !result );
         g.destroy();
-
-        // trying to instance an unknown node type
+    }
+    SECTION( "trying to instance an unknown node type" ) {
         nlohmann::json noInstanceIdentification = {
             { "instance", "graph with missing node model information" },
             { "model",
               { { "name", "Core DataflowGraph" },
                 { "graph",
                   { { "nodes", { { { "model", { { "name", "Source<float>" } } } } } } } } } } };
-        result = g.fromJson( noInstanceIdentification );
+        auto result = g.fromJson( noInstanceIdentification );
         REQUIRE( !result );
         g.destroy();
-
-        // errors in the connection description
+    }
+    SECTION( "errors in the connection description" ) {
         nlohmann::json reusingNodeIdentification = {
             { "instance", "graph with wrong connection" },
             { "model",
@@ -136,10 +142,11 @@ TEST_CASE( "Dataflow/Core/Graph", "[Dataflow][Core][Graph]" ) {
                       { { { "instance", "Source" }, { "model", { { "name", "Source<float>" } } } },
                         { { "instance", "Source" }, { "model", { { "name", "Sink<int>" } } } } } },
                     { "connections", { { { "out_node", "wrongId" } } } } } } } } };
-        result = g.fromJson( reusingNodeIdentification );
+        auto result = g.fromJson( reusingNodeIdentification );
         REQUIRE( !result );
-        g.destroy();
-        reusingNodeIdentification = {
+    }
+    SECTION( "wrong connection 0" ) {
+        nlohmann::json reusingNodeIdentification = {
             { "instance", "graph with wrong connection" },
             { "model",
               { { "name", "Core DataflowGraph" },
@@ -148,11 +155,10 @@ TEST_CASE( "Dataflow/Core/Graph", "[Dataflow][Core][Graph]" ) {
                       { { { "model", { { "name", "Source<float>" } } } },
                         { { "model", { { "name", "Sink<int>" } } } } } },
                     { "connections", { { { "out_node", "wrongId" } } } } } } } } };
-        result = g.fromJson( reusingNodeIdentification );
+        auto result = g.fromJson( reusingNodeIdentification );
         REQUIRE( !result );
-        g.destroy();
-
-        // errors in the connection description
+    }
+    SECTION( "wrong connection 1" ) {
         nlohmann::json wrongConnection = {
             { "instance", "graph with wrong connection" },
             { "model",
@@ -163,11 +169,12 @@ TEST_CASE( "Dataflow/Core/Graph", "[Dataflow][Core][Graph]" ) {
                           { "model", { { "name", "Source<float>" } } } },
                         { { "instance", "SinkInt" }, { "model", { { "name", "Sink<int>" } } } } } },
                     { "connections", { { { "out_node", "wrongId" } } } } } } } } };
-        result = g.fromJson( wrongConnection );
+        auto result = g.fromJson( wrongConnection );
         REQUIRE( !result );
         g.destroy();
-
-        wrongConnection = {
+    }
+    SECTION( "wrong connection 2" ) {
+        nlohmann::json wrongConnection = {
             { "instance", "Test Graph Inline" },
             { "model",
               { { "name", "Core DataflowGraph" },
@@ -178,11 +185,11 @@ TEST_CASE( "Dataflow/Core/Graph", "[Dataflow][Core][Graph]" ) {
                         { { "instance", "SinkInt" }, { "model", { { "name", "Sink<int>" } } } } } },
                     { "connections",
                       { { { "out_node", "SourceFloat" }, { "out_index", 2 } } } } } } } } };
-        result = g.fromJson( wrongConnection );
+        auto result = g.fromJson( wrongConnection );
         REQUIRE( !result );
-        g.destroy();
-
-        wrongConnection = {
+    }
+    SECTION( "wrong connection 3" ) {
+        nlohmann::json wrongConnection = {
             { "instance", "Test Graph Inline" },
             { "model",
               { { "name", "Core DataflowGraph" },
@@ -196,11 +203,11 @@ TEST_CASE( "Dataflow/Core/Graph", "[Dataflow][Core][Graph]" ) {
                           { "out_index", 0 },
                           { "in_node", "Sink" },
                           { "in_port", "from" } } } } } } } } };
-        result = g.fromJson( wrongConnection );
+        auto result = g.fromJson( wrongConnection );
         REQUIRE( !result );
-        g.destroy();
-
-        wrongConnection = {
+    }
+    SECTION( "wrong connection 4" ) {
+        nlohmann::json wrongConnection = {
             { "instance", "Test Graph Inline" },
             { "model",
               { { "name", "Core DataflowGraph" },
@@ -214,11 +221,10 @@ TEST_CASE( "Dataflow/Core/Graph", "[Dataflow][Core][Graph]" ) {
                           { "out_index", 0 },
                           { "in_node", "SinkInt" },
                           { "in_port", "from" } } } } } } } } };
-        result = g.fromJson( wrongConnection );
+        auto result = g.fromJson( wrongConnection );
         REQUIRE( !result );
-        g.destroy();
-
-        // Constructed a correct graph
+    }
+    SECTION( "correct graph" ) {
         nlohmann::json goodSimpleGraph = {
             { "instance", "Test Graph Inline" },
             { "model",
@@ -234,8 +240,8 @@ TEST_CASE( "Dataflow/Core/Graph", "[Dataflow][Core][Graph]" ) {
                           { "out_port", "to" },
                           { "in_node", "SinkFloat" },
                           { "in_index", 0 } } } } } } } } };
-        result = g.fromJson( goodSimpleGraph );
-        REQUIRE( result );
+
+        REQUIRE( g.fromJson( goodSimpleGraph ) );
 
         // trying to add a duplicated node
         auto duplicatedNodeName =
@@ -243,10 +249,13 @@ TEST_CASE( "Dataflow/Core/Graph", "[Dataflow][Core][Graph]" ) {
         auto r = g.addNode( duplicatedNodeName );
         REQUIRE( !r );
 
+        // get unknown node
         auto sinkFloatNode = g.getNode( "Sink" );
         REQUIRE( sinkFloatNode == nullptr );
+        // get known node
         sinkFloatNode = g.getNode( "SinkFloat" );
         REQUIRE( sinkFloatNode != nullptr );
+
         auto sourceFloatNode = g.getNode( "SourceFloat" );
         REQUIRE( sourceFloatNode != nullptr );
 
@@ -263,45 +272,41 @@ TEST_CASE( "Dataflow/Core/Graph", "[Dataflow][Core][Graph]" ) {
         REQUIRE( !g.addLink( sourceIntNode, "out", sinkIntNode, "in" ) );
 
         REQUIRE( g.addNode( sinkIntNode ) );
-        // output port of "from" node not found
+
         // output port of "from" node not found
         REQUIRE( !g.addLink( sourceIntNode, "out", sinkIntNode, "in" ) );
 
         // input port of "to" node not found
-        result = g.addLink( sourceIntNode, "to", sinkIntNode, "in" );
-        REQUIRE( !result );
+        REQUIRE( !g.addLink( sourceIntNode, "to", sinkIntNode, "in" ) );
 
         // link OK
-        result = g.addLink( sourceIntNode, "to", sinkIntNode, "from" );
-        REQUIRE( result );
+        REQUIRE( g.addLink( sourceIntNode, "to", sinkIntNode, "from" ) );
 
         // from port of "to" node already linked
-        result = g.addLink( sourceIntNode, "to", sinkIntNode, "from" );
-        REQUIRE( !result );
+        REQUIRE( !g.addLink( sourceIntNode, "to", sinkIntNode, "from" ) );
 
         // type mismatch
-        result = g.addLink( sourceIntNode, "to", sinkFloatNode, "from" );
-        REQUIRE( !result );
+        REQUIRE( !g.addLink( sourceIntNode, "to", sinkFloatNode, "from" ) );
 
         // protect the graph to prevent link removal
         g.setNodesAndLinksProtection( true );
         REQUIRE( g.getNodesAndLinksProtection() );
         // unable to remove links from protected graph ...
-        result = g.removeLink( sinkIntNode, "from" );
-        REQUIRE( !result );
+        REQUIRE( !g.removeLink( sinkIntNode, "from" ) );
         g.setNodesAndLinksProtection( false );
         REQUIRE( !g.getNodesAndLinksProtection() );
         // remove link OK
-        result = g.removeLink( sinkIntNode, "from" );
-        REQUIRE( result );
+
+        REQUIRE( g.removeLink( sinkIntNode, "from" ) );
 
         // input port not found to remove its link
-        result = g.removeLink( sinkIntNode, "in" );
-        REQUIRE( !result );
+
+        REQUIRE( !g.removeLink( sinkIntNode, "in" ) );
 
         // compile the graph
-        result = g.compile();
-        REQUIRE( result );
+        inspectGraph( g );
+
+        REQUIRE( g.compile() );
         REQUIRE( g.isCompiled() );
 
         // clear the graph
@@ -317,77 +322,75 @@ TEST_CASE( "Dataflow/Core/Graph", "[Dataflow][Core][Graph]" ) {
         REQUIRE( nullNode == nullptr );
         nullNode = g.getNode( "SinkFloat" );
         REQUIRE( nullNode == nullptr );
-
-        // destroy everything
-        g.destroy();
     }
+    // destroy everything
+    g.destroy();
+}
 
-    SECTION( "Inspection of a graph" ) {
-        auto& factories = NodeFactoriesManager::getFactoryManager();
-        auto coreFactory =
-            factories.find( NodeFactoriesManager::dataFlowBuiltInsFactoryName )->second;
-        using namespace Ra::Dataflow::Core;
+TEST_CASE( "Inspection of a graph" ) {
+    auto& factories  = NodeFactoriesManager::getFactoryManager();
+    auto coreFactory = factories.find( NodeFactoriesManager::dataFlowBuiltInsFactoryName )->second;
+    using namespace Ra::Dataflow::Core;
 
-        // add some nodes to factory
-        using FloatFilterSource    = Sources::FunctionSourceNode<float, const float&>;
-        using FloatFunctionSource  = Sources::FunctionSourceNode<float, const float&, const float&>;
-        using FloatPredicateSource = Sources::FunctionSourceNode<bool, const float&, const float&>;
-        using ReduceNode           = Functionals::ReduceNode<Ra::Core::VectorArray<float>>;
-        using TransformNode        = Functionals::TransformNode<Ra::Core::VectorArray<float>>;
-        using FloatVectorSource    = Sources::SingleDataSourceNode<Ra::Core::VectorArray<float>>;
-        REGISTER_TYPE_TO_FACTORY( coreFactory, FloatVectorSource, Sources );
-        REGISTER_TYPE_TO_FACTORY( coreFactory, FloatFilterSource, Sources );
-        REGISTER_TYPE_TO_FACTORY( coreFactory, FloatFunctionSource, Sources );
-        REGISTER_TYPE_TO_FACTORY( coreFactory, FloatPredicateSource, Sources );
-        REGISTER_TYPE_TO_FACTORY( coreFactory, ReduceNode, Functionals );
-        REGISTER_TYPE_TO_FACTORY( coreFactory, TransformNode, Functionals );
+    // add some nodes to factory
+    using FloatFilterSource    = Sources::FunctionSourceNode<float, const float&>;
+    using FloatFunctionSource  = Sources::FunctionSourceNode<float, const float&, const float&>;
+    using FloatPredicateSource = Sources::FunctionSourceNode<bool, const float&, const float&>;
+    using ReduceNode           = Functionals::ReduceNode<Ra::Core::VectorArray<float>>;
+    using TransformNode        = Functionals::TransformNode<Ra::Core::VectorArray<float>>;
+    using FloatVectorSource    = Sources::SingleDataSourceNode<Ra::Core::VectorArray<float>>;
+    REGISTER_TYPE_TO_FACTORY( coreFactory, FloatVectorSource, Sources );
+    REGISTER_TYPE_TO_FACTORY( coreFactory, FloatFilterSource, Sources );
+    REGISTER_TYPE_TO_FACTORY( coreFactory, FloatFunctionSource, Sources );
+    REGISTER_TYPE_TO_FACTORY( coreFactory, FloatPredicateSource, Sources );
+    REGISTER_TYPE_TO_FACTORY( coreFactory, ReduceNode, Functionals );
+    REGISTER_TYPE_TO_FACTORY( coreFactory, TransformNode, Functionals );
 
-        std::cout << "Loading graph data/Dataflow/ExampleGraph.json\n";
-        auto g = DataflowGraph::loadGraphFromJsonFile( "data/Dataflow/ExampleGraph.json" );
-        REQUIRE( g );
-        // Factories used by the graph
-        const auto& nodes = g->getNodes();
-        REQUIRE( nodes.size() == g->getNodesCount() );
-        auto c = g->compile();
-        REQUIRE( c == true );
-        REQUIRE( g->isCompiled() );
-        // Prints the graph content
-        inspectGraph( *g );
-        g->needsRecompile();
-        REQUIRE( !g->isCompiled() );
+    std::cout << "Loading graph data/Dataflow/ExampleGraph.json\n";
+    auto g = DataflowGraph::loadGraphFromJsonFile( "data/Dataflow/ExampleGraph.json" );
+    REQUIRE( g );
+    // Factories used by the graph
+    const auto& nodes = g->getNodes();
+    REQUIRE( nodes.size() == g->getNodesCount() );
+    auto c = g->compile();
+    REQUIRE( c == true );
+    REQUIRE( g->isCompiled() );
+    // Prints the graph content
+    inspectGraph( *g );
+    g->needsRecompile();
+    REQUIRE( !g->isCompiled() );
 
-        // removing the boolean sink from the graph
-        auto n        = g->getNode( "validation value" );
-        auto useCount = n.use_count();
-        REQUIRE( n->getInstanceName() == "validation value" );
-        c = g->removeNode( n );
-        REQUIRE( c == true );
-        REQUIRE( n );
-        REQUIRE( n.use_count() == useCount - 1 );
-        c = g->compile();
-        REQUIRE( c == true );
+    // removing the boolean sink from the graph
+    auto n        = g->getNode( "validation value" );
+    auto useCount = n.use_count();
+    REQUIRE( n->getInstanceName() == "validation value" );
+    c = g->removeNode( n );
+    REQUIRE( c == true );
+    REQUIRE( n );
+    REQUIRE( n.use_count() == useCount - 1 );
+    c = g->compile();
+    REQUIRE( c == true );
 
-        // Simplified graph after compilation
-        auto& cn = g->getNodesByLevel();
-        // the source "Validator" is no more in level 0 as it is not reachable from a sink in the
-        // graph.
-        auto found = std::find_if( cn[0].begin(), cn[0].end(), []( const auto& nn ) {
-            return nn->getInstanceName() == "Validator";
-        } );
-        REQUIRE( found == cn[0].end() );
+    // Simplified graph after compilation
+    auto& cn = g->getNodesByLevel();
+    // the source "Validator" is no more in level 0 as it is not reachable from a sink in the
+    // graph.
+    auto found = std::find_if( cn[0].begin(), cn[0].end(), []( const auto& nn ) {
+        return nn->getInstanceName() == "Validator";
+    } );
+    REQUIRE( found == cn[0].end() );
 
-        // removing the source "Validator"
-        n = g->getNode( "Validator" );
-        REQUIRE( n->getInstanceName() == "Validator" );
-        // protect the graph to prevent node removal
-        g->setNodesAndLinksProtection( true );
-        c = g->removeNode( n );
-        REQUIRE( !c );
-        g->setNodesAndLinksProtection( false );
-        c = g->removeNode( n );
-        REQUIRE( c );
+    // removing the source "Validator"
+    n = g->getNode( "Validator" );
+    REQUIRE( n->getInstanceName() == "Validator" );
+    // protect the graph to prevent node removal
+    g->setNodesAndLinksProtection( true );
+    c = g->removeNode( n );
+    REQUIRE( !c );
+    g->setNodesAndLinksProtection( false );
+    c = g->removeNode( n );
+    REQUIRE( c );
 
-        std::cout << "####### Graph after sink and source removal\n";
-        inspectGraph( *g );
-    }
+    std::cout << "####### Graph after sink and source removal\n";
+    inspectGraph( *g );
 }
