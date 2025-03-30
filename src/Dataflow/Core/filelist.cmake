377efb172c9fedf4ec6e3092ae560d4347d89a97
dlyr <github@dlyr.fr>
2025-03-30 12:00:56 +0200

    [dataflow] Update filelist.cmake with script.

--- src/Dataflow/Core/filelist.cmake
+++ src/Dataflow/Core/filelist.cmake
@@ -1,9 +1,13 @@
 # ----------------------------------------------------
-# This file is maintened by hand, no script support yet
+# This file can be generated from a script:
+# To do so, run "./generateFilelistForModule.sh Dataflow/Core"
+# from ./scripts directory
 # ----------------------------------------------------
 
-set(dataflow_core_sources DataflowGraph.cpp Node.cpp NodeFactory.cpp Nodes/CoreBuiltInsNodes.cpp
-                          Port.cpp
+set(dataflow_core_sources
+    DataflowGraph.cpp Node.cpp NodeFactory.cpp Nodes/CoreBuiltInsNodes.cpp
+    Nodes/Private/FunctionalsNodeFactory.cpp Nodes/Private/SinksNodeFactory.cpp
+    Nodes/Private/SourcesNodeFactory.cpp Port.cpp
 )
 
 set(dataflow_core_headers
@@ -19,16 +23,15 @@ set(dataflow_core_headers
     Nodes/Functionals/FunctionNode.hpp
     Nodes/Functionals/ReduceNode.hpp
     Nodes/Functionals/TransformNode.hpp
+    Nodes/Private/FunctionalsNodeFactory.hpp
+    Nodes/Private/SinksNodeFactory.hpp
+    Nodes/Private/SourcesNodeFactory.hpp
     Nodes/Sinks/CoreDataSinks.hpp
     Nodes/Sinks/SinkNode.hpp
     Nodes/Sources/CoreDataSources.hpp
     Nodes/Sources/FunctionSource.hpp
     Nodes/Sources/SingleDataSourceNode.hpp
     Port.hpp
-)
-
-set(dataflow_core_private
-    Nodes/Private/FunctionalsNodeFactory.hpp Nodes/Private/FunctionalsNodeFactory.cpp
-    Nodes/Private/SinksNodeFactory.hpp Nodes/Private/SinksNodeFactory.cpp
-    Nodes/Private/SourcesNodeFactory.hpp Nodes/Private/SourcesNodeFactory.cpp
+    PortIn.hpp
+    PortOut.hpp
 )
