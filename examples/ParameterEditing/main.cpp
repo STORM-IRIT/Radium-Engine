a268c7c22b9559ed5eef27412ceeaae90ed72ed0
dlyr <github@dlyr.fr>
2025-03-30 21:39:29 +0200

    [examples] Use BasicUiBuider (in place of ad hoc visitor).

--- examples/ParameterEditing/main.cpp
+++ examples/ParameterEditing/main.cpp
@@ -4,6 +4,7 @@
 #include <Core/Containers/VariableSetEnumManagement.hpp>
 #include <Core/Utils/TypesUtils.hpp>
 #include <Engine/Data/RenderParameters.hpp>
+#include <Gui/ParameterSetEditor/BasicUiBuilder.hpp>
 #include <Gui/ParameterSetEditor/ParameterSetEditor.hpp>
 
 #include <nlohmann/json.hpp>
@@ -121,110 +122,6 @@ using ValuesType = typename std::underlying_type_t<Values>;
 enum Values2 : unsigned int { VALUE2_0 = 10, VALUE2_1 = 20, VALUE2_2 = 30 };
 using Values2Type = typename std::underlying_type_t<Values2>;
 
-class UiBuilder : public DynamicVisitor
-{
-  public:
-    UiBuilder( VariableSet& params, VariableSetEditor* pse, const nlohmann::json& constraints ) :
-        m_params { params }, m_pse { pse }, m_constraints { constraints } {
-        addOperator<bool>( *this );
-        addOperator<int>( *this );
-        addOperator<unsigned int>( *this );
-        addOperator<Scalar>( *this );
-        addOperator<std::vector<int>>( *this );
-        addOperator<Ra::Core::Utils::Color>( *this );
-        addOperator<Vector2>( *this );
-        addOperator<Vector3>( *this );
-        addOperator<Vector4>( *this );
-        addOperator<Matrix3>( *this );
-        addOperator<std::reference_wrapper<RenderParameters>>( *this );
-        addOperator<std::reference_wrapper<const RenderParameters>>( *this );
-    }
-
-    void operator()( const std::string& name, bool& p ) {
-        auto onBoolParameterChanged = [pse = this->m_pse, &p, nm = name]( bool val ) {
-            p = val;
-            emit pse->parameterModified( nm );
-        };
-        if ( m_constraints.contains( name ) ) {
-            if ( m_constraints[name]["editable"] ) {
-                const auto& m           = m_constraints[name];
-                std::string description = m.contains( "description" ) ? m["description"] : "";
-                std::string nm          = m.contains( "name" ) ? std::string { m["name"] } : name;
-                m_pse->addOption( nm, onBoolParameterChanged, p, description );
-            }
-        }
-        else if ( m_pse->showUnspecified() ) {
-            m_pse->addOption( name, onBoolParameterChanged, p );
-        }
-    }
-
-    template <typename TParam, std::enable_if_t<std::is_arithmetic<TParam>::value, bool> = true>
-    void operator()( const std::string& name, TParam& p ) {
-        using namespace Ra::Core::VariableSetEnumManagement;
-
-        if ( getEnumConverter<TParam>( m_params, name ) ) {
-            // known enum
-            m_pse->addEnumWidget( name, p, m_params, m_constraints );
-        }
-        else {
-            // number (or unknown enum
-            m_pse->addNumberWidget( name, p, m_params, m_constraints );
-        }
-    }
-
-    template <typename TParam,
-              typename TAllocator,
-              std::enable_if_t<std::is_arithmetic<TParam>::value, bool> = true>
-    void operator()( const std::string& name, std::vector<TParam, TAllocator>& p ) {
-        m_pse->addVectorWidget( name, p, m_params, m_constraints );
-    }
-
-    void operator()( const std::string& name, Ra::Core::Utils::Color& p ) {
-        auto onColorParameterChanged = [pse = this->m_pse, &params = m_params, nm = name](
-                                           const Ra::Core::Utils::Color& val ) {
-            params.setVariable( nm, val );
-            emit pse->parameterModified( nm );
-        };
-        if ( m_constraints.contains( name ) ) {
-            const auto& m           = m_constraints[name];
-            std::string description = m.contains( "description" ) ? m["description"] : "";
-            std::string nm          = m.contains( "name" ) ? std::string { m["name"] } : name;
-            m_pse->addColorInput( nm, onColorParameterChanged, p, m["maxItems"] == 4, description );
-        }
-        else if ( m_pse->showUnspecified() ) {
-            m_pse->addColorInput( name, onColorParameterChanged, p );
-        }
-    }
-
-    template <template <typename, int...> typename M, typename T, int... dim>
-    void operator()( const std::string& name, M<T, dim...>& p ) {
-        m_pse->addMatrixWidget( name, p, m_params, m_constraints );
-    }
-
-    //    void operator()( const std::string& /*name*/,
-    //                   Data::RenderParameters::TextureInfo& /*p*/,
-    //               Core::VariableSet&& /*params*/ ) {
-    // textures are not yet editable
-    //}
-
-    template <typename T>
-    void operator()( const std::string& name, std::reference_wrapper<T>& p ) {
-        m_pse->addLabel( name );
-        if constexpr ( std::is_assignable_v<VariableSet, typename std::decay<T>::type> ) {
-            if constexpr ( std::is_const_v<T> ) {
-                p.get().visit(
-                    *this, const_cast<VariableSet&>( static_cast<const VariableSet&>( p.get() ) ) );
-            }
-            else { p.get().visit( *this, static_cast<VariableSet&>( p.get() ) ); }
-        }
-    }
-
-  private:
-    VariableSetEditor* m_pse { nullptr };
-    const nlohmann::json& m_constraints;
-    VariableSet& m_params;
-};
-
 int main( int argc, char* argv[] ) {
     //! [Filling json parameter descriptor]
     auto parameterSet_metadata = R"(
@@ -335,7 +232,7 @@ int main( int argc, char* argv[] ) {
 
     //! [Filling the editor with the parameter set ]
 
-    UiBuilder builder { parameters, &editor, parameterSet_metadata };
+    BasicUiBuilder builder { parameters, &editor, parameterSet_metadata };
     parameters.visit( builder );
 
     //    editor.setupUi( parameters, parameterSet_metadata );
