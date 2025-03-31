54783ed197088d7fcd2e0316af5bfea821e17fd6
dlyr <github@dlyr.fr>
2025-03-31 18:16:28 +0200

    [pch] Update (or create) pch list.

--- src/Dataflow/Rendering/pch.hpp
+++ src/Dataflow/Rendering/pch.hpp
@@ -1,1 +1,14 @@
 #pragma once
+
+#include <Core/Resources/Resources.hpp>
+#include <Core/Utils/Color.hpp>
+#include <Dataflow/Core/DataflowGraph.hpp>
+#include <Dataflow/Core/Node.hpp>
+#include <Dataflow/RaDataflow.hpp>
+#include <Engine/Data/ShaderProgramManager.hpp>
+#include <Engine/Data/Texture.hpp>
+#include <Engine/Data/ViewingParameters.hpp>
+#include <Engine/Scene/DefaultCameraManager.hpp>
+#include <Engine/Scene/DefaultLightManager.hpp>
+#include <Engine/Scene/Light.hpp>
+#include <globjects/Framebuffer.h>
