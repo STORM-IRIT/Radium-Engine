0e15a5605c11d0d7ecc6723856f9bfc32e3a07ac
dlyr <github@dlyr.fr>
2025-10-24 08:53:17 +0200

    [dataflow-qtgui] semitransparent background to emphasis checkboxes.

--- src/Dataflow/QtGui/GraphEditor/GraphModel.cpp
+++ src/Dataflow/QtGui/GraphEditor/GraphModel.cpp
@@ -136,7 +136,8 @@ bool GraphModel::nodeExists( NodeId const nodeId ) const {
 
 QWidget* GraphModel::getWidget( std::shared_ptr<Core::Node> node ) const {
     QWidget* controlPanel = new QWidget;
-    controlPanel->setStyleSheet( "background-color:transparent;" );
+    //    controlPanel->setStyleSheet( "background-color:transparent;" );
+    controlPanel->setWindowOpacity( 0.8 );
     QVBoxLayout* layout = new QVBoxLayout( controlPanel );
 
     auto node_inputs = node->input_variables();
