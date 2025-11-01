4053a655ce4901086dafc8819bb35fdbe59f611c
dlyr <github@dlyr.fr>
2025-11-01 15:40:46 +0100

    [examples] More enum, cleaner, in ParameterEditing.

--- examples/ParameterEditing/main.cpp
+++ examples/ParameterEditing/main.cpp
@@ -1,5 +1,6 @@
 // Include Radium base application and its simple Gui
 
+#include "Core/Containers/MakeShared.hpp"
 #include <Core/Containers/VariableSet.hpp>
 #include <Core/Containers/VariableSetEnumManagement.hpp>
 #include <Core/Utils/TypesUtils.hpp>
@@ -13,18 +14,38 @@
 #include <QDialog>
 #include <QVBoxLayout>
 
-#include <typeinfo>
+#include <type_traits>
 
 using namespace Ra::Engine::Data;
 using namespace Ra::Gui;
 using namespace Ra::Core;
+
+// Enumeration that will be available as string or numeric value
+// four cases
+// enum class with enum converter
+enum class Values1 : unsigned int { VALUE1_0 = 10, VALUE1_1 = 20, VALUE1_2 = 30 };
+using ValuesType1 = typename std::underlying_type_t<Values1>;
+
+// classic enum, added as int in set -> int spin box
+enum Values2 : unsigned int { VALUE2_0 = 1, VALUE2_1 = 2, VALUE2_2 = 3 };
+using Values2Type2 = typename std::underlying_type_t<Values2>;
+
+// classic enum, added as int in set with converter -> enum combo box
+enum Values3 : unsigned int { VALUE3_0 = 100, VALUE3_1 = 200, VALUE3_2 = 300 };
+using ValuesType3 = typename std::underlying_type_t<Values3>;
+
+// enum class without converter, but with values in constraints -> enum combo box
+enum class Values4 : unsigned int { VALUE4_0 = 0, VALUE4_1 = 1 };
+using ValuesType4 = typename std::underlying_type_t<Values4>;
+
+// enum class without converter nor values are ignored.
 
 // Visitor to print all or some of the parameters stored in a RenderParameter object.
 // This visitor is very similar to the one used to build the editing ui (RenderParameterUiBuilder
 // in ParameterSetEditor.cpp) and could be used with a predicate function accepting variable wrt
 // their name
 struct ParameterPrinter {
-    using types              = Ra::Engine::Data::RenderParameters::BindableTypes;
+    using types = Ra::Engine::Data::RenderParameters::BindableTypes::Append<Values1, Values4>;
     using SelectionPredicate = std::function<bool( const std::string& )>;
 
     void operator()(
@@ -61,6 +82,17 @@ struct ParameterPrinter {
                        std::ostream_iterator<T>( std::cout, ", " ) );
             if ( !value.empty() ) { std::cout << value.back(); }
             std::cout << "]\n";
+        }
+    }
+
+    template <typename T, std::enable_if_t<std::is_enum_v<T>, bool> = true>
+    void operator()(
+        const std::string& name,
+        T& value,
+        SelectionPredicate&& pred = []( const std::string& ) { return true; } ) {
+        if ( pred( name ) ) {
+            std::cout << name << " (" << Utils::demangleType<T>() << ") --> "
+                      << static_cast<std::underlying_type_t<T>>( value ) << "\n";
         }
     }
 
@@ -111,13 +143,6 @@ struct ParameterPrinter {
     }
 };
 
-// Enumeration that will be available as string or numeric value
-enum Values : unsigned int { VALUE_0 = 10, VALUE_1 = 20, VALUE_2 = 30 };
-using ValuesType = typename std::underlying_type_t<Values>;
-
-enum Values2 : unsigned int { VALUE2_0 = 10, VALUE2_1 = 20, VALUE2_2 = 30 };
-using Values2Type = typename std::underlying_type_t<Values2>;
-
 int main( int argc, char* argv[] ) {
     //! [Filling json parameter descriptor]
     auto parameterSet_metadata = R"(
@@ -127,9 +152,22 @@ int main( int argc, char* argv[] ) {
         "editable": true,
         "type": "boolean"
         },
-    "enum": {
-        "description": "unscoped enum, ranging from 10 to 30 with step 10",
+    "enum1": {
+        "description": "enum class with converter",
         "type": "enum"
+    },
+    "enum2": {
+        "description": "plain enum without converter, added as uint",
+        "type": "enum"
+    },
+    "enum3": {
+        "description": "plain enum with converter, added as uint",
+        "type": "enum"
+    },
+    "enum4": {
+        "description": "enum class without converter, values are mendatory",
+        "type": "enum",
+        "values":["VALUE4_0","VALUE4_1"]
     },
     "int_constrained": {
       "description": "Integer between -10 and 10",
@@ -186,19 +224,28 @@ int main( int argc, char* argv[] ) {
     //! [Creating the editing dialog]
 
     //! [Management of string<->value conversion for enumeration parameters]
-    auto vnc = new Ra::Core::Utils::EnumConverter<ValuesType>( { { Values::VALUE_0, "VALUE_0" },
-                                                                 { Values::VALUE_1, "VALUE_1" },
-                                                                 { Values::VALUE_2, "VALUE_2" } } );
-    auto valuesEnumConverter = std::shared_ptr<Ra::Core::Utils::EnumConverter<ValuesType>>( vnc );
+    using namespace Ra::Core::Utils;
+    auto valuesEnumConverter1 = std::make_shared<EnumConverter<Values1>>(
+        EnumConverter<Values1>::InitializerList { { Values1::VALUE1_0, "VALUE1_0" },
+                                                  { Values1::VALUE1_1, "VALUE1_1" },
+                                                  { Values1::VALUE1_2, "VALUE1_2" } } );
+
+    auto valuesEnumConverter3 = std::make_shared<EnumConverter<ValuesType3>>(
+        EnumConverter<ValuesType3>::InitializerList { { Values3::VALUE3_0, "VALUE3_0" },
+                                                      { Values3::VALUE3_1, "VALUE3_1" },
+                                                      { Values3::VALUE3_2, "VALUE3_2" } } );
     //! [Management of string<->value conversion for enumeration parameters]
 
     //! [filling the parameter set to edit ]
     RenderParameters parameters;
     using namespace Ra::Core::VariableSetEnumManagement;
-    addEnumConverter( parameters, "enum", valuesEnumConverter );
     parameters.setVariable( "bool", false );
-    setEnumVariable( parameters, "enum", Values::VALUE_1 );
+    addEnumConverter( parameters, "enum1", valuesEnumConverter1 );
+    addEnumConverter( parameters, "enum3", valuesEnumConverter3 );
+    parameters.setVariable( "enum1", Values1::VALUE1_1 );
     setEnumVariable( parameters, "enum2", Values2::VALUE2_1 );
+    setEnumVariable( parameters, "enum3", Values3::VALUE3_2 );
+    parameters.setVariable( "enum4", Values4::VALUE4_0 );
     parameters.setVariable( "int", int( 0 ) );
     parameters.setVariable( "int_constrained", int( 0 ) );
     parameters.setVariable( "uint", (unsigned int)( 10 ) );
@@ -236,6 +283,12 @@ int main( int argc, char* argv[] ) {
     // std::is_assignable_v<VariableSet>
     builder.addOperator<RenderParameters>( builder );
 
+    // add operator for enum1, functor already present as template accepting std::is_enum<enum>
+    builder.addOperator<Values1>( builder );
+    builder.addOperator<Values2>( builder ); // do nothing, since enum2 added as int
+    builder.addOperator<Values3>( builder ); // do nothing, since enum3 added as int
+    builder.addOperator<Values4>( builder );
+
     parameters.visit( builder );
 
     auto printParameter = [&parameters]( const std::string& p ) {
@@ -253,6 +306,5 @@ int main( int argc, char* argv[] ) {
     //! [Printing several parameters after editing ]
     std::cout << "\nPrinting all parameters before quit : ";
     parameters.visit( ParameterPrinter {} );
-
     //! [Printing several parameters after editing ]
 }
