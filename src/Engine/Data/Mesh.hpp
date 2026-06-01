0c13a1d998dea509d5eae160ff0bc4deea039487
dlyr <github@dlyr.fr>
2026-06-01 08:47:16 +0200

    [engine] KeyHash is in a template class, no need to export.
    Fix indent.

--- src/Engine/Data/Mesh.hpp
+++ src/Engine/Data/Mesh.hpp
@@ -375,9 +375,9 @@ class MultiIndexedGeometry : public CoreGeometryDisplayable<T>
     using LayerKeyType            = std::pair<LayerSemanticCollection, std::string>;
 
     using EntryType = std::pair<bool, VaoIndices*>;
-    struct RA_CORE_API KeyHash {
-        std::size_t operator()( const LayerKeyType& k ) const {
-            // Mix semantic collection into a single identifier string
+    struct KeyHash {
+        std::size_t operator()( const LayerKeyType& k )
+            const { // Mix semantic collection into a single identifier string
             std::ostringstream stream;
             std::copy(
                 k.first.begin(), k.first.end(), std::ostream_iterator<std::string>( stream, "" ) );
@@ -388,9 +388,9 @@ class MultiIndexedGeometry : public CoreGeometryDisplayable<T>
             return std::hash<std::string> {}( result ) ^
                    ( std::hash<std::string> {}( k.second ) << 1 );
         }
-    }; // namespace Data
+    };
     std::unordered_map<LayerKeyType, EntryType, KeyHash> m_indices;
-}; // namespace Engine
+};
 
 /// LineMesh, own a Core::Geometry::LineMesh
 class RA_ENGINE_API LineMesh : public IndexedGeometry<Core::Geometry::LineMesh>
